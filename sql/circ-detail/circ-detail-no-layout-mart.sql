46ee08b124432771be059c57f383eeb963efa131
Nassib Nassar <246609+nassibnassar@users.noreply.github.com>
2019-01-22 21:55:47 +0000

    Update query with column name change

--- sql/circ-detail/circ-detail-no-layout-mart.sql
+++ sql/circ-detail/circ-detail-no-layout-mart.sql
@@ -6,7 +6,7 @@ SELECT loc.location_name AS location_name,
             FROM fact_loans
             WHERE loan_date >= '2017-01-01' AND loan_date <= '2018-12-31'
     ) l
-        LEFT JOIN dim_locations loc ON l.location_id = loc.location_id
+        LEFT JOIN dim_locations loc ON l.location_id = loc.id
         LEFT JOIN dim_users u ON l.user_id = u.id
     GROUP BY loc.location_name, u.group_name
     ORDER BY loc.location_name, u.group_name;
