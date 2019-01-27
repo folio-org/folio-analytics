75c82bd8d2be428c83c8b6f24a2fe91373527e9c
Nassib Nassar <246609+nassibnassar@users.noreply.github.com>
2019-01-27 05:06:19 +0000

    Rename tables to track schema

--- sql/circ-detail/circ-detail-no-layout-mart.sql
+++ sql/circ-detail/circ-detail-no-layout-mart.sql
@@ -3,11 +3,11 @@ SELECT loc.location_name AS location_name,
        count(l.id) AS count
     FROM (
         SELECT id, user_id, location_id
-            FROM fact_loans
+            FROM f_loans
             WHERE loan_date >= '2017-01-01' AND loan_date <= '2018-12-31'
     ) l
-        LEFT JOIN dim_locations loc ON l.location_id = loc.id
-        LEFT JOIN dim_users u ON l.user_id = u.id
+        LEFT JOIN d_locations loc ON l.location_id = loc.id
+        LEFT JOIN d_users u ON l.user_id = u.id
     GROUP BY loc.location_name, u.group_name
     ORDER BY loc.location_name, u.group_name;
 
