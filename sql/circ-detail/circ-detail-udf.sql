bcfc263cbdf2f77b6b0985eb59103ef0256fe50f
Nassib Nassar <246609+nassibnassar@users.noreply.github.com>
2019-02-05 00:33:42 +0000

    Update query to use keys instead of FOLIO IDs

--- sql/circ-detail/circ-detail-udf.sql
+++ sql/circ-detail/circ-detail-udf.sql
@@ -6,14 +6,14 @@ CREATE FUNCTION circ_detail(start_date DATE, end_date DATE)
     AS $$
 SELECT loc.location_name AS location_name,
        u.group_name AS group_name,
-       count(l.id) AS ct
+       count(l.loan_key) AS ct
     FROM (
-        SELECT id, user_id, location_id
-            FROM f_loans
+        SELECT loan_key, user_key, location_key
+            FROM loans
             WHERE loan_date >= start_date AND loan_date <= end_date
     ) l
-        LEFT JOIN d_locations loc ON l.location_id = loc.id
-        LEFT JOIN d_users u ON l.user_id = u.id
+        LEFT JOIN locations loc ON l.location_key = loc.location_key
+        LEFT JOIN users u ON l.user_key = u.user_key
     GROUP BY loc.location_name, u.group_name
     ORDER BY loc.location_name, u.group_name;
 $$
