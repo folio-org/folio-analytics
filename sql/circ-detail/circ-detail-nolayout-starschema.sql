4bf3d241fe6c5a6084795b7f74300812c8775502
amzoss <angela.zoss@gmail.com>
2019-03-25 09:00:25 -0400

    Update query to use keys instead of FOLIO IDs

--- sql/circ-detail/circ-detail-nolayout-starschema.sql
+++ sql/circ-detail/circ-detail-nolayout-starschema.sql
@@ -1,13 +1,14 @@
 SELECT loc.location_name AS location_name,
        u.group_name AS group_name,
-       count(l.id) AS ct
+       count(l.loan_key) AS ct
     FROM (
-        SELECT id, user_id, location_id
-            FROM f_loans
+        SELECT loan_key, user_key, location_key
+            FROM loans
             WHERE loan_date >= '2017-01-01' AND loan_date <= '2018-12-31'
     ) l
-        LEFT JOIN d_locations loc ON l.location_id = loc.id
-        LEFT JOIN d_users u ON l.user_id = u.id
-    GROUP BY loc.location_name, u.group_name
-    ORDER BY loc.location_name, u.group_name;
-
+        LEFT JOIN locations loc 
+              ON l.location_key = loc.location_key
+        LEFT JOIN users u
+              ON l.user_key = u.user_key
+        GROUP BY loc.location_name, u.group_name
+        ORDER BY loc.location_name, u.group_name;
