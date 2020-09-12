29f4098add40f683be43eaba33152afbd4e786c2
Nassib Nassar <246609+nassibnassar@users.noreply.github.com>
2020-09-11 21:59:55 -0400

    Reformat local.instances_ext

--- sql/derived_tables/redshift/instances_ext_rs.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE local.instances_ext
-    ALTER DISTKEY instance_id;
-
-ALTER TABLE local.instances_ext
-    ALTER COMPOUND SORTKEY (instance_id);
-
-VACUUM local.instances_ext;
-ANALYZE local.instances_ext;
