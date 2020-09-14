2d92261f757a2d0bf65177c3e7d7b6fb1619872d
Nassib Nassar <246609+nassibnassar@users.noreply.github.com>
2020-09-14 11:51:23 -0400

    Add table local.instances_languages

---
+++ sql/derived_tables/instances_languages.sql
@@ -0,0 +1,20 @@
+DROP TABLE IF EXISTS local.instances_languages;
+
+CREATE TABLE local.instances_languages AS
+SELECT
+    instances.id AS instance_id,
+    instances.hrid AS instance_hrid,
+    json_array_elements_text(json_extract_path(instances.data, 'languages')) AS
+    LANGUAGE
+FROM
+    inventory_instances AS instances;
+
+CREATE INDEX ON local.instances_languages (instance_id);
+
+CREATE INDEX ON local.instances_languages (instance_hrid);
+
+CREATE INDEX ON local.instances_languages (
+LANGUAGE);
+
+VACUUM ANALYZE local.instances_languages;
+
