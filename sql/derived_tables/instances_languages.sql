2ac08eafdcda280d6a2bef371fca2ae62903d8dd
Nassib Nassar <246609+nassibnassar@users.noreply.github.com>
2020-09-14 11:54:12 -0400

    Quote language attribute name

--- sql/derived_tables/instances_languages.sql
+++ sql/derived_tables/instances_languages.sql
@@ -4,8 +4,7 @@ CREATE TABLE local.instances_languages AS
 SELECT
     instances.id AS instance_id,
     instances.hrid AS instance_hrid,
-    json_array_elements_text(json_extract_path(instances.data, 'languages')) AS
-    LANGUAGE
+    json_array_elements_text(json_extract_path(instances.data, 'languages')) AS "language"
 FROM
     inventory_instances AS instances;
 
