afe4cdaa277df9e15c70561d25d46f17a009d5ee
Nassib Nassar <246609+nassibnassar@users.noreply.github.com>
2020-09-15 15:31:55 -0400

    Add table local.instances_nature_content

---
+++ sql/derived_tables/instances_nature_content.sql
@@ -0,0 +1,36 @@
+DROP TABLE IF EXISTS local.instances_nature_content;
+
+CREATE TABLE local.instances_nature_content AS
+WITH nature_content AS (
+    SELECT
+        id AS instance_id,
+        hrid AS instance_hrid,
+        json_array_elements_text(json_extract_path(data, 'natureOfContentTermIds')) AS nature_content_id
+    FROM
+        inventory_instances
+)
+SELECT
+    instance_id,
+    instance_hrid,
+    nature_content_id,
+    nature_content_term.id AS nature_content_term_id,
+    nature_content_term.name AS nature_content_term_name,
+    nature_content_term.source AS nature_content_term_source
+FROM
+    nature_content
+    JOIN inventory_nature_of_content_terms AS nature_content_term ON nature_content_term.id = nature_content.nature_content_id;
+
+CREATE INDEX ON local.instances_nature_content (instance_id);
+
+CREATE INDEX ON local.instances_nature_content (instance_hrid);
+
+CREATE INDEX ON local.instances_nature_content (nature_content_term_id);
+
+CREATE INDEX ON local.instances_nature_content (nature_content_term_name);
+
+CREATE INDEX ON local.instances_nature_content (nature_content_term_source);
+
+VACUUM local.instances_nature_content;
+
+ANALYZE local.instances_nature_content;
+
