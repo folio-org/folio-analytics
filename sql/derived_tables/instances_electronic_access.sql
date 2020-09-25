bf8b6ea245cf8dc7a3ff309b848f553044485e7a
Nassib Nassar <246609+nassibnassar@users.noreply.github.com>
2020-09-25 18:44:43 -0400

    Fix #87, array extraction in derived table instances_electronic_access

--- sql/derived_tables/instances_electronic_access.sql
+++ sql/derived_tables/instances_electronic_access.sql
@@ -2,46 +2,35 @@ DROP TABLE IF EXISTS local.instances_electronic_access;
 
 -- Create table for electronic access points for instance records
 CREATE TABLE local.instances_electronic_access AS
-WITH instances_relationship_ids AS (
-    SELECT
-        instance.id AS instance_id,
-        instance.hrid AS instance_hrid,
-        json_extract_path_text(json_array_elements(json_extract_path(instance.data, 'electronicAccess')), 'linkText') AS electronic_access_link_text,
-        json_extract_path_text(json_array_elements(json_extract_path(instance.data, 'electronicAccess')), 'materialsSpecification') AS electronic_access_materials_specification,
-        json_extract_path_text(json_array_elements(json_extract_path(instance.data, 'electronicAccess')), 'publicNote') AS electronic_access_public_note,
-        json_extract_path_text(json_array_elements(json_extract_path(instance.data, 'electronicAccess')), 'relationshipId') AS electronic_access_relationship_id,
-        json_extract_path_text(json_array_elements(json_extract_path(instance.data, 'electronicAccess')), 'uri') AS electronic_access_uri
-    FROM
-        inventory_instances AS instance
-)
 SELECT
-    instances_relationship_ids.instance_id,
-    instances_relationship_ids.instance_hrid,
-    instances_relationship_ids.electronic_access_link_text,
-    instances_relationship_ids.electronic_access_materials_specification,
-    instances_relationship_ids.electronic_access_public_note,
-    instances_relationship_ids.electronic_access_relationship_id,
-    inventory_electronic_access_relationships.name AS electronic_access_relationship_id_name,
-    instances_relationship_ids.electronic_access_uri
+    instance.id AS instance_id,
+    instance.hrid AS instance_hrid,
+    json_extract_path_text(electronic_access.data, 'linkText') AS link_text,
+    json_extract_path_text(electronic_access.data, 'materialsSpecification') AS materials_specification,
+    json_extract_path_text(electronic_access.data, 'publicNote') AS public_note,
+    json_extract_path_text(electronic_access.data, 'relationshipId') AS relationship_id,
+    inventory_instance_relationship_types.name AS relationship_type_id_name,
+    json_extract_path_text(electronic_access.data, 'uri') AS uri
 FROM
-    instances_relationship_ids
-    LEFT JOIN inventory_electronic_access_relationships ON instances_relationship_ids.electronic_access_relationship_id = inventory_electronic_access_relationships.id;
+    inventory_instances AS instance
+    CROSS JOIN json_array_elements(json_extract_path(data, 'electronicAccess')) AS electronic_access(data)
+    LEFT JOIN inventory_instance_relationship_types ON json_extract_path_text(electronic_access.data, 'relationshipId') = inventory_instance_relationship_types.id;
 
 CREATE INDEX ON local.instances_electronic_access (instance_id);
 
 CREATE INDEX ON local.instances_electronic_access (instance_hrid);
 
-CREATE INDEX ON local.instances_electronic_access (electronic_access_link_text);
+CREATE INDEX ON local.instances_electronic_access (link_text);
 
-CREATE INDEX ON local.instances_electronic_access (electronic_access_materials_specification);
+CREATE INDEX ON local.instances_electronic_access (materials_specification);
 
-CREATE INDEX ON local.instances_electronic_access (electronic_access_public_note);
+CREATE INDEX ON local.instances_electronic_access (public_note);
 
-CREATE INDEX ON local.instances_electronic_access (electronic_access_relationship_id);
+CREATE INDEX ON local.instances_electronic_access (relationship_id);
 
-CREATE INDEX ON local.instances_electronic_access (electronic_access_relationship_id_name);
+CREATE INDEX ON local.instances_electronic_access (relationship_type_id_name);
 
-CREATE INDEX ON local.instances_electronic_access (electronic_access_uri);
+CREATE INDEX ON local.instances_electronic_access (uri);
 
 VACUUM local.instances_electronic_access;
 
