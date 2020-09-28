430a6c8cce2d864e35071a71ad6b119468bd5448
Nassib Nassar <246609+nassibnassar@users.noreply.github.com>
2020-09-28 12:00:08 -0400

    Fix incorrect join in electronic access derived tables
    Closes folio-org/folio-analytics#92

--- sql/derived_tables/holdings_electronic_access.sql
+++ sql/derived_tables/holdings_electronic_access.sql
@@ -3,34 +3,34 @@ DROP TABLE IF EXISTS local.holdings_electronic_access;
 -- Create table for electronic access points for holdings records
 CREATE TABLE local.holdings_electronic_access AS
 SELECT
-    holdings.id AS holdings_id,
-    holdings.hrid AS holdings_hrid,
-    json_extract_path_text(electronic_access.data, 'linkText') AS link_text,
-    json_extract_path_text(electronic_access.data, 'materialsSpecification') AS materials_specification,
-    json_extract_path_text(electronic_access.data, 'publicNote') AS public_note,
-    json_extract_path_text(electronic_access.data, 'relationshipId') AS relationship_id,
-    inventory_instance_relationship_types.name AS relationship_type_id_name,
-    json_extract_path_text(electronic_access.data, 'uri') AS uri
+    holdings.id AS hol_id,
+    holdings.hrid AS hold_hrid,
+    json_extract_path_text(electronic_access.data, 'linkText') AS hol_link_text,
+    json_extract_path_text(electronic_access.data, 'materialsSpecification') AS hol_materials_specification,
+    json_extract_path_text(electronic_access.data, 'publicNote') AS hol_public_note,
+    json_extract_path_text(electronic_access.data, 'relationshipId') AS hol_elec_access_relationship_id,
+    inventory_electronic_access_relationships.name AS hol_elec_access_relationship_name,
+    json_extract_path_text(electronic_access.data, 'uri') AS hol_uri
 FROM
     inventory_holdings AS holdings
     CROSS JOIN json_array_elements(json_extract_path(data, 'electronicAccess')) AS electronic_access(data)
-    LEFT JOIN inventory_instance_relationship_types ON json_extract_path_text(electronic_access.data, 'relationshipId') = inventory_instance_relationship_types.id;
+    LEFT JOIN inventory_electronic_access_relationships ON json_extract_path_text(electronic_access.data, 'relationshipId') = inventory_electronic_access_relationships.id;
 
-CREATE INDEX ON local.holdings_electronic_access (holdings_id);
+CREATE INDEX ON local.holdings_electronic_access (hol_id);
 
-CREATE INDEX ON local.holdings_electronic_access (holdings_hrid);
+CREATE INDEX ON local.holdings_electronic_access (hol_hrid);
 
-CREATE INDEX ON local.holdings_electronic_access (link_text);
+CREATE INDEX ON local.holdings_electronic_access (hol_link_text);
 
-CREATE INDEX ON local.holdings_electronic_access (materials_specification);
+CREATE INDEX ON local.holdings_electronic_access (hol_materials_specification);
 
-CREATE INDEX ON local.holdings_electronic_access (public_note);
+CREATE INDEX ON local.holdings_electronic_access (hol_public_note);
 
-CREATE INDEX ON local.holdings_electronic_access (relationship_id);
+CREATE INDEX ON local.holdings_electronic_access (hol_elec_access_relationship_id);
 
-CREATE INDEX ON local.holdings_electronic_access (relationship_type_id_name);
+CREATE INDEX ON local.holdings_electronic_access (hol_elec_access_relationship_name);
 
-CREATE INDEX ON local.holdings_electronic_access (uri);
+CREATE INDEX ON local.holdings_electronic_access (hol_uri);
 
 VACUUM local.holdings_electronic_access;
 
