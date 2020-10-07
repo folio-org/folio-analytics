9f6b6933509febc25a41b42933984856495d3fa1
Nassib Nassar <246609+nassibnassar@users.noreply.github.com>
2020-10-07 17:03:23 -0400

    Fix minor formatting in instances_publication

--- sql/derived_tables/instances_publication.sql
+++ sql/derived_tables/instances_publication.sql
@@ -2,13 +2,12 @@ DROP TABLE IF EXISTS local.instances_publication;
 
 -- Create table for publication information that includes publication date, place, and publisher name from instance records
 CREATE TABLE local.instances_publication AS
-
 SELECT
     instance.id AS inst_id,
     instance.hrid AS inst_hrid,
     json_extract_path_text(publication.data, 'dateOfPublication') AS inst_pub_date,
-    json_extract_path_text(publication.DATA, 'place') AS inst_pub_place,
-    json_extract_path_text(publication.DATA, 'publisher') AS inst_pub_publisher
+    json_extract_path_text(publication.data, 'place') AS inst_pub_place,
+    json_extract_path_text(publication.data, 'publisher') AS inst_pub_publisher
 FROM
     inventory_instances AS instance
     CROSS JOIN json_array_elements(json_extract_path(instance.data, 'publication')) AS publication(data);
