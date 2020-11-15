10d7ea616e879c5f561b0f70a33c9c283ee69301
Nassib Nassar <246609+nassibnassar@users.noreply.github.com>
2020-11-14 23:59:30 -0500

    Change array extraction in local.instance_nature_content

--- sql/derived_tables/instance_nature_content.sql
+++ sql/derived_tables/instance_nature_content.sql
@@ -1,14 +1,14 @@
 DROP TABLE IF EXISTS local.instance_nature_content;
 
--- Create a local table with the nature of content information from instance records
 CREATE TABLE local.instance_nature_content AS
 WITH nature_content AS (
     SELECT
         id AS instance_id,
         hrid AS instance_hrid,
-        json_array_elements_text(json_extract_path(data, 'natureOfContentTermIds')) AS nature_of_content_term_id
+        nature_of_content_term_ids.data #>> '{}' AS nature_of_content_term_id
     FROM
         inventory_instances
+        CROSS JOIN json_array_elements(json_extract_path(data, 'natureOfContentTermIds')) AS nature_of_content_term_ids (data)
 )
 SELECT
     nature_content.instance_id,
