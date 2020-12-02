0390abaebc24a3963dd8013d731de2a89b06a596
Axel Dörrer <a.doerrer@ub.uni-mainz.de>
2020-12-02 17:52:11 +0100

    added parameter filter on language to title count and cleared some typos

--- sql/report_queries/title_count/title_count.sql
+++ sql/report_queries/title_count/title_count.sql
@@ -14,7 +14,7 @@ folio_reporting.locations_libraries
 AGGREGATION 
 type_id, type_name, mode_of_issuance_id, mode_of_issuance_name, instance_format_id, instance_format_code, instance_format_name, first_language,
 instance_statistical_code_id, instance_statistical_code, instance_statistical_code_name, nature_of_content_id, nature_of_content_code, nature_of_content_name,
-holdings_type_id, holdings_type_name, holdings_callnumber_type_id, holding_scallnumber_type_name, holdings_statistical_code_id, holdings_statistical_code, holdings_statistical_code_name,
+holdings_type_id, holdings_type_name, holdings_callnumber_type_id, holdings_callnumber_type_name, holdings_statistical_code_id, holdings_statistical_code, holdings_statistical_code_name,
 holdings_receipt_status, location_name, previously_held, instance_super_relation_relationship_type_id, instance_super_relation_relationship_type_name,
 instance_sub_relation_relationship_type_id, instance_sub_relation_relationship_type_name
  
@@ -28,10 +28,8 @@ instance_discovery_suppress, non-virtual titles by inform.format_name and loc.li
 
 STILL IN PROGRESS:
 Need adjustment in aggregation, filters, hardcoded filters 
-
-*/
+ */
 WITH parameters AS (
-
     SELECT
         '1950-04-01'::DATE AS cataloged_date_start_date, --ex:2000-01-01
         '2020-05-01'::DATE AS cataloged_date_end_date, -- ex:2020-01-01
@@ -44,17 +42,18 @@ WITH parameters AS (
         ''::VARCHAR AS instance_format_filter1, -- select 'audio -- audio disc', 'computer -- other' etc. or leave blank for all. You can use %% as wildcards.
         ''::VARCHAR AS instance_format_filter2, -- select 'audio -- audio disc', 'computer -- other' etc. or leave blank for all. You can use %% as wildcards.
         ''::VARCHAR AS instance_format_filter3, -- select 'audio -- audio disc', 'computer -- other' etc. or leave blank for all. You can use %% as wildcards.
+        ''::VARCHAR AS instance_language_filter, -- select 'eng', 'ger' etc. or leave blank for all.
         ''::VARCHAR AS instance_mode_of_issuance_filter, -- select 'integrating resource', 'serial', 'multipart monograph' etc. or leave blank for all.
         ''::VARCHAR AS nature_of_content_terms_filter, -- select 'textbook', 'journal' etc. or leave blank for all.
-        ''::VARCHAR AS holdings_receipt_status_filter, -- select 'partially received', 'fully received' etc.
+        ''::VARCHAR AS holdings_receipt_status_filter, -- select 'partially received', 'fully received' etc. or leave blank for all.
         ''::VARCHAR AS holdings_type_filter, -- select 'Electronic', 'Monograph' etc. or elave blank for all. (This is case sensitive)
-        ''::VARCHAR AS holdings_callnumber_type_filter, -- select ..... or leave blank for all.
+        ''::VARCHAR AS holdings_callnumber_type_filter, -- select 'LC Modified’, ‘Title’, ‘Shelved separately’ or leave blank for all.
         ''::VARCHAR AS holdings_callnumber_filter, -- use %% as wildcards '1234-abc%%','%%1234-abc','%%1234-abc%%'
-        ''::VARCHAR AS holdings_acquisition_method_filter, -- select ..... or leave blank for all.
+        ''::VARCHAR AS holdings_acquisition_method_filter, -- select ‘Purchase’, ‘Approval’, ‘Gifts’ or leave blank for all.
         -- STATISTICAL CODES
         ''::VARCHAR AS instance_statistical_code_filter, -- select 'Active Serial', 'Book, print (books)', 'Book, electronic (ebooks), 'Microfiche' etc. or leave blank for all.
         ''::VARCHAR AS holdings_statistical_code_filter, -- select 'Active Serial', 'Book, print (books)', 'Book, electronic (ebooks), 'Microfiche' etc. or leave blank for all.
-        ''::VARCHAR AS instance_statistical_code_type_filter, -- select ... or leave blank for all.
+        ''::VARCHAR AS instance_statistical_code_type_filter, -- select ‘ARL (Collection stats)’, ‘DISC (Discovery)’ or leave blank for all.
         -- LOCATION
         ''::VARCHAR AS permanent_location_name_filter, --Online, Annex, Main Library
         ''::VARCHAR AS campus_filter, -- select 'Main Campus','City Campus','Online' as needed
@@ -62,7 +61,6 @@ WITH parameters AS (
         ''::VARCHAR AS institution_filter -- select 'Kobenhavns Universitet','Montoya College' as needed
 )
 SELECT
-    
     inst.type_id,
     inst.type_name,
     inst.mode_of_issuance_id,
@@ -74,13 +72,13 @@ SELECT
     insc.statistical_code_id AS instance_statistical_code_id,
     insc.statistical_code AS instance_statistical_code,
     insc.statistical_code_name AS instance_statistical_code_name,
-	innc.nature_of_content_term_id AS nature_of_content_id,
+    innc.nature_of_content_term_id AS nature_of_content_id,
     innc.nature_of_content_term_id AS nature_of_content_code,
     innc.nature_of_content_term_name AS nature_of_content_name,
-	hld.type_id AS holdings_type_id,
+    hld.type_id AS holdings_type_id,
     hld.type_name AS holdings_type_name,
     hld.call_number_type_id AS holdings_callnumber_type_id,
-    hld.call_number_type_name AS holding_scallnumber_type_name,
+    hld.call_number_type_name AS holdings_callnumber_type_name,
     hsc.statistical_code_id AS holdings_statistical_code_id,
     hsc.statistical_code AS holdings_statistical_code,
     hsc.statistical_code_name AS holdings_statistical_code_name,
@@ -91,10 +89,9 @@ SELECT
     super_relation.relationship_type_name AS instance_super_relation_relationship_type_name,
     sub_relation.relationship_type_id AS instance_sub_relation_relationship_type_id,
     sub_relation.relationship_type_name AS instance_sub_relation_relationship_type_name,
-    
     COUNT(DISTINCT inst.instance_id) AS "title count"
 FROM
-    folio_reporting.instance_ext  AS inst
+    folio_reporting.instance_ext AS inst
     LEFT JOIN folio_reporting.instance_statistical_codes AS insc ON inst.instance_id = insc.instance_id
     LEFT JOIN folio_reporting.instance_nature_content AS innc ON inst.instance_id = innc.instance_id
     LEFT JOIN folio_reporting.instance_formats AS inform ON inst.instance_id = inform.instance_id
@@ -103,7 +100,7 @@ FROM
     LEFT JOIN folio_reporting.instance_relationships_ext AS sub_relation ON sub_relation.relationship_sub_instance_id = inst.instance_id
     LEFT JOIN folio_reporting.holdings_ext AS hld ON inst.instance_id = hld.instance_id
     LEFT JOIN folio_reporting.holdings_statistical_codes AS hsc ON hld.holdings_id = hsc.holdings_id
-    LEFT JOIN folio_reporting.locations_libraries AS loc ON hld.permanent_location_id = loc.location_id
+    LEFT JOIN folio_reporting.locations_libraries AS loc ON hld.permanent_location_id = loc.location_id  
 WHERE 
 
 	-- hardcoded filters 
@@ -161,6 +158,10 @@ WHERE
 			(SELECT instance_format_filter1 FROM parameters) = ''
 			AND (SELECT instance_format_filter2 FROM parameters) = ''
 			AND (SELECT instance_format_filter3 FROM parameters) = ''))
+	AND	
+	(
+		((SELECT "language" FROM folio_reporting.instance_languages AS lng WHERE lng.instance_id = inst.instance_id LIMIT 1) = (SELECT instance_language_filter FROM parameters))
+			OR ((SELECT instance_language_filter FROM parameters) = ''))
 	AND 
 	(inst.mode_of_issuance_name = (SELECT instance_mode_of_issuance_filter FROM parameters)
 	    OR (SELECT instance_mode_of_issuance_filter FROM parameters) = '')
@@ -202,9 +203,8 @@ WHERE
 		OR (SELECT library_filter FROM parameters) = '')
 	AND 
 	(hld.permanent_location_name = (SELECT permanent_location_name_filter FROM parameters)
-		OR (SELECT permanent_location_name_filter FROM parameters) = '')
+		OR (SELECT permanent_location_name_filter FROM parameters) = '')	
 GROUP BY
-
     inst.type_id,
     inst.type_name,
     inst.mode_of_issuance_id,
@@ -216,10 +216,10 @@ GROUP BY
     insc.statistical_code_id,
     insc.statistical_code,
     insc.statistical_code_name,
-	innc.nature_of_content_term_id,
+    innc.nature_of_content_term_id,
     innc.nature_of_content_term_id,
     innc.nature_of_content_term_name,
-	hld.type_id,
+    hld.type_id,
     hld.type_name,
     hld.call_number_type_id,
     hld.call_number_type_name,
