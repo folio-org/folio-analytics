fa2879b7e27dadf975d6265ead9a516a6971bf32
Nassib Nassar <246609+nassibnassar@users.noreply.github.com>
2021-01-06 10:52:18 -0500

    Extract attributes from json in po_instance

--- sql/derived_tables/po_instance.sql
+++ sql/derived_tables/po_instance.sql
@@ -8,21 +8,21 @@ SELECT
        json_extract_path_text(po_purchase_orders.data, 'metadata','createdDate') AS created_date,  
        json_extract_path_text(po_purchase_orders.data, 'metadata','createdByUserId') AS created_by, 
        json_extract_path_text(configuration_entries.value::json, 'name') AS created_location,
-       po_lines.instance_id AS pol_instance_id,
+       json_extract_path_text(po_lines.data, 'instanceId') AS pol_instance_id,
        inventory_instances.title AS title,
-       po_lines.publication_date AS date_of_publication, 
-       po_lines.publisher AS publisher, 
-       po_lines.requester AS requester, 
-       po_lines.rush AS rush
+       json_extract_path_text(po_lines.data, 'publicationDate') AS date_of_publication, 
+       json_extract_path_text(po_lines.data, 'publisher') AS publisher, 
+       json_extract_path_text(po_lines.data, 'requester') AS requester, 
+       json_extract_path_text(po_lines.data, 'rush') AS rush
 FROM po_purchase_orders
 LEFT JOIN   
-       po_lines ON po_purchase_orders.id=po_lines.purchase_order_id
+       po_lines ON po_purchase_orders.id=json_extract_path_text(po_lines.data, 'purchaseOrderId')
 LEFT JOIN inventory_instances ON 
-       po_lines.instance_id = inventory_instances.id
+       json_extract_path_text(po_lines.data, 'instanceId') = inventory_instances.id
 LEFT JOIN organization_organizations ON 
        po_purchase_orders.vendor=organization_organizations.id
 LEFT JOIN configuration_entries ON
-    po_purchase_orders.bill_to = configuration_entries.id;
+    json_extract_path_text(po_purchase_orders.data, 'billTo') = configuration_entries.id;
 
 CREATE INDEX ON folio_reporting.po_instance (po_number);
 CREATE INDEX ON folio_reporting.po_instance (vendor_code);
@@ -30,10 +30,9 @@ CREATE INDEX ON folio_reporting.po_instance (status_approved);
 CREATE INDEX ON folio_reporting.po_instance (created_date);
 CREATE INDEX ON folio_reporting.po_instance (created_by);
 CREATE INDEX ON folio_reporting.po_instance (created_location);
-CREATE INDEX ON folio_reporting.po_instance (pol_intance_id);
+CREATE INDEX ON folio_reporting.po_instance (pol_instance_id);
 CREATE INDEX ON folio_reporting.po_instance (title);
 CREATE INDEX ON folio_reporting.po_instance (date_of_publication);
 CREATE INDEX ON folio_reporting.po_instance (publisher);
 CREATE INDEX ON folio_reporting.po_instance (requester);
 CREATE INDEX ON folio_reporting.po_instance (rush);
-       
