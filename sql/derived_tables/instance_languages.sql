b71b2a9af3b6293c58f53fd2b15b04a2ec9218b5
Nassib Nassar <246609+nassibnassar@users.noreply.github.com>
2021-01-11 17:02:47 -0500

    Use cross join to extract arrays in instance_languages

--- sql/derived_tables/instance_languages.sql
+++ sql/derived_tables/instance_languages.sql
@@ -5,9 +5,10 @@ CREATE TABLE folio_reporting.instance_languages AS
 SELECT
     instances.id AS instance_id,
     instances.hrid AS instance_hrid,
-    json_array_elements_text(json_extract_path(instances.data, 'languages')) AS "language"
+    languages.data #>> '{}' AS "language"
 FROM
-    inventory_instances AS instances;
+    inventory_instances AS instances
+    CROSS JOIN json_array_elements(json_extract_path(data, 'languages')) AS languages (data);
 
 CREATE INDEX ON folio_reporting.instance_languages (instance_id);
 
