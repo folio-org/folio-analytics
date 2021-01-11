0ec48779e9eee6755bd34a01f9f59aa590372243
Nassib Nassar <246609+nassibnassar@users.noreply.github.com>
2021-01-11 18:14:25 -0500

    Add array ordinality to instance_languages

--- sql/derived_tables/instance_languages.sql
+++ sql/derived_tables/instance_languages.sql
@@ -1,14 +1,15 @@
 DROP TABLE IF EXISTS folio_reporting.instance_languages;
 
---Create a local table for languages in instance records.
+-- Create a local table for languages in instance records.
 CREATE TABLE folio_reporting.instance_languages AS
 SELECT
     instances.id AS instance_id,
     instances.hrid AS instance_hrid,
-    languages.data #>> '{}' AS "language"
+    languages.data #>> '{}' AS "language",
+    languages.ordinality
 FROM
     inventory_instances AS instances
-    CROSS JOIN json_array_elements(json_extract_path(data, 'languages')) AS languages (data);
+    CROSS JOIN LATERAL json_array_elements(json_extract_path(data, 'languages')) WITH ORDINALITY AS languages (data);
 
 CREATE INDEX ON folio_reporting.instance_languages (instance_id);
 
