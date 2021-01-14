a0213dab0c49da799b7cc599a9731c9b7b97a323
Vandana Shah <vp25@cornell.edu>
2021-01-14 14:24:41 -0500

    Add files via upload

--- sql/report_queries/claimed_returned/claimed_returned_refactored.sql
+++ sql/report_queries/claimed_returned/claimed_returned_refactored.sql
@@ -12,6 +12,12 @@ WITH parameters AS (
         '' :: VARCHAR AS current_item_permanent_location_filter, --Online, Annex, Main Library
         '' :: VARCHAR AS current_item_temporary_location_filter, --Online, Annex, Main Library
         'Main Library' :: VARCHAR AS current_item_effective_location_filter --Online, Annex, Main Library
+   ),
+   notes as (
+   SELECT item_id,
+   STRING_AGG (note, '|') AS item_notes
+   FROM folio_reporting.item_notes
+   GROUP BY item_id
    )
 SELECT 
 	(SELECT start_date :: VARCHAR FROM parameters) ||
@@ -21,14 +27,13 @@ SELECT
     li.item_id,
     li.loan_date,
     li.loan_due_date,
-    li.item_status,
-  --li.claimed_returned_date (not yet in the loans or the loans_items tables)  
-    li.item_effective_location_name_at_check_out,
     li.loan_return_date,
+  --li.claimed_returned_date (not yet in the loans or the loans_items tables)  
+    li.item_status,
+    nn.item_notes,
+    li.item_effective_location_name_at_check_out,
     li.loan_policy_name,
-    li.user_id,
-    li.patron_group_name,
-    li.proxy_user_id,
+    li.permanent_loan_type_name,
     li.current_item_temporary_location_name,
     li.current_item_effective_location_name,
     li.current_item_permanent_location_name,
@@ -39,12 +44,6 @@ SELECT
     li.enumeration,
     li.item_level_call_number,
     li.number_of_pieces,
-    li.permanent_loan_type_name,
-    --li.item_notes (this needs to be added to the derived table)
-    JSON_EXTRACT_PATH_TEXT(uu.data, 'personal', 'firstName') :: VARCHAR AS first_name,
-    JSON_EXTRACT_PATH_TEXT(uu.data, 'personal', 'middleName') :: VARCHAR AS middle_name,
-    JSON_EXTRACT_PATH_TEXT(uu.data, 'personal', 'lastName') :: VARCHAR AS last_name,
-    JSON_EXTRACT_PATH_TEXT(uu.data, 'personal', 'email' :: VARCHAR) AS email,
     ie.volume,
     ie.call_number,
     he.holdings_id,
@@ -53,24 +52,30 @@ SELECT
     he.temporary_location_name,
     he.shelving_title,
     ii.cataloged_date, 
- -- MYSTERY: If the date of publication (line below) is included, we get only one row of data.    
- -- JSON_EXTRACT_PATH_TEXT(JSON_ARRAY_ELEMENTS(JSON_EXTRACT_PATH(ii.data, 'publication')),'dateOfPublication') :: VARCHAR AS date_of_publication,
+	ip.date_of_publication,
+    lrc.num_loans,
+    lrc.num_renewals,
+    li.user_id,
+    li.patron_group_name,
+    li.proxy_user_id,
     JSON_EXTRACT_PATH_TEXT(uu.data, 'personal', 'firstName') :: VARCHAR AS first_name,
     JSON_EXTRACT_PATH_TEXT(uu.data, 'personal', 'middleName') :: VARCHAR AS middle_name,
     JSON_EXTRACT_PATH_TEXT(uu.data, 'personal', 'lastName') :: VARCHAR AS last_name,
-    JSON_EXTRACT_PATH_TEXT(uu.data, 'personal', 'email' :: VARCHAR) AS email,
-    lrc.num_loans,
-    lrc.num_renewals
+    JSON_EXTRACT_PATH_TEXT(uu.data, 'personal', 'email' :: VARCHAR) AS email
 FROM
     folio_reporting.loans_items AS li
 LEFT JOIN public.user_users AS uu
 	ON li.user_id=uu.id
 LEFT JOIN folio_reporting.item_ext AS ie
 	ON li.item_id=ie.item_id
+LEFT JOIN notes AS nn
+	ON li.item_id=nn.item_id
 LEFT JOIN folio_reporting.holdings_ext AS he
 	ON ie.holdings_record_id = he.holdings_id
 LEFT JOIN public.inventory_instances as ii
 	ON he.instance_id=ii.id
+LEFT JOIN folio_reporting.instance_publication as ip
+	ON he.instance_id=ip.instance_id
 LEFT JOIN folio_reporting.loans_renewal_count AS lrc
 	ON li.item_id=lrc.item_id
 WHERE li.current_item_permanent_location_name = (SELECT current_item_permanent_location_filter FROM parameters)
