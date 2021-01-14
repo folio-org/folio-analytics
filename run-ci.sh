8d443641bdff3b9f1593351d8fe013c9dbbccc2d
Nassib Nassar <246609+nassibnassar@users.noreply.github.com>
2021-01-14 00:36:30 -0500

    Clean up debugging code

--- run-ci.sh
+++ run-ci.sh
@@ -3,8 +3,4 @@ set -e
 cd sql/derived_tables
 for f in $( cat runlist.txt ); do
 	psql -bq -v ON_ERROR_STOP=on -d folio_snapshot -f $f
-	#STATUS=$?
-	#if [ $STATUS -ne 0 ]; then
-	#        exit $STATUS
-	#fi
 done
