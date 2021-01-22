af5b09f23345b64587b771adfec6b9d059c81ddc
Axel Dörrer <a.doerrer@ub.uni-mainz.de>
2021-01-22 13:05:56 +0100

    removed WITH clause from derived table po_lines_detail_subscription

--- sql/derived_tables/po_lines_details_subscription.sql
+++ sql/derived_tables/po_lines_details_subscription.sql
@@ -1,25 +1,18 @@
 DROP TABLE IF EXISTS folio_reporting.po_lines_details_subscription;
 
 -- Create a local table for subscription details in po_lines.
+
 CREATE TABLE folio_reporting.po_lines_details_subscription AS
-/* Subquery to extract nested JSON data */
-WITH temp_pol_detail_subs AS (
-    SELECT
-        pol.id AS pol_id,
-        json_extract_path_text(data, 'details', 'subscriptionFrom') ::DATE AS pol_subscription_from,
-        json_extract_path_text(data, 'details', 'subscriptionTo') ::DATE AS pol_subscription_to,
-        json_extract_path_text(data, 'details', 'subscriptionInterval') AS pol_subscription_interval
-    FROM
-        po_lines AS pol
-)
-/* Main query */
 SELECT
-    pol_subs.pol_id,
-    pol_subs.pol_subscription_from AS pol_subscription_from,
-    pol_subs.pol_subscription_to AS pol_subscription_to,
-    pol_subs.pol_subscription_interval AS pol_subscription_interval
+    pol.id AS pol_id,
+    json_extract_path_text(
+        data, 'details', 'subscriptionFrom')::DATE AS pol_subscription_from,
+    json_extract_path_text(
+        data, 'details', 'subscriptionTo')::DATE AS pol_subscription_to,
+    json_extract_path_text(
+        data, 'details', 'subscriptionInterval') AS pol_subscription_interval
 FROM
-    temp_pol_detail_subs AS pol_subs;
+    po_lines AS pol;
 
 CREATE INDEX ON folio_reporting.po_lines_details_subscription (pol_id);
 
