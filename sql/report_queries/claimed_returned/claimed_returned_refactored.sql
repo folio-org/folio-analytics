a11a68e37c48866edae8088da4d3a17dd6ac53ae
Vandana Shah <vp25@cornell.edu>
2021-01-22 09:24:09 -0500

    Add files via upload

--- sql/report_queries/claimed_returned/claimed_returned_refactored.sql
+++ sql/report_queries/claimed_returned/claimed_returned_refactored.sql
@@ -1,4 +1,5 @@
 --Claimed returned refactored using derived tables
+--FILTERS: loan item status, loan date range, location name 
 
 WITH parameters AS (
     SELECT
@@ -11,17 +12,20 @@ WITH parameters AS (
         /* Fill in a location name, OR leave blank for all locations */
         '' :: VARCHAR AS current_item_permanent_location_filter, --Online, Annex, Main Library
         '' :: VARCHAR AS current_item_temporary_location_filter, --Online, Annex, Main Library
-        'Main Library' :: VARCHAR AS current_item_effective_location_filter --Online, Annex, Main Library
+        'Main Library' :: VARCHAR AS current_item_effective_location_filter,--Online, Annex, Main Library
+        '' :: VARCHAR AS current_item_permanent_location_institution_filter, -- 'Københavns Universitet','Montoya College'
+        '' :: VARCHAR AS current_item_permanent_location_campus_filter, -- 'Main Campus','City Campus','Online'
+        '' :: VARCHAR AS current_item_permanent_location_library_filter -- 'Datalogisk Institut','Adelaide Library'
    ),
    notes as (
    		SELECT item_id,
-   		STRING_AGG (note, '|') AS item_notes
+   		STRING_AGG (DISTINCT note, '|'::text) AS item_notes
    		FROM folio_reporting.item_notes
    		GROUP BY item_id
    ),
    publication_d as (
    		SELECT instance_id,
-   		STRING_AGG (date_of_publication, '|') AS dates_of_publication
+   		STRING_AGG (DISTINCT date_of_publication, '|'::text) AS dates_of_publication
    		FROM folio_reporting.instance_publication
    		GROUP BY instance_id
    )
@@ -34,7 +38,7 @@ SELECT
     li.loan_date,
     li.loan_due_date,
     li.loan_return_date,
-  --li.claimed_returned_date (not yet in the loans or the loans_items tables)  
+    JSON_EXTRACT_PATH_TEXT(l.data, 'claimedReturnedDate') :: DATE AS claimed_returned_date,
     li.item_status,
     nn.item_notes,
     li.item_effective_location_name_at_check_out,
@@ -43,6 +47,9 @@ SELECT
     li.current_item_temporary_location_name,
     li.current_item_effective_location_name,
     li.current_item_permanent_location_name,
+    li.current_item_permanent_location_library_name,
+    li.current_item_permanent_location_campus_name,
+    li.current_item_permanent_location_institution_name,
     li.barcode,
     li.material_type_name,
     li.chronology,
@@ -52,8 +59,8 @@ SELECT
     li.number_of_pieces,
     ie.volume,
     ie.call_number,
-    he.holdings_id,
-    he.instance_id,
+  --he.holdings_id,
+  --he.instance_id,
     he.permanent_location_name,
     he.temporary_location_name,
     he.shelving_title,
@@ -61,9 +68,9 @@ SELECT
 	pd.dates_of_publication,
     lrc.num_loans,
     lrc.num_renewals,
-    li.user_id,
+  --li.user_id,
     li.patron_group_name,
-    li.proxy_user_id,
+--  li.proxy_user_id,
     JSON_EXTRACT_PATH_TEXT(uu.data, 'personal', 'firstName') :: VARCHAR AS first_name,
     JSON_EXTRACT_PATH_TEXT(uu.data, 'personal', 'middleName') :: VARCHAR AS middle_name,
     JSON_EXTRACT_PATH_TEXT(uu.data, 'personal', 'lastName') :: VARCHAR AS last_name,
@@ -72,6 +79,8 @@ FROM
     folio_reporting.loans_items AS li
 LEFT JOIN public.user_users AS uu
 	ON li.user_id=uu.id
+LEFT JOIN public.circulation_loans AS l
+	ON li.loan_id=l.id
 LEFT JOIN folio_reporting.item_ext AS ie
 	ON li.item_id=ie.item_id
 LEFT JOIN notes AS nn
@@ -89,6 +98,9 @@ LEFT JOIN folio_reporting.loans_renewal_count AS lrc
 WHERE li.current_item_permanent_location_name = (SELECT current_item_permanent_location_filter FROM parameters)
 		OR  (li.current_item_temporary_location_name = (SELECT current_item_temporary_location_filter FROM parameters))
 		OR  (li.current_item_effective_location_name = (SELECT current_item_effective_location_filter FROM parameters))
+		OR	(li.current_item_permanent_location_library_name = (SELECT current_item_permanent_location_library_filter FROM parameters))
+		OR	(li.current_item_permanent_location_institution_name = (SELECT current_item_permanent_location_institution_filter FROM parameters))
+		OR	(li.current_item_permanent_location_campus_name = (SELECT current_item_permanent_location_campus_filter FROM parameters))
 	AND	li.item_status = (SELECT item_status_filter FROM parameters)  	
 	AND li.loan_date >= (SELECT start_date FROM parameters)
 	AND li.loan_date < (SELECT end_date FROM parameters)
