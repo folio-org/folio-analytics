5b8fca018c701bdc252595cd9e9c1e9e9b3c5942
Nassib Nassar <246609+nassibnassar@users.noreply.github.com>
2021-01-26 16:31:57 -0500

    Update array extraction in instance_editions

--- sql/derived_tables/instance_editions.sql
+++ sql/derived_tables/instance_editions.sql
@@ -4,9 +4,12 @@ CREATE TABLE folio_reporting.instance_editions AS
 SELECT
     instances.id AS instance_id,
     instances.hrid AS instance_hrid,
-    json_array_elements_text(json_extract_path(instances.data, 'editions')) AS edition
+    editions.data #>> '{}' AS edition,
+    editions.ordinality
 FROM
-    inventory_instances AS instances;
+    inventory_instances AS instances
+    CROSS JOIN LATERAL json_array_elements(json_extract_path(data, 'editions'))
+    WITH ORDINALITY AS editions (data);
 
 CREATE INDEX ON folio_reporting.instance_editions (instance_id);
 
