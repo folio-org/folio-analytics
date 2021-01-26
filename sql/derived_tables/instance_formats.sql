acc1491f7ac6800f7ffe4c2d095674ff10dbceb0
Nassib Nassar <246609+nassibnassar@users.noreply.github.com>
2021-01-26 16:52:57 -0500

    Update array extraction in instance_formats

--- sql/derived_tables/instance_formats.sql
+++ sql/derived_tables/instance_formats.sql
@@ -5,14 +5,19 @@ WITH instances AS (
     SELECT
         id,
         hrid,
-        json_array_elements_text(json_extract_path(data, 'instanceFormatIds')) AS instance_format_id
+        instance_format_ids.data #>> '{}' AS instance_format_id,
+        instance_format_ids.ordinality
     FROM
         inventory_instances
+        CROSS JOIN LATERAL json_array_elements(json_extract_path(data, 'instanceFormatIds'))
+        WITH ORDINALITY
+        AS instance_format_ids (data)
 )
 SELECT
     instances.id AS instance_id,
     instances.hrid AS instance_hrid,
     instances.instance_format_id AS format_id,
+    instances.ordinality AS format_ordinality,
     formats.code AS format_code,
     formats.name AS format_name,
     formats.source AS format_source
