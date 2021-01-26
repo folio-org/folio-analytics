6aface140d83084a6a5cfb6cc8b35a782c3c5641
Nassib Nassar <246609+nassibnassar@users.noreply.github.com>
2021-01-26 18:41:13 -0500

    Clarify ordinality column name and add index

--- sql/derived_tables/instance_series.sql
+++ sql/derived_tables/instance_series.sql
@@ -6,7 +6,7 @@ SELECT
     instances.id AS instance_id,
     instances.hrid AS instance_hrid,
     series.data #>> '{}' AS series,
-    series.ordinality
+    series.ordinality AS series_ordinality
 FROM
     inventory_instances AS instances
     CROSS JOIN LATERAL json_array_elements(json_extract_path(data, 'series'))
@@ -17,4 +17,6 @@ CREATE INDEX ON folio_reporting.instance_series (instance_id);
 CREATE INDEX ON folio_reporting.instance_series (instance_hrid);
 
 CREATE INDEX ON folio_reporting.instance_series (series);
+
+CREATE INDEX ON folio_reporting.instance_series (series_ordinality);
 
