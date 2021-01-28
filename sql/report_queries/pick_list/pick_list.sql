5013cbdcb2c44ede23d08b972ed648925644b3ae
jenmawe <jenmawe@gmail.com>
2021-01-28 13:58:07 -0500

    shelf list and refactor pick list

--- sql/report_queries/pick_list/pick_list.sql
+++ sql/report_queries/pick_list/pick_list.sql
@@ -1,6 +1,6 @@
 /* FIELDS TO INCLUDE:
-    Item Discovery Suppression -- no longer in ldp or ldpqdev
-    Holding Discovery Suppression -- no longer in ldp or ldpqdev
+    Item Discovery Suppression
+    Holding Discovery Suppression 
     Instance Discovery Suppression
     Instance Format Name
     Instance Type
@@ -13,12 +13,12 @@
     Contributor/Author (Primary)
     Title
     Index Title
-    Volume -- no longer in ldp or ldpqdev
+    Volume 
     Enumeration
     Chronology
     Copy Number
     Item-Number of Pieces
-    Item-Number of Pieces Missing -- no longer in ldp or ldpqdev
+    Item-Number of Pieces Missing
     Item Barcode
     Item Status
     Identifiers ISBN
@@ -55,36 +55,31 @@ WITH parameters AS (
 location_filtering AS (
     SELECT
         i.id AS i_id,
-        loc.id AS loc_id, -- not used
-        loc."name" AS loc_name,
+        loc.location_id AS loc_id, -- not used
+        loc.location_name AS loc_name,
         loc.library_id AS library_id, -- not used
-        lib."name" AS lib_name,
+        loc.library_name AS lib_name,
         loc.campus_id AS campus_id, -- not used
-        cmp."name" AS camp_name,
+        loc.campus_name AS camp_name,
         loc.institution_id AS institution_id, -- not used
-        ins."name" AS instut_name
+        loc.institution_name AS instut_name
     FROM
         inventory_items AS i
-        LEFT JOIN inventory_locations AS loc
-            ON i.effective_location_id = loc.id
-        LEFT JOIN inventory_libraries AS lib
-            ON loc.library_id = lib.id
-        LEFT JOIN inventory_campuses AS cmp
-            ON loc.campus_id = cmp.id
-        LEFT JOIN inventory_institutions AS ins
-            ON loc.institution_id = ins.id
+        LEFT JOIN folio_reporting.locations_libraries AS loc
+            ON i.effective_location_id = loc.location_id
     WHERE
-        ins."name" = (SELECT institution_filter FROM parameters)
-        OR cmp."name" = (SELECT campus_filter FROM parameters)
-        OR lib."name" = (SELECT library_filter FROM parameters)
-        OR loc."name" = (SELECT location_filter FROM parameters)
+        loc.institution_name = (SELECT institution_filter FROM parameters)
+        OR loc.campus_name = (SELECT campus_filter FROM parameters)
+        OR loc.library_name = (SELECT library_filter FROM parameters)
+        OR loc.location_name = (SELECT location_filter FROM parameters)
 ),
 instance_formats_extract AS (
     SELECT
-        inst.id AS instance_id,
-        JSON_ARRAY_ELEMENTS_TEXT(JSON_EXTRACT_PATH(inst.data, 'instanceFormatIds')) :: VARCHAR AS instance_format_id
+        inst.instance_id AS instance_id,
+        frif.format_id AS instance_format_id
     FROM
-        inventory_instances AS inst
+        folio_reporting.instance_ext AS inst
+        LEFT JOIN folio_reporting.instance_formats AS frif ON inst.instance_id = frif.instance_id 
 ),
 instance_format_names AS (
     SELECT
@@ -97,11 +92,11 @@ instance_format_names AS (
 ),
 identifiers_extract AS (
     SELECT
-        inst.id AS instance_id,
-        JSON_EXTRACT_PATH_TEXT(JSON_ARRAY_ELEMENTS(JSON_EXTRACT_PATH(inst.data, 'identifiers')),'identifierTypeId') :: VARCHAR AS type_id,
-        JSON_EXTRACT_PATH_TEXT(JSON_ARRAY_ELEMENTS(JSON_EXTRACT_PATH(inst.data, 'identifiers')),'value') :: VARCHAR AS value
+        inst.instance_id AS instance_id,
+        inst.identifier_type_id AS type_id,
+        inst.identifier_type_name AS value
     FROM
-        inventory_instances as inst
+        folio_reporting.instance_identifiers as inst
 ),
 isbn_identifiers AS (
     SELECT instance_id, type_id, value
@@ -155,11 +150,11 @@ eff_call_no AS (
 ),
 contributor_extract AS (
     SELECT
-        inst.id AS instance_id,
-        JSON_EXTRACT_PATH_TEXT(JSON_ARRAY_ELEMENTS(JSON_EXTRACT_PATH(inst.data, 'contributors')),'name') :: VARCHAR AS name,
-        JSON_EXTRACT_PATH_TEXT(JSON_ARRAY_ELEMENTS(JSON_EXTRACT_PATH(inst.data, 'contributors')),'primary') :: VARCHAR AS primary
+        inst.instance_id AS instance_id,
+        inst.contributor_name AS name,
+        inst.contributor_primary AS primary
     FROM
-        inventory_instances AS inst
+        folio_reporting.instance_contributors AS inst
 ),
 contributor_extract_name AS (
     SELECT
@@ -177,20 +172,20 @@ copy_number_agg AS (
         STRING_AGG(DISTINCT item_copy_number, '|') AS copy_numbers
     FROM (
         SELECT
-            i.id AS item_id,
-            JSON_ARRAY_ELEMENTS_TEXT(JSON_EXTRACT_PATH(i.data, 'copyNumbers')) :: VARCHAR AS item_copy_number
+            i.item_id AS item_id,
+            i.copy_number AS item_copy_number
         FROM
-            inventory_items AS i
+            folio_reporting.item_ext AS i
     ) AS copy_number_extract
     GROUP BY item_id
 ),
 publisher_extract AS (
     SELECT
-        inst.id AS instance_id,
-        JSON_EXTRACT_PATH_TEXT(JSON_ARRAY_ELEMENTS(JSON_EXTRACT_PATH(inst.data, 'publication')),'dateOfPublication') :: VARCHAR AS date,
-        JSON_EXTRACT_PATH_TEXT(JSON_ARRAY_ELEMENTS(JSON_EXTRACT_PATH(inst.data, 'publication')),'publisher') :: VARCHAR AS name
+        inst.instance_id AS instance_id,
+        inst.date_of_publication AS date,
+        inst.publisher AS name
     FROM
-        inventory_instances AS inst
+        folio_reporting.instance_publication AS inst
 ),
 publisher_names AS (
     SELECT
@@ -216,16 +211,14 @@ SELECT
     inst.title AS title,
     inst.index_title AS index_title,
     contributor_extract_name.contributors,
-    --i.volume AS volume, -- no longer in ldp or ldpqdev, using alternative below
-    JSON_EXTRACT_PATH_TEXT(i.data, 'volume') :: VARCHAR AS volume,
+    i.volume AS volume,
     i.enumeration AS enumeration,
     i.chronology AS chronology,
     i.barcode,
     copy_number_agg.copy_numbers,
-    JSON_EXTRACT_PATH_TEXT(i.data, 'status', 'name') :: VARCHAR AS item_status,
+    i.status_name AS item_status,
     i.number_of_pieces,
-    --i.number_of_missing_pieces, -- no longer in ldp or ldpqdev, using alt. below
-    JSON_EXTRACT_PATH_TEXT(i.data, 'number_of_missing_pieces') :: VARCHAR AS number_of_missing_pieces,
+    i.number_of_missing_pieces, 
     isbn_agg.isbn_values,
     issn_agg.issn_values,
     publisher_names.publisher_name,
@@ -235,44 +228,39 @@ SELECT
     instance_format_names.instance_format_names,
     l_types.name AS item_perm_loan_type,
     loans_lookup.count_loans AS count_loans,
-    --i.id AS item_id, -- just an id, take out of results?
-    --h.id AS holding_id, -- just an id, take out of results?
-    --inst.id AS instance_id, -- just an id, take out of results?
-    --i.discovery_suppress AS item_supp, -- no longer in ldp or ldpqdev, using alt. below
-    JSON_EXTRACT_PATH_TEXT(i.data, 'discovery_suppress') :: VARCHAR AS item_supp,
-    --h.discovery_suppress AS hold_supp, -- no longer in ldp or ldpqdev, using alt. below
-    JSON_EXTRACT_PATH_TEXT(h.data, 'discovery_suppress') :: VARCHAR AS hold_supp,
+    i.discovery_suppress AS item_supp,
+    h.discovery_suppress AS hold_supp,
     inst.discovery_suppress AS inst_supp
 FROM
-    inventory_items AS i
+    folio_reporting.item_ext AS i
     RIGHT JOIN location_filtering
-        ON i.id = location_filtering.i_id
+        ON i.item_id = location_filtering.i_id
     LEFT JOIN copy_number_agg
-        ON i.id = copy_number_agg.item_id
+        ON i.item_id = copy_number_agg.item_id
     LEFT JOIN inventory_material_types AS m_types
         ON i.material_type_id = m_types.id
     LEFT JOIN inventory_loan_types AS l_types
         ON i.permanent_loan_type_id = l_types.id
     LEFT JOIN loans_lookup
-        ON i.id = loans_lookup.item_id
+        ON i.item_id = loans_lookup.item_id
     LEFT JOIN eff_call_no
-        ON i.id = eff_call_no.item_id
-    LEFT JOIN inventory_holdings AS h
-        ON i.holdings_record_id = h.id
-    LEFT JOIN inventory_instances AS inst
-        ON h.instance_id = inst.id
+        ON i.item_id = eff_call_no.item_id
+    LEFT JOIN folio_reporting.holdings_ext AS h
+        ON i.holdings_record_id = h.holdings_id
+    LEFT JOIN folio_reporting.instance_ext AS inst
+        ON h.instance_id = inst.instance_id 
     LEFT JOIN instance_format_names
-        ON inst.id = instance_format_names.instance_id
+        ON inst.instance_id = instance_format_names.instance_id
     LEFT JOIN inventory_instance_types AS inst_types
-        ON inst.instance_type_id = inst_types.id
+        ON inst.type_id = inst_types.id
     LEFT JOIN contributor_extract_name
-        ON inst.id = contributor_extract_name.instance_id
+        ON inst.instance_id = contributor_extract_name.instance_id
     LEFT JOIN isbn_agg
-        ON inst.id = isbn_agg.instance_id
+        ON inst.instance_id = isbn_agg.instance_id
     LEFT JOIN issn_agg
-        ON inst.id = issn_agg.instance_id
+        ON inst.instance_id = issn_agg.instance_id
     LEFT JOIN publisher_names
-        ON inst.id = publisher_names.instance_id
+        ON inst.instance_id = publisher_names.instance_id
     LEFT JOIN publisher_dates
-        ON inst.id = publisher_dates.instance_id
+        ON inst.instance_id = publisher_dates.instance_id
 ;
