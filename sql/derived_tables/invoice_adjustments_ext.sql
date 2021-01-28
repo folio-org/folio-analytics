58edc6f7fe0c49fbfec97e6db06f74333c4c1d9b
Nassib Nassar <246609+nassibnassar@users.noreply.github.com>
2021-01-28 15:03:49 -0500

    Adjust formatting

--- sql/derived_tables/invoice_adjustments_ext.sql
+++ sql/derived_tables/invoice_adjustments_ext.sql
@@ -15,8 +15,8 @@ WITH invl_total AS (
 SELECT
     inv.id AS invoice_id,
     invl.id AS invl_id,
-    COALESCE (invl.total, 0) AS invoice_line_value,
-    COALESCE (invadj.adjustment_value, 0) AS inv_adjust_total_value, --This is the total of the invoice adjustment
+    coalesce(invl.total, 0) AS invoice_line_value,
+    coalesce(invadj.adjustment_value, 0) AS inv_adjust_total_value, --This is the total of the invoice adjustment
     invltotal.invl_total AS "invls_total",
     invadj.adjustment_prorate AS inv_adj_prorate,
     invadj.adjustment_relationtototal AS inv_adj_relationToTotal,
@@ -24,7 +24,7 @@ SELECT
         OR invltotal.invl_total = 0 THEN
         0.0000
     ELSE
-        round(COALESCE (invl.total, 0) / invltotal.invl_total, 4)
+        round(coalesce(invl.total, 0) / invltotal.invl_total, 4)
     END AS ratio_of_inv_adj_per_invoice_line,
     --Above: This is the ratio of the invoice adjustment per invoice line
     CASE WHEN invadj.adjustment_value IS NULL
@@ -32,7 +32,7 @@ SELECT
         OR invltotal.invl_total = 0 THEN
         0.0000
     ELSE
-        round(invadj.adjustment_value * (COALESCE(invl.total, 0) / invltotal.invl_total), 4)
+        round(invadj.adjustment_value * (coalesce(invl.total, 0) / invltotal.invl_total), 4)
     END AS inv_adj_total
     --Above:  This is the adjustment at the invoice line level, taking into consideration the total ratio per invoice line.
 FROM
