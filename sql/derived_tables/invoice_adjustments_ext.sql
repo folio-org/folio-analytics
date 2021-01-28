fd351acc7f50958a7a38281c4aef8faac0432866
NancyBolduc <57685173+NancyBolduc@users.noreply.github.com>
2021-01-28 12:34:25 -0500

    Update invoice_adjustments_ext.sql

--- sql/derived_tables/invoice_adjustments_ext.sql
+++ sql/derived_tables/invoice_adjustments_ext.sql
@@ -5,9 +5,9 @@ CREATE TABLE folio_reporting.invoice_adjustments_ext AS
 WITH invl_total AS (
     SELECT
         inv.id AS inv_id,
-        sum(invl.total) AS invl_total
+        sum(invl.total) ::numeric(12,2) AS invl_total
     FROM
-        INVOICE_INVOICES AS inv
+        invoice_invoices AS inv
         LEFT JOIN invoice_lines AS invl ON inv.id = invl.invoice_id
     GROUP BY
         inv_id
@@ -18,29 +18,31 @@ SELECT
     CASE WHEN invl.total IS NULL THEN
         0
     ELSE
-        invl.total
+        invl.total ::numeric(12,2)
     END AS invoice_line_value,
-    CASE WHEN invadj.adjustment_value::decimal IS NULL THEN
-        0
-    ELSE
-        invadj.adjustment_value::decimal
-    END AS inv_adjust_total_value, --This is the total of the invoice adjustment 
-    invl.total AS "invl_total",
+    COALESCE(invadj.adjustment_value::numeric(12,2),0) AS inv_adjust_total_value, --This is the total of the invoice adjustment 
     invltotal.invl_total AS "invls_total",
     invadj.adjustment_prorate AS inv_adj_prorate,
     invadj.adjustment_relationtototal AS inv_adj_relationToTotal,
-    invl.total::decimal / invltotal.invl_total::decimal AS perc_of_inv_adj_per_invoice_line, --This is the percentage of the invoice adjustment per invoice line
-    CASE WHEN invl.total IS NULL THEN
-        0
-    ELSE
-        invadj.adjustment_value::decimal * (
-            invl.total ::decimal / invltotal.invl_total)
+        CASE WHEN invltotal.invl_total IS NULL OR invltotal.invl_total = 0 THEN 0
+            ELSE
+                COALESCE(invl.total, 0) / invltotal.invl_total 
+                END
+
+                AS perc_of_inv_adj_per_invoice_line, --This is the percentage of the invoice adjustment per invoice line
+    
+    
+        CASE WHEN invltotal.invl_total IS NULL OR invltotal.invl_total = 0 THEN 0
+      
+        ELSE
+            invadj.adjustment_value::numeric(12,2) * (
+            COALESCE(invl.total, 0)::numeric(12,2) / invltotal.invl_total)
     END AS inv_adj_total
     --Above:  This is the adjustment at the invoice line level, taking into consideration the total percentage per invoice line.
 FROM
     invoice_invoices AS inv
     LEFT JOIN invoice_lines AS invl ON invl.invoice_id = inv.id
-    LEFT JOIN folio_reporting.invoice_adjustment_in_addition_to AS invadj ON invadj.invoice_id = inv.id
+    LEFT JOIN folio_reporting.invoice_adjustments_in_addition_to AS invadj ON invadj.invoice_id = inv.id
     LEFT JOIN invl_total AS invltotal ON inv.id = invltotal.inv_id
 GROUP BY
     inv.id,
@@ -58,8 +60,6 @@ CREATE INDEX ON folio_reporting.invoice_adjustments_ext (invl_id);
 CREATE INDEX ON folio_reporting.invoice_adjustments_ext (invoice_line_value);
 
 CREATE INDEX ON folio_reporting.invoice_adjustments_ext (inv_adjust_total_value);
-
-CREATE INDEX ON folio_reporting.invoice_adjustments_ext (invl_total);
 
 CREATE INDEX ON folio_reporting.invoice_adjustments_ext (invls_total);
 
