e5984a02974672ecaa6c13fd52f07ee06b1a1ef0
Nassib Nassar <246609+nassibnassar@users.noreply.github.com>
2021-01-28 14:37:37 -0500

    Use ratio instead of percentage

--- sql/derived_tables/invoice_adjustments_ext.sql
+++ sql/derived_tables/invoice_adjustments_ext.sql
@@ -1,6 +1,6 @@
 DROP TABLE IF EXISTS folio_reporting.invoice_adjustments_ext;
 
--- This table includes the percentage of the invoice lines amount in relation to the invoice_line total to calculate the invoice adjustment by row
+-- This table includes the ratio of the invoice lines amount in relation to the invoice_line total to calculate the invoice adjustment by row
 CREATE TABLE folio_reporting.invoice_adjustments_ext AS
 WITH invl_total AS (
     SELECT
@@ -25,15 +25,15 @@ SELECT
         0
     ELSE
         COALESCE (invl.total, 0) / invltotal.invl_total
-    END AS perc_of_inv_adj_per_invoice_line,
-    --Above: This is the percentage of the invoice adjustment per invoice line
+    END AS ratio_of_inv_adj_per_invoice_line,
+    --Above: This is the ratio of the invoice adjustment per invoice line
     CASE WHEN invltotal.invl_total IS NULL
         OR invltotal.invl_total = 0 THEN
         0
     ELSE
         invadj.adjustment_value * (COALESCE(invl.total, 0) / invltotal.invl_total)
     END AS inv_adj_total
-    --Above:  This is the adjustment at the invoice line level, taking into consideration the total percentage per invoice line.
+    --Above:  This is the adjustment at the invoice line level, taking into consideration the total ratio per invoice line.
 FROM
     invoice_invoices AS inv
     LEFT JOIN invoice_lines AS invl ON json_extract_path_text(invl.data, 'invoiceId') = inv.id
@@ -62,7 +62,7 @@ CREATE INDEX ON folio_reporting.invoice_adjustments_ext (inv_adj_prorate);
 
 CREATE INDEX ON folio_reporting.invoice_adjustments_ext (inv_adj_relationToTotal);
 
-CREATE INDEX ON folio_reporting.invoice_adjustments_ext (perc_of_inv_adj_per_invoice_line);
+CREATE INDEX ON folio_reporting.invoice_adjustments_ext (ratio_of_inv_adj_per_invoice_line);
 
 CREATE INDEX ON folio_reporting.invoice_adjustments_ext (inv_adj_total);
 
