3e2de170381975666d1d5ba730c7928709fdd71a
Axel Dörrer <a.doerrer@ub.uni-mainz.de>
2021-03-15 09:18:32 +0100

    Used json_extrac functions for fields that might not be there to not fail the query when used in a runlist script

--- sql/derived_tables/invoice_transactions.sql
+++ sql/derived_tables/invoice_transactions.sql
@@ -13,27 +13,27 @@ SELECT
     ft.id AS transaction_id,
     ft.amount AS transaction_amount,
     ft.currency AS transaction_currency,
-    ft.expense_class_id AS transaction_expense_class_id,
+    json_extract_path_text(ft.data, 'expenseClassId') AS transaction_expense_class_id,
     ft.fiscal_year_id AS transaction_fiscal_year_id,
     ft.from_fund_id AS transaction_from_fund_id,
     ff.name AS transaction_from_fund_name,
     ff.code AS transaction_from_fund_code,
     fb.id AS transaction_from_budget_id,
     fb.name AS transaction_from_budget_name,
-    ft.source_invoice_id AS invoice_id,
-    ft.source_invoice_line_id AS invoice_line_id,
+    json_extract_path_text(ft.data, 'sourceInvoiceId') AS invoice_id,
+    json_extract_path_text(ft.data, 'sourceInvoiceLineId') AS invoice_line_id,
     ft.transaction_type AS transaction_type,
     ii.invoice_date AS invoice_date,
-    ii.payment_date AS invoice_payment_date,
-    ii.exchange_rate AS invoice_exchange_rate,
-    il.total AS invoice_line_total,
+    json_extract_path_text(ii.data, 'paymentDate') AS invoice_payment_date,
+    json_extract_path_text(ii.data, 'exchangeRate') AS invoice_exchange_rate,
+    json_extract_path_text(il.data, 'total') AS invoice_line_total,
     ii.currency AS invoice_currency,
-    il.po_line_id AS po_line_id,
+    json_extract_path_text(il.data, 'poLineId') AS po_line_id,
     ii.vendor_id AS invoice_vendor_id
 FROM
     finance_transactions AS ft
-    LEFT JOIN invoice_invoices AS ii ON ft.source_invoice_id = ii.id
-    LEFT JOIN invoice_lines AS il ON ft.source_invoice_line_id = il.id
+    LEFT JOIN invoice_invoices AS ii ON json_extract_path_text(ft.data, 'sourceInvoiceId') = ii.id
+    LEFT JOIN invoice_lines AS il ON json_extract_path_text(ft.data, 'sourceInvoiceLineId') = il.id
     LEFT JOIN finance_funds AS ff ON ft.from_fund_id = ff.id
     LEFT JOIN finance_budgets AS fb ON ft.from_fund_id = fb.fund_id
 WHERE
