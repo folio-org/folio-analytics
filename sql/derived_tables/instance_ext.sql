598bfd50350e9fed5264c1d24cd45cbbfd2687e1
jenmawe <jenmawe@gmail.com>
2021-03-18 10:57:32 -0400

    updated derived table instance_ext

--- sql/derived_tables/instance_ext.sql
+++ sql/derived_tables/instance_ext.sql
@@ -7,7 +7,7 @@ SELECT
     instance.id AS instance_id,
     instance.hrid AS instance_hrid,
     json_extract_path_text(instance.data, 'catalogedDate') AS cataloged_date,
-    instance.discovery_suppress,
+    instance.title,
     instance.index_title,
     instance.instance_type_id AS type_id,
     instance_type.name AS type_name,
@@ -15,10 +15,15 @@ SELECT
     mode_of_issuance.name AS mode_of_issuance_name,
     json_extract_path_text(instance.data, 'previouslyHeld')::boolean AS previously_held,
     instance.source AS instance_source,
-    instance.staff_suppress,
+    json_extract_path_text(INSTANCE.DATA, 'discovery_suppress')::boolean AS discovery_suppress,
+    json_extract_path_text(INSTANCE.DATA, 'staff_suppress')::boolean AS staff_suppress,
     json_extract_path_text(instance.data, 'statusId') AS status_id,
     instance_status.name AS status_name,
-    instance.title
+    instance.status_updated_date,
+    json_extract_path_text(instance.data, 'source') AS record_source,
+    json_extract_path_text(instance.data, 'metadata', 'createdDate') AS record_created_date,
+    json_extract_path_text(instance.data, 'metadata', 'updatedByUserId') AS updated_by_user_id,
+    json_extract_path_text(instance.data, 'metadata', 'updatedDate') AS updated_date
 FROM
     inventory_instances AS instance
     LEFT JOIN inventory_instance_types AS instance_type ON instance.instance_type_id = instance_type.id
@@ -31,7 +36,7 @@ CREATE INDEX ON folio_reporting.instance_ext (instance_hrid);
 
 CREATE INDEX ON folio_reporting.instance_ext (cataloged_date);
 
-CREATE INDEX ON folio_reporting.instance_ext (discovery_suppress);
+CREATE INDEX ON folio_reporting.instance_ext (title);
 
 CREATE INDEX ON folio_reporting.instance_ext (index_title);
 
@@ -47,11 +52,20 @@ CREATE INDEX ON folio_reporting.instance_ext (previously_held);
 
 CREATE INDEX ON folio_reporting.instance_ext (instance_source);
 
+CREATE INDEX ON folio_reporting.instance_ext (discovery_suppress);
+
 CREATE INDEX ON folio_reporting.instance_ext (staff_suppress);
 
 CREATE INDEX ON folio_reporting.instance_ext (status_id);
 
 CREATE INDEX ON folio_reporting.instance_ext (status_name);
 
-CREATE INDEX ON folio_reporting.instance_ext (title);
+CREATE INDEX ON folio_reporting.instance_ext (status_updated_date);
 
+CREATE INDEX ON folio_reporting.instance_ext (record_source);
+
+CREATE INDEX ON folio_reporting.instance_ext (record_created_date);
+
+CREATE INDEX ON folio_reporting.instance_ext (updated_by_user_id);
+
+CREATE INDEX ON folio_reporting.instance_ext (updated_date);
