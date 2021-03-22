73dd3388159a4d5bfd67f9a11596de1df1e1f879
Angela Zoss <az49@duke.edu>
2021-03-22 17:23:31 -0400

    adding loans_renewal_dates derived table query for LDP

--- sql/derived_tables/loans_renewal_dates.sql
+++ sql/derived_tables/loans_renewal_dates.sql
@@ -4,29 +4,40 @@
  * or to validate the results. When we move to metadb, will probably want to use
  * metadb's native history functionality instead. */
   
--- loan with renewal history in folio_snapshot: 2927e07a-322b-459b-a35e-b6aa231e24e2
--- can't find loans with renewal history in reports_dev, so will need to add data
-
--- write first version against folio_snapshot, since this is
--- scheduled for milestone 1.1
-
 -- Questions:
 -- Do we want to count "renewedThroughOverride" as a renewal for circulation reports?
 -- like, for annual statistics, would that count as a renewal? It does
 -- increment renewal count.
 
-SELECT
-    id AS loan_history_id,
-    created_date AS loan_action_date,
-    json_extract_path_text(data, 'loan', 'id') AS loan_id,
-    json_extract_path_text(data, 'loan', 'itemId') AS item_id,
-    json_extract_path_text(data, 'loan', 'action') AS loan_action,
-    json_extract_path_text(data, 'loan', 'renewalCount') AS loan_renewal_count,
-    json_extract_path_text(data, 'loan', 'status', 'name') AS loan_status
-FROM public.circulation_loan_history
-WHERE
-    json_extract_path_text(data, 'loan', 'action') IN ('renewed', 'renewedThroughOverride')
-ORDER BY
-    loan_id,
-    action_date
+DROP TABLE IF EXISTS folio_reporting.loans_renewal_dates;
+
+CREATE TABLE folio_reporting.loans_renewal_dates AS
+    SELECT
+        id AS loan_history_id,
+        created_date AS loan_action_date,
+        json_extract_path_text(data, 'loan', 'id') AS loan_id,
+        json_extract_path_text(data, 'loan', 'itemId') AS item_id,
+        json_extract_path_text(data, 'loan', 'action') AS loan_action,
+        json_extract_path_text(data, 'loan', 'renewalCount') AS loan_renewal_count,
+        json_extract_path_text(data, 'loan', 'status', 'name') AS loan_status
+    FROM public.circulation_loan_history
+    WHERE
+        json_extract_path_text(data, 'loan', 'action') IN ('renewed', 'renewedThroughOverride')
+    ORDER BY
+        loan_id,
+        loan_action_date
 ;
+
+CREATE INDEX ON folio_reporting.loans_renewal_dates (loan_history_id);
+
+CREATE INDEX ON folio_reporting.loans_renewal_dates (loan_action_date);
+
+CREATE INDEX ON folio_reporting.loans_renewal_dates (loan_id);
+
+CREATE INDEX ON folio_reporting.loans_renewal_dates (item_id);
+
+CREATE INDEX ON folio_reporting.loans_renewal_dates (loan_action);
+
+CREATE INDEX ON folio_reporting.loans_renewal_dates (loan_renewal_count);
+
+CREATE INDEX ON folio_reporting.loans_renewal_dates (loan_status);
