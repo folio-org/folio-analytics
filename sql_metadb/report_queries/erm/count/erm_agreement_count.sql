5b9660c802667428209348484c238eb061d0fa4f
Axel Dörrer <a.doerrer@ub.uni-mainz.de>
2021-04-28 16:40:40 +0200

    Added pci_removed_ts to derived table to filter titles removed from packages

--- sql_metadb/report_queries/erm/count/erm_agreement_count.sql
+++ sql_metadb/report_queries/erm/count/erm_agreement_count.sql
@@ -21,17 +21,16 @@ WITH parameters AS (
         --'2022-01-01' :: DATE AS sa_end_date, -- end date day is NOT included in interval -> enter next day		
 		-- filters on erm_resource level
 		''::VARCHAR AS resource_type, -- Enter your erm resource type eg. 'monograph', 'serial' etc.
- -- As there is an issue on the field type for dates in MetaDB date filters are commented out and will be reimplemented when solved.
 		-- filters on entitelment level
-        -- NULL::DATE AS ent_start_date,
-        -- NULL::DATE AS ent_end_date,
+        NULL::DATE AS ent_start_date,
+        NULL::DATE AS ent_end_date,
         --'2021-01-01' :: DATE AS ent_start_date, -- start date day is included in interval
-        --'2022-01-01' :: DATE AS ent_end_date, -- end date day is NOT included in interval -> enter next day	
+        --'2022-01-01' :: DATE AS ent_end_date, -- end date day is NOT included in interval -> enter next day
 		-- filters on package content item level
-        -- NULL::DATE AS pci_start_date,
-        -- NULL::DATE AS pci_end_date
+        NULL::DATE AS pci_start_date,
+        NULL::DATE AS pci_end_date
         --'2021-01-01' :: DATE AS pci_start_date, -- start date day is included in interval
-        --'2022-01-01' :: DATE AS pci_end_date, -- end date day is NOT included in interval -> enter next day		
+        --'2022-01-01' :: DATE AS pci_end_date, -- end date day is NOT included in interval -> enter next day
 )
 
 SELECT
@@ -43,24 +42,27 @@ FROM
     LEFT JOIN folio_derived.agreements_subscription_agreement_entitlement AS sa_ent ON pci_list.entitlement_id = sa_ent.entitlement_id
     LEFT JOIN folio_agreements.refdata_value AS agrestat ON agrestat.rdv_id = sa_ent.subscription_agreement_status
 
-WHERE 
-	((agrestat.rdv_label = (SELECT agreement_status FROM parameters)) OR 
-		((SELECT agreement_status FROM parameters) = '')
-/*	
- -- As there is an issue on the field type for dates in MetaDB date filters are commented out and will be reimplemented when solved.
-	AND 			
+WHERE
+ -- Don't count removed titles from packages
+    pci_list.pci_removed_ts IS NULL
+ -- Start with parameter filters
+    AND
+	((agrestat.rdv_label = (SELECT agreement_status FROM parameters)) OR
+		((SELECT agreement_status FROM parameters) = ''))
+	AND
     ((pci_list.pci_access_start < (SELECT pci_end_date FROM parameters) AND
 	    (pci_list.pci_access_end >= (SELECT pci_start_date FROM parameters) OR pci_list.pci_access_end IS NULL))
-	    OR 
-		(((SELECT pci_start_date FROM parameters) IS NULL) 
+	    OR
+		(((SELECT pci_start_date FROM parameters) IS NULL)
 			OR ((SELECT pci_end_date FROM parameters) IS NULL)))
-	AND 			
+	AND
     ((sa_ent.entitlement_active_from < (SELECT ent_end_date FROM parameters) AND
 	    (sa_ent.entitlement_active_to >= (SELECT ent_start_date FROM parameters) OR sa_ent.entitlement_active_to IS NULL))
-	    OR 
-		(((SELECT ent_start_date FROM parameters) IS NULL) 
-			OR ((SELECT ent_end_date FROM parameters) IS NULL)))	
-	-- subscription agreement time period will be added when available (IRIS)	
+	    OR
+		(((SELECT ent_start_date FROM parameters) IS NULL)
+			OR ((SELECT ent_end_date FROM parameters) IS NULL)))
+	-- subscription agreement time period will be added when available (IRIS)
+	/*
 	AND 			
     ((sa_ent.sa_start_date < (SELECT a_end_date FROM parameters) AND
 	    (sa_ent.sa_end_date >= (SELECT a_start_date FROM parameters) OR sa_ent.sa_end_date IS NULL))
