4e6053fab229590b578e123769e9bba02e6b88b0
Axel Dörrer <a.doerrer@ub.uni-mainz.de>
2021-09-24 19:00:15 +0200

    Magnified derived table and reordered fields alphabetical

--- sql_metadb/derived_tables/agreements_package_content_item.sql
+++ sql_metadb/derived_tables/agreements_package_content_item.sql
@@ -47,47 +47,47 @@ SELECT
     pci_list.res_publication_type_category
 FROM (
     SELECT
+        ent.ent_id AS entitlement_id,
+        id.id_id AS identifier_id,
+        id.id_value AS identifier_value,
+        id.id_ns_fk AS identifier_namespace_id,
+        idns.idns_value AS identifiernamespace_name,
+        org.org_name AS org_vendor_name,
+        pack.pkg_reference AS package_reference,
+        pack.pkg_remote_kb AS package_remote_kb_id,
+        pack.pkg_source AS package_source,
+        pack.pkg_vendor_fk AS package_vendor_id,
         pci.id AS pci_id,
         pci.pci_access_start,
         pci.pci_access_end,
         pci.pci_pkg_fk AS pci_package_id,
         pci.pci_removed_ts,
-        pack.pkg_source AS package_source,
-        pack.pkg_vendor_fk AS package_vendor_id,
-        org.org_name AS org_vendor_name,
-        pack.pkg_remote_kb AS package_remote_kb_id,
-        remotekb.rkb_name AS remotekb_remote_kb_name,
-        pack.pkg_reference AS package_reference,
         pci.pci_pti_fk AS pci_platform_title_instance_id,
-        pti.pti_pt_fk AS pti_platform_id,
         pt.pt_name AS pt_platform_name,
+        pti.pti_pt_fk AS pti_platform_id,
         pti.pti_ti_fk AS pti_title_instance_id,
         pti.pti_url,
-        ti.id AS ti_id,
-        ti.ti_work_fk AS ti_work_id,
-        ti.ti_date_monograph_published,
-        ti.ti_first_author,
-        ti.ti_monograph_edition,
-        ti.ti_monograph_volume,
-        ti.ti_first_editor,
-        id.id_id AS identifier_id,
-        id.id_value AS identifier_value,
-        id.id_ns_fk AS identifier_namespace_id,
-        idns.idns_value AS identifiernamespace_name,
-        ent.ent_id AS entitlement_id,
+        remotekb.rkb_name AS remotekb_remote_kb_name,
         res.res_name,
+        res.res_publication_type_fk,
         res.res_sub_type_fk,
+        res.res_type_fk,
+        rpub.rdv_value AS res_publication_type_value,
+        rpub.rdv_label AS res_publication_type_label,
+        rpubc.rdc_description AS res_publication_type_category,
         rst.rdv_value AS res_sub_type_value,
         rst.rdv_label AS res_sub_type_label,
         rstc.rdc_description AS res_sub_type_category,
-        res.res_type_fk,
         rt.rdv_value AS res_type_value,
         rt.rdv_label AS res_type_label,
         rtc.rdc_description AS res_type_category,
-        res.res_publication_type_fk,
-        rpub.rdv_value AS res_publication_type_value,
-        rpub.rdv_label AS res_publication_type_label,
-        rpubc.rdc_description AS res_publication_type_category
+        ti.id AS ti_id,
+        ti.ti_date_monograph_published,
+        ti.ti_first_author,
+        ti.ti_first_editor,
+        ti.ti_monograph_edition,
+        ti.ti_monograph_volume,
+        ti.ti_work_fk AS ti_work_id
     FROM
         folio_agreements.package_content_item AS pci
         INNER JOIN folio_agreements.entitlement AS ent ON pci.id = ent.ent_resource_fk
@@ -108,67 +108,67 @@ FROM (
         LEFT JOIN folio_agreements.refdata_value AS rpub ON res.res_publication_type_fk = rpub.rdv_id
         LEFT JOIN folio_agreements.refdata_category AS rpubc ON rpub.rdv_owner = rpubc.rdc_id
 UNION
-SELECT
-    pci.id AS pci_id,
-    pci.pci_access_start,
-    pci.pci_access_end,
-    pci.pci_pkg_fk AS pci_package_id,
-    pci.pci_removed_ts,
-    pack.pkg_source AS package_source,
-    pack.pkg_vendor_fk AS package_vendor_id,
-    org.org_name AS org_vendor_name,
-    pack.pkg_remote_kb AS package_remote_kb_id,
-    remotekb.rkb_name AS remotekb_remote_kb_name,
-    pack.pkg_reference AS package_reference,
-    pci.pci_pti_fk AS pci_platform_title_instance_id,
-    pti.pti_pt_fk AS pti_platform_id,
-    pt.pt_name AS pt_platform_name,
-    pti.pti_ti_fk AS pti_title_instance_id,
-    pti.pti_url,
-    ti.id AS ti_id,
-    ti.ti_work_fk AS ti_work_id,
-    ti.ti_date_monograph_published,
-    ti.ti_first_author,
-    ti.ti_monograph_edition,
-    ti.ti_monograph_volume,
-    ti.ti_first_editor,
-    id.id_id AS identifier_id,
-    id.id_value AS identifier_value,
-    id.id_ns_fk AS identifier_namespace_id,
-    idns.idns_value AS identifiernamespace_name,
-    ent.ent_id AS entitlement_id,
-    res.res_name,
-    res.res_sub_type_fk,
-    rst.rdv_value AS res_sub_type_value,
-    rst.rdv_label AS res_sub_type_label,
-    rstc.rdc_description AS res_sub_type_category,
-    res.res_type_fk,
-    rt.rdv_value AS res_type_value,
-    rt.rdv_label AS res_type_label,
-    rtc.rdc_description AS res_type_category,
-    res.res_publication_type_fk,
-    rpub.rdv_value AS res_publication_type_value,
-    rpub.rdv_label AS res_publication_type_label,
-    rpubc.rdc_description AS res_publication_type_category
-FROM
-    folio_agreements.package_content_item AS pci
-    LEFT JOIN folio_agreements.package AS pack ON pci.pci_pkg_fk = pack.id
-    INNER JOIN folio_agreements.entitlement AS ent ON ent.ent_resource_fk = pack.id
-    LEFT JOIN folio_agreements.org AS org ON pack.pkg_vendor_fk = org.org_id
-    LEFT JOIN folio_agreements.remotekb AS remotekb ON pack.pkg_remote_kb = remotekb.rkb_id
-    LEFT JOIN folio_agreements.platform_title_instance AS pti ON pci.pci_pti_fk = pti.id
-    LEFT JOIN folio_agreements.platform AS pt ON pti.pti_pt_fk = pt.pt_id
-    LEFT JOIN folio_agreements.title_instance AS ti ON pti.pti_ti_fk = ti.id
-    LEFT JOIN folio_agreements.identifier_occurrence AS oc ON ti.id = oc.io_ti_fk
-    LEFT JOIN folio_agreements.identifier AS id ON oc.io_identifier_fk = id.id_id
-    LEFT JOIN folio_agreements.identifier_namespace AS idns ON id.id_ns_fk = idns.idns_id
-    LEFT JOIN folio_agreements.erm_resource AS res ON res.id = pti.pti_ti_fk
-    LEFT JOIN folio_agreements.refdata_value AS rst ON res.res_sub_type_fk = rst.rdv_id
-    LEFT JOIN folio_agreements.refdata_category AS rstc ON rst.rdv_owner = rstc.rdc_id
-    LEFT JOIN folio_agreements.refdata_value AS rt ON res.res_type_fk = rt.rdv_id
-    LEFT JOIN folio_agreements.refdata_category AS rtc ON rt.rdv_owner = rtc.rdc_id
-    LEFT JOIN folio_agreements.refdata_value AS rpub ON res.res_publication_type_fk = rpub.rdv_id
-    LEFT JOIN folio_agreements.refdata_category AS rpubc ON rpub.rdv_owner = rpubc.rdc_id) AS pci_list;
+    SELECT
+        ent.ent_id AS entitlement_id,
+        id.id_id AS identifier_id,
+        id.id_value AS identifier_value,
+        id.id_ns_fk AS identifier_namespace_id,
+        idns.idns_value AS identifiernamespace_name,
+        org.org_name AS org_vendor_name,
+        pack.pkg_reference AS package_reference,
+        pack.pkg_remote_kb AS package_remote_kb_id,
+        pack.pkg_source AS package_source,
+        pack.pkg_vendor_fk AS package_vendor_id,
+        pci.id AS pci_id,
+        pci.pci_access_start,
+        pci.pci_access_end,
+        pci.pci_pkg_fk AS pci_package_id,
+        pci.pci_removed_ts,
+        pci.pci_pti_fk AS pci_platform_title_instance_id,
+        pt.pt_name AS pt_platform_name,
+        pti.pti_pt_fk AS pti_platform_id,
+        pti.pti_ti_fk AS pti_title_instance_id,
+        pti.pti_url,
+        remotekb.rkb_name AS remotekb_remote_kb_name,
+        res.res_name,
+        res.res_publication_type_fk,
+        res.res_sub_type_fk,
+        res.res_type_fk,
+        rpub.rdv_value AS res_publication_type_value,
+        rpub.rdv_label AS res_publication_type_label,
+        rpubc.rdc_description AS res_publication_type_category,
+        rst.rdv_value AS res_sub_type_value,
+        rst.rdv_label AS res_sub_type_label,
+        rstc.rdc_description AS res_sub_type_category,
+        rt.rdv_value AS res_type_value,
+        rt.rdv_label AS res_type_label,
+        rtc.rdc_description AS res_type_category,
+        ti.id AS ti_id,
+        ti.ti_date_monograph_published,
+        ti.ti_first_author,
+        ti.ti_first_editor,
+        ti.ti_monograph_edition,
+        ti.ti_monograph_volume,
+        ti.ti_work_fk AS ti_work_id
+    FROM
+        folio_agreements.package_content_item AS pci
+        LEFT JOIN folio_agreements.package AS pack ON pci.pci_pkg_fk = pack.id
+        INNER JOIN folio_agreements.entitlement AS ent ON ent.ent_resource_fk = pack.id
+        LEFT JOIN folio_agreements.org AS org ON pack.pkg_vendor_fk = org.org_id
+        LEFT JOIN folio_agreements.remotekb AS remotekb ON pack.pkg_remote_kb = remotekb.rkb_id
+        LEFT JOIN folio_agreements.platform_title_instance AS pti ON pci.pci_pti_fk = pti.id
+        LEFT JOIN folio_agreements.platform AS pt ON pti.pti_pt_fk = pt.pt_id
+        LEFT JOIN folio_agreements.title_instance AS ti ON pti.pti_ti_fk = ti.id
+        LEFT JOIN folio_agreements.identifier_occurrence AS oc ON ti.id = oc.io_ti_fk
+        LEFT JOIN folio_agreements.identifier AS id ON oc.io_identifier_fk = id.id_id
+        LEFT JOIN folio_agreements.identifier_namespace AS idns ON id.id_ns_fk = idns.idns_id
+        LEFT JOIN folio_agreements.erm_resource AS res ON res.id = pti.pti_ti_fk
+        LEFT JOIN folio_agreements.refdata_value AS rst ON res.res_sub_type_fk = rst.rdv_id
+        LEFT JOIN folio_agreements.refdata_category AS rstc ON rst.rdv_owner = rstc.rdc_id
+        LEFT JOIN folio_agreements.refdata_value AS rt ON res.res_type_fk = rt.rdv_id
+        LEFT JOIN folio_agreements.refdata_category AS rtc ON rt.rdv_owner = rtc.rdc_id
+        LEFT JOIN folio_agreements.refdata_value AS rpub ON res.res_publication_type_fk = rpub.rdv_id
+        LEFT JOIN folio_agreements.refdata_category AS rpubc ON rpub.rdv_owner = rpubc.rdc_id) AS pci_list;
 
 CREATE INDEX ON folio_derived.agreements_package_content_item (pci_id);
 
