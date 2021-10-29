6e0a9f7cb5f8ee4afe6c7e2ab5168db23543f56f
Axel Dörrer <a.doerrer@ub.uni-mainz.de>
2021-10-29 09:07:36 +0200

    Converted derived table queries to work with metadb

--- sql_metadb/derived_tables/holdings_ext.sql
+++ sql_metadb/derived_tables/holdings_ext.sql
@@ -7,63 +7,61 @@ CREATE TABLE folio_derived.holdings_ext AS
 WITH holdings AS (
     SELECT
         h.id,
-        h.hrid,
-        h.acquisition_method,
-        h.call_number,
-        -- call_number_prefix,
-        -- call_number_suffix,
-        h.call_number_type_id,
-        h.copy_number,
-        h.holdings_type_id,
-        h.ill_policy_id,
-        h.instance_id,
-        h.permanent_location_id,
-        h.receipt_status,
-        h.retention_policy,
-        h.shelving_title,
-        -- discovery_suppress,
-	md.created_date,
-	md.updated_by_user_id,
-	md.updated_date,
-        -- temporary_location_id
-	'' AS temporary_location_id  -- temporary
+        json_extract_path_text(h.jsonb, 'hrid') AS hrid,
+        json_extract_path_text(h.jsonb, 'acquisitionMethod') AS acquisition_method,
+        json_extract_path_text(h.jsonb, 'callNumber') AS call_number,
+        json_extract_path_text(h.jsonb, 'callNumberPrefix') AS call_number_prefix,
+        json_extract_path_text(h.jsonb, 'callNumberSuffix') AS call_number_suffix,
+        json_extract_path_text(h.jsonb, 'callNumberTypeId') AS call_number_type_id,
+        json_extract_path_text(h.jsonb, 'copyNumber') AS copy_number,
+        json_extract_path_text(h.jsonb, 'holdingsTypeId') AS holdings_type_id,
+        json_extract_path_text(h.jsonb, 'illPolicyId') AS ill_policy_id,
+        json_extract_path_text(h.jsonb, 'instanceId') AS instance_id,
+        json_extract_path_text(h.jsonb, 'permanentLocationId') AS permanent_location_id,
+        json_extract_path_text(h.jsonb, 'receiptStatus') AS receipt_status,
+        json_extract_path_text(h.jsonb, 'retentionPolicy') AS retention_policy,
+        json_extract_path_text(h.jsonb, 'shelvingTitle') AS shelving_title,
+        json_extract_path_text(h.jsonb, 'discoverySuppress') AS discovery_suppress,
+        json_extract_path_text(h.jsonb, 'metadata', 'createdDate') AS created_date,
+        json_extract_path_text(h.jsonb, 'metadata', 'createdByUserId') AS updated_by_user_id,
+        json_extract_path_text(h.jsonb, 'metadata', 'updatedDate') AS updated_date,
+        json_extract_path_text(h.jsonb, 'temporaryLocationId') AS temporary_location_id
     FROM
-        folio_inventory.holdings_record_j AS h
-	JOIN folio_inventory.holdings_record_j_metadata AS md ON h.id = md.id
+        folio_inventory.holdings_record AS h
 )
 SELECT
     holdings.id AS holdings_id,
     holdings.hrid AS holdings_hrid,
     holdings.acquisition_method,
     holdings.call_number,
-    -- holdings.call_number_prefix,
-    -- holdings.call_number_suffix,
+    holdings.call_number_prefix,
+    holdings.call_number_suffix,
     holdings.call_number_type_id,
-    holdings_call_number_type.name AS call_number_type_name,
+    json_extract_path_text(holdings_call_number_type.jsonb, 'name') AS call_number_type_name,
     holdings.copy_number,
     holdings.holdings_type_id AS type_id,
-    holdings_type.name AS type_name,
+    json_extract_path_text(holdings_type.jsonb, 'name') AS type_name,
     holdings.ill_policy_id,
-    holdings_ill_policy.name AS ill_policy_name,
+    json_extract_path_text(holdings_ill_policy.jsonb, 'name') AS ill_policy_name,
     holdings.instance_id,
     holdings.permanent_location_id,
-    holdings_permanent_location.name AS permanent_location_name,
+    json_extract_path_text(holdings_permanent_location.jsonb, 'name') AS permanent_location_name,
     holdings.temporary_location_id,
-    holdings_temporary_location.name AS temporary_location_name,
+    json_extract_path_text(holdings_temporary_location.jsonb, 'name') AS temporary_location_name,
     holdings.receipt_status,
     holdings.retention_policy,
     holdings.shelving_title,
-    -- holdings.discovery_suppress,
+    holdings.discovery_suppress,
     holdings.created_date,
     holdings.updated_by_user_id,
     holdings.updated_date
 FROM
     holdings
-    LEFT JOIN folio_inventory.holdings_type_j AS holdings_type ON holdings.holdings_type_id = holdings_type.id
-    LEFT JOIN folio_inventory.ill_policy_j AS holdings_ill_policy ON holdings.ill_policy_id = holdings_ill_policy.id
-    LEFT JOIN folio_inventory.call_number_type_j AS holdings_call_number_type ON holdings.call_number_type_id = holdings_call_number_type.id
-    LEFT JOIN folio_inventory.location_j AS holdings_permanent_location ON holdings.permanent_location_id = holdings_permanent_location.id
-    LEFT JOIN folio_inventory.location_j AS holdings_temporary_location ON holdings.temporary_location_id = holdings_temporary_location.id;
+    LEFT JOIN folio_inventory.holdings_type AS holdings_type ON holdings.holdings_type_id = holdings_type.id
+    LEFT JOIN folio_inventory.ill_policy AS holdings_ill_policy ON holdings.ill_policy_id = holdings_ill_policy.id
+    LEFT JOIN folio_inventory.call_number_type AS holdings_call_number_type ON holdings.call_number_type_id = holdings_call_number_type.id
+    LEFT JOIN folio_inventory.location AS holdings_permanent_location ON holdings.permanent_location_id = holdings_permanent_location.id
+    LEFT JOIN folio_inventory.location AS holdings_temporary_location ON holdings.temporary_location_id = holdings_temporary_location.id;
 
 CREATE INDEX ON folio_derived.holdings_ext (holdings_id);
 
@@ -73,9 +71,9 @@ CREATE INDEX ON folio_derived.holdings_ext (acquisition_method);
 
 CREATE INDEX ON folio_derived.holdings_ext (call_number);
 
--- CREATE INDEX ON folio_derived.holdings_ext (call_number_prefix);
+CREATE INDEX ON folio_derived.holdings_ext (call_number_prefix);
 
--- CREATE INDEX ON folio_derived.holdings_ext (call_number_suffix);
+CREATE INDEX ON folio_derived.holdings_ext (call_number_suffix);
 
 CREATE INDEX ON folio_derived.holdings_ext (call_number_type_id);
 
@@ -107,7 +105,7 @@ CREATE INDEX ON folio_derived.holdings_ext (retention_policy);
 
 CREATE INDEX ON folio_derived.holdings_ext (shelving_title);
 
--- CREATE INDEX ON folio_derived.holdings_ext (discovery_suppress);
+CREATE INDEX ON folio_derived.holdings_ext (discovery_suppress);
 
 CREATE INDEX ON folio_derived.holdings_ext (created_date);
 
