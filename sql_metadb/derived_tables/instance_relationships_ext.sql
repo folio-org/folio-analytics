04e8d627efef9251006ff3effc8f97c3bee88caa
Nassib Nassar <nrn@etymon.com>
2021-11-02 21:28:35 -0400

    Use JSON transformation in instance_relationships_ext

--- sql_metadb/derived_tables/instance_relationships_ext.sql
+++ sql_metadb/derived_tables/instance_relationships_ext.sql
@@ -5,12 +5,12 @@ CREATE TABLE folio_derived.instance_relationships_ext AS
 SELECT
     r.id AS relationship_id,
     r.instancerelationshiptypeid AS relationship_type_id,
-    json_extract_path_text(t.jsonb, 'name') AS relationship_type_name,
+    t.name AS relationship_type_name,
     r.subinstanceid AS relationship_sub_instance_id,
     r.superinstanceid AS relationship_super_instance_id
 FROM
     folio_inventory.instance_relationship AS r
-    LEFT JOIN folio_inventory.instance_relationship_type AS t
+    LEFT JOIN folio_inventory.instance_relationship_type__t AS t
         ON t.id = r.instancerelationshiptypeid;
 
 CREATE INDEX ON folio_derived.instance_relationships_ext (relationship_id);
