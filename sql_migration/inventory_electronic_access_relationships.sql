68832b2affc03f618d349b69e38ad13f5127eb3b
Nassib Nassar <nrn@etymon.com>
2022-02-05 18:01:42 -0500

    Add migration table inventory_electronic_access_relationships

---
+++ sql_migration/inventory_electronic_access_relationships.sql
@@ -0,0 +1,18 @@
+DROP TABLE IF EXISTS inventory_electronic_access_relationships;
+
+CREATE TABLE inventory_electronic_access_relationships AS
+SELECT
+    id::varchar(36),
+    jsonb_extract_path_text(jsonb, 'name')::varchar(65535) AS name,
+    jsonb_extract_path_text(jsonb, 'source')::varchar(65535) AS source,
+    jsonb_pretty(jsonb)::json AS data
+FROM
+    folio_inventory.electronic_access_relationship;
+
+ALTER TABLE inventory_electronic_access_relationships ADD PRIMARY KEY (id);
+
+CREATE INDEX ON inventory_electronic_access_relationships (name);
+
+CREATE INDEX ON inventory_electronic_access_relationships (source);
+
+VACUUM ANALYZE inventory_electronic_access_relationships;
