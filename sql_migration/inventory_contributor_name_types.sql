8893022561646c8dfa4fcdb423fb512dc59fbedb
Nassib Nassar <nrn@etymon.com>
2022-02-05 17:55:14 -0500

    Add migration table inventory_contributor_name_types

---
+++ sql_migration/inventory_contributor_name_types.sql
@@ -0,0 +1,18 @@
+DROP TABLE IF EXISTS inventory_contributor_name_types;
+
+CREATE TABLE inventory_contributor_name_types AS
+SELECT
+    id::varchar(36),
+    jsonb_extract_path_text(jsonb, 'name')::varchar(65535) AS name,
+    jsonb_extract_path_text(jsonb, 'ordering')::varchar(65535) AS ordering,
+    jsonb_pretty(jsonb)::json AS data
+FROM
+    folio_inventory.classification_type;
+
+ALTER TABLE inventory_contributor_name_types ADD PRIMARY KEY (id);
+
+CREATE INDEX ON inventory_contributor_name_types (name);
+
+CREATE INDEX ON inventory_contributor_name_types (ordering);
+
+VACUUM ANALYZE inventory_contributor_name_types;
