a05001adcad5afb71fd4bf4a727eebea348e30a6
Nassib Nassar <nrn@etymon.com>
2022-02-05 19:15:42 -0500

    Add migration table inventory_item_note_types

---
+++ sql_migration/inventory_item_note_types.sql
@@ -0,0 +1,18 @@
+DROP TABLE IF EXISTS inventory_item_note_types;
+
+CREATE TABLE inventory_item_note_types AS
+SELECT
+    id::varchar(36),
+    jsonb_extract_path_text(jsonb, 'name')::varchar(65535) AS name,
+    jsonb_extract_path_text(jsonb, 'source')::varchar(65535) AS source,
+    jsonb_pretty(jsonb)::json AS data
+FROM
+    folio_inventory.item_note_type;
+
+ALTER TABLE inventory_item_note_types ADD PRIMARY KEY (id);
+
+CREATE INDEX ON inventory_item_note_types (name);
+
+CREATE INDEX ON inventory_item_note_types (source);
+
+VACUUM ANALYZE inventory_item_note_types;
