d1241bd7a390ae8bb5b0b08291c489a61bfc4c99
Natalya Pikulik <62571646+natalyapik@users.noreply.github.com>
2022-02-16 12:13:49 -0500

    Update holding_notes.sql
    corrected indexes according to main query

--- sql_metadb/derived_tables/holding_notes.sql
+++ sql_metadb/derived_tables/holding_notes.sql
@@ -6,7 +6,7 @@ WITH notes AS (
     h.id AS holding_id,
     jsonb_extract_path_text(h.jsonb, 'hrid') AS holding_hrid,
     jsonb_extract_path_text(notes.jsonb, 'note') AS note,
-    jsonb_extract_path_text(notes.jsonb, 'holdingsNoteTypeId')::uuid AS holding_note_type_id,
+    jsonb_extract_path_text(notes.jsonb, 'holdingsNoteTypeId')::uuid AS note_type_id,
     notes.ordinality AS note_ordinality
     FROM 
     folio_inventory.holdings_record h
@@ -19,12 +19,12 @@ SELECT
     n.holding_hrid,
     n.note,
     hntt.name AS note_type_name, 
-    n.holding_note_type_id,
+    n.note_type_id,
     n.note_ordinality,
     hnt.creation_date AS note_date_created,
     hnt.created_by AS note_created_by_user_id,
     jsonb_extract_path_text(hnt.jsonb, 'updatedDate') AS note_date_updated,
-    jsonb_extract_path_text(hnt.jsonb, 'updatedByUserId') AS updated_by_user_id
+    jsonb_extract_path_text(hnt.jsonb, 'updatedByUserId') AS note_updated_by_user_id
     FROM notes AS n
     LEFT JOIN folio_inventory.INSTANCE i ON n.instance_id = i.id
     LEFT JOIN folio_inventory.holdings_note_type AS hnt ON n.holding_note_type_id = hnt.id
@@ -32,12 +32,14 @@ SELECT
 ;
 CREATE INDEX ON holdings_notes (instance_id);
 CREATE INDEX ON holdings_notes (instance_hrid);
-CREATE INDEX ON holdings_notes (holdings_id);
-CREATE INDEX ON holdings_notes (holdings_hrid);
+CREATE INDEX ON holdings_notes (holding_id);
+CREATE INDEX ON holdings_notes (holding_hrid);
 CREATE INDEX ON holdings_notes (note);
-CREATE INDEX ON holdings_notes (holdings_note_type_id);
-CREATE INDEX ON holdings_notes (note_name);
-CREATE INDEX ON holdings_notes (staff_only_note);
-CREATE INDEX ON holdings_notes (statistical_code_name);
-CREATE INDEX ON holdings_notes (stat_code_ordinality);
+CREATE INDEX ON holdings_notes (note_type_name);
+CREATE INDEX ON holdings_notes (note_type_id);
+CREATE INDEX ON holdings_notes (note_ordinality);
+CREATE INDEX ON holdings_notes (note_date_created);
+CREATE INDEX ON holdings_notes (note_created_by_user_id);
+CREATE INDEX ON holdings_notes (note_date_updated);
+CREATE INDEX ON holdings_notes (note_updated_by_user_id);
 VACUUM ANALYZE holdings_notes;
