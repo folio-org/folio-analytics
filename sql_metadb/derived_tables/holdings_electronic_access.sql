a612f194cd67741447e681a5c8519719289ea876
Natalya Pikulik <62571646+natalyapik@users.noreply.github.com>
2022-02-17 10:35:53 -0500

    Update holdings_electronic_access.sql
    corrected "ordinality"

--- sql_metadb/derived_tables/holdings_electronic_access.sql
+++ sql_metadb/derived_tables/holdings_electronic_access.sql
@@ -11,24 +11,36 @@ SELECT
     jsonb_extract_path_text(eaccess.jsonb, 'linkText') AS link_text,
     jsonb_extract_path_text(eaccess.jsonb, 'materialsSpecification') AS material_specification,
     jsonb_extract_path_text(eaccess.jsonb, 'publicNote') AS public_note,
-    eaccess.ORDINALITY AS eaccess_ordinality
+    eaccess.ordinality AS eaccess_ordinality
 FROM 
-    folio_inventory.holdings_record h
-    LEFT JOIN folio_inventory.INSTANCE i ON h.instanceid = i.id
+    folio_inventory.holdings_record AS h
+    LEFT JOIN folio_inventory.instance AS i ON h.instanceid = i.id
     CROSS JOIN LATERAL jsonb_array_elements(jsonb_extract_path(h.jsonb, 'electronicAccess'))
     WITH ORDINALITY AS eaccess (jsonb)
     LEFT JOIN folio_inventory.electronic_access_relationship AS ear ON 
         jsonb_extract_path_text(eaccess.jsonb, 'relationshipId')::uuid = ear.id
  ;
+ 
 CREATE INDEX ON holdings_electronic_access (instance_id);
+
 CREATE INDEX ON holdings_electronic_access (instance_hrid);
+
 CREATE INDEX ON holdings_electronic_access (holding_id);
+
 CREATE INDEX ON holdings_electronic_access (holding_hrid);
+
 CREATE INDEX ON holdings_electronic_access (relationship_id);
+
 CREATE INDEX ON holdings_electronic_access (relationship_name);
+
 CREATE INDEX ON holdings_electronic_access (uri);
+
 CREATE INDEX ON holdings_electronic_access (link_text);
+
 CREATE INDEX ON holdings_electronic_access (material_specification);
+
 CREATE INDEX ON holdings_electronic_access (public_note);
+
 CREATE INDEX ON holdings_electronic_access (eaccess_ordinality);
+
 VACUUM ANALYZE holdings_electronic_access;
