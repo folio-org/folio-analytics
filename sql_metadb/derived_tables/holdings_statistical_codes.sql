d55b7e2dd52eca21672ef79ae837e037df3a6c16
Nassib Nassar <nrn@etymon.com>
2022-02-21 18:35:53 -0500

    Reformat SQL source

--- sql_metadb/derived_tables/holdings_statistical_codes.sql
+++ sql_metadb/derived_tables/holdings_statistical_codes.sql
@@ -1,19 +1,21 @@
 -- Create a local holdings table with the id and name for the code and type.
+
 DROP TABLE IF EXISTS holdings_statistical_codes;
-CREATE TABLE holdings_statistical_codes AS 
+
+CREATE TABLE holdings_statistical_codes AS
 WITH stcodes AS (
-    SELECT 
-    h.instanceid AS instance_id, 
-    jsonb_extract_path_text(i.jsonb, 'hrid') AS instance_hrid,
-    h.id AS holdings_id, 
-    jsonb_extract_path_text(h.jsonb, 'hrid') AS holdings_hrid,
-    (sc.jsonb #>> '{}')::uuid AS statistical_code_id,
-    sc.ORDINALITY AS statistical_code_ordinality
-    FROM 
-    folio_inventory.holdings_record h
-    LEFT JOIN folio_inventory.INSTANCE i ON h.instanceid = i.id
-    CROSS JOIN LATERAL jsonb_array_elements(jsonb_extract_path(h.jsonb, 'statisticalCodeIds'))
-    WITH ORDINALITY AS sc (jsonb))
+    SELECT
+        h.instanceid AS instance_id,
+        jsonb_extract_path_text(i.jsonb, 'hrid') AS instance_hrid,
+        h.id AS holdings_id,
+        jsonb_extract_path_text(h.jsonb, 'hrid') AS holdings_hrid,
+        (sc.jsonb #>> '{}') ::uuid AS statistical_code_id,
+        sc.ordinality AS statistical_code_ordinality
+    FROM
+        folio_inventory.holdings_record AS h
+    LEFT JOIN folio_inventory.instance AS i ON h.instanceid = i.id
+    CROSS JOIN LATERAL jsonb_array_elements(jsonb_extract_path(h.jsonb, 'statisticalCodeIds')) WITH ORDINALITY AS sc (jsonb)
+)
 SELECT
     stc.instance_id,
     stc.instance_hrid,
@@ -24,20 +26,31 @@ SELECT
     sctt.name AS statistical_code_type_name,
     sct.code AS statistical_code,
     sct.name AS statistical_code_name,
-    stc.statistical_code_ordinality 
-FROM 
+    stc.statistical_code_ordinality
+FROM
     stcodes AS stc
-    LEFT JOIN folio_inventory.statistical_code__t AS sct ON stc.statistical_code_id::uuid = sct.id::uuid 
+    LEFT JOIN folio_inventory.statistical_code__t AS sct ON stc.statistical_code_id::uuid = sct.id::uuid
     LEFT JOIN folio_inventory.statistical_code_type__t AS sctt ON sct.statistical_code_type_id::uuid = sctt.id::uuid;
-    
+
 CREATE INDEX ON holdings_statistical_codes (instance_id);
+
 CREATE INDEX ON holdings_statistical_codes (instance_hrid);
+
 CREATE INDEX ON holdings_statistical_codes (holdings_id);
+
 CREATE INDEX ON holdings_statistical_codes (holdings_hrid);
+
 CREATE INDEX ON holdings_statistical_codes (statistical_code_id);
+
 CREATE INDEX ON holdings_statistical_codes (statistical_code_type_id);
+
 CREATE INDEX ON holdings_statistical_codes (statistical_code_type_name);
+
 CREATE INDEX ON holdings_statistical_codes (statistical_code);
+
 CREATE INDEX ON holdings_statistical_codes (statistical_code_name);
+
 CREATE INDEX ON holdings_statistical_codes (statistical_code_ordinality);
+
 VACUUM ANALYZE holdings_statistical_codes;
+
