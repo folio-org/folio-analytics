63bde13242dbf0b27b9a2da14b28488d2cbd337a
Natalya Pikulik <62571646+natalyapik@users.noreply.github.com>
2022-02-22 13:53:25 -0500

    Create holdings_statements_index.sql
    added new metadb derived table holdings_statements_indexes

---
+++ sql_metadb/derived_tables/holdings_statements_index.sql
@@ -0,0 +1,47 @@
+-- This table contains holdings statements for indexes with their
+-- associated public/staff only notes; regular holdings statements and
+-- holdings statements for supplements are in separate tables. Here note is a public note.
+DROP TABLE IF EXISTS holdings_statements_indexes;
+CREATE TABLE holdings_statements_indexes AS 
+WITH holdings AS (
+    SELECT 
+    h.instanceid AS instance_id,
+    h.id AS holdings_id,
+    jsonb_extract_path_text(h.jsonb, 'hrid') AS holdings_hrid,
+    jsonb_extract_path_text(hs.jsonb, 'statement') AS "statement",
+    jsonb_extract_path_text(hs.jsonb, 'note') AS public_note,
+    jsonb_extract_path_text(hs.jsonb, 'staffNote') AS staff_note,
+    hs.ordinality AS statement_ordinality
+    FROM folio_inventory.holdings_record h
+    CROSS JOIN LATERAL jsonb_array_elements(jsonb_extract_path(h.jsonb, 'holdingsStatementsForIndexes'))
+    WITH ORDINALITY AS hs (jsonb))
+SELECT    
+    hs.instance_id,
+    jsonb_extract_path_text(i.jsonb, 'hrid') AS instance_hrid,   
+    hs.holdings_id,
+    hs.holdings_hrid,
+    hs."statement",
+    hs.public_note,
+    hs.staff_note, 
+    hs.statement_ordinality
+    FROM holdings AS hs
+    LEFT JOIN folio_inventory.instance i ON hs.instance_id = i.id
+;
+
+CREATE INDEX ON holdings_statements_indexes (instance_id);
+
+CREATE INDEX ON holdings_statements_indexes (instance_hrid);
+
+CREATE INDEX ON holdings_statements_indexes (holdings_id);
+
+CREATE INDEX ON holdings_statements_indexes (holdings_hrid);
+
+CREATE INDEX ON holdings_statements_indexes ("statement");
+
+CREATE INDEX ON holdings_statements_indexes (public_note);
+
+CREATE INDEX ON holdings_statements_indexes (staff_note);
+
+CREATE INDEX ON holdings_statements_indexes (statement_ordinality);
+
+VACUUM ANALYZE holdings_statements_indexes;
