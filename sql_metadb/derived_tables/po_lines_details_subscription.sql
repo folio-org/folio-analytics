3f619bbacc03202ca7a6027cd7ce99617e5fc2a2
Nassib Nassar <nrn@etymon.com>
2022-02-24 18:45:29 -0500

    Adjust SQL formatting

--- sql_metadb/derived_tables/po_lines_details_subscription.sql
+++ sql_metadb/derived_tables/po_lines_details_subscription.sql
@@ -1,12 +1,13 @@
+-- Create a local table for subscription details in po_lines.
+
 DROP TABLE IF EXISTS po_lines_details_subscription;
 
--- Create a local table for subscription details in po_lines.
 CREATE TABLE po_lines_details_subscription AS
 SELECT
     pol.id AS pol_id,
     jsonb_extract_path_text(jsonb, 'details', 'subscriptionFrom')::timestamptz AS pol_subscription_from,
     jsonb_extract_path_text(jsonb, 'details', 'subscriptionTo')::timestamptz AS pol_subscription_to,
-    jsonb_extract_path_text(jsonb, 'details', 'subscriptionInterval')::int AS pol_subscription_interval
+    jsonb_extract_path_text(jsonb, 'details', 'subscriptionInterval')::integer AS pol_subscription_interval
 FROM
     folio_orders.po_line AS pol;
 
@@ -18,5 +19,5 @@ CREATE INDEX ON po_lines_details_subscription (pol_subscription_to);
 
 CREATE INDEX ON po_lines_details_subscription (pol_subscription_interval);
 
+VACUUM ANALYZE po_lines_details_subscription;
 
-VACUUM ANALYZE  po_lines_details_subscription;
