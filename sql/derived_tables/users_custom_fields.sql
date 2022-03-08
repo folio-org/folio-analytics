0a9717c1f7afcb3176634d691d2073ad2ed3a83f
Angela Zoss <angela.zoss@gmail.com>
2022-03-07 23:25:45 -0500

    trying to replace tabs with spaces

--- sql/derived_tables/users_custom_fields.sql
+++ sql/derived_tables/users_custom_fields.sql
@@ -4,18 +4,18 @@ DROP TABLE IF EXISTS users_custom_fields;
 -- the custom fields object into a table with one custom field per row
 -- and potentially multiple rows per user
 CREATE TABLE users_custom_fields AS
-	WITH objects AS (
-		SELECT 
-			uu.id::uuid AS user_id,
-			json_extract_path(uu.data, 'customFields') AS custom_fields_object
-		FROM user_users AS uu
-		WHERE json_extract_path(uu.data, 'customFields') IS NOT NULL
-	)
-	SELECT 
-		user_id, 
-		key AS field_name, 
-		value::varchar AS field_value
-	FROM objects, json_each(objects.custom_fields_object);
+WITH objects AS (
+    SELECT
+        uu.id::uuid AS user_id,
+        json_extract_path(uu.data, 'customFields') AS custom_fields_object
+    FROM user_users AS uu
+    WHERE json_extract_path(uu.data, 'customFields') IS NOT NULL
+)
+SELECT
+    user_id,
+    key AS field_name,
+    value::varchar AS field_value
+FROM objects, json_each(objects.custom_fields_object);
 
 CREATE INDEX ON users_custom_fields (user_id);
 
--- sql_metadb/derived_tables/users_custom_fields.sql
+++ sql_metadb/derived_tables/users_custom_fields.sql
@@ -4,18 +4,18 @@ DROP TABLE IF EXISTS users_custom_fields;
 -- the custom fields object into a table with one custom field per row
 -- and potentially multiple rows per user
 CREATE TABLE users_custom_fields AS
-	WITH objects AS (
-		SELECT 
-			uu.id AS user_id,
-			jsonb_extract_path(uu.jsonb, 'customFields') AS custom_fields_object
-		FROM folio_users.users AS uu
-		WHERE jsonb_extract_path(uu.jsonb, 'customFields') IS NOT NULL
-	)
-	SELECT 
-		user_id, 
-		key AS field_name, 
-		value::varchar AS field_value
-	FROM objects, jsonb_each(objects.custom_fields_object);
+WITH objects AS (
+    SELECT
+        uu.id AS user_id,
+        jsonb_extract_path(uu.jsonb, 'customFields') AS custom_fields_object
+    FROM folio_users.users AS uu
+    WHERE jsonb_extract_path(uu.jsonb, 'customFields') IS NOT NULL
+)
+SELECT
+    user_id,
+    key AS field_name,
+    value::varchar AS field_value
+FROM objects, jsonb_each(objects.custom_fields_object);
 
 CREATE INDEX ON users_custom_fields (user_id);
 
