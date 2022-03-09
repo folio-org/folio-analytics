bdf51c32b72fa620fdb3c603451761d34ab48188
Nassib Nassar <nrn@etymon.com>
2022-03-09 18:01:37 -0500

    Adjust SQL formatting

--- sql_metadb/derived_tables/holdings_electronic_access.sql
+++ sql_metadb/derived_tables/holdings_electronic_access.sql
@@ -1,8 +1,10 @@
 -- Create table for electronic access points for holdings records
+
 DROP TABLE IF EXISTS holdings_electronic_access;
-CREATE TABLE holdings_electronic_access AS 
-SELECT 
-    h.instanceid AS instance_id, 
+
+CREATE TABLE holdings_electronic_access AS
+SELECT
+    h.instanceid AS instance_id,
     jsonb_extract_path_text(i.jsonb, 'hrid') AS instance_hrid,
     h.id AS holdings_id,
     jsonb_extract_path_text(h.jsonb, 'hrid') AS holdings_hrid,
@@ -13,15 +15,12 @@ SELECT
     jsonb_extract_path_text(eaccess.jsonb, 'materialsSpecification') AS material_specification,
     jsonb_extract_path_text(eaccess.jsonb, 'publicNote') AS public_note,
     eaccess.ordinality AS eaccess_ordinality
-FROM 
+FROM
     folio_inventory.holdings_record AS h
     LEFT JOIN folio_inventory.instance AS i ON h.instanceid = i.id
-    CROSS JOIN LATERAL jsonb_array_elements(jsonb_extract_path(h.jsonb, 'electronicAccess'))
-    WITH ORDINALITY AS eaccess (jsonb)
-    LEFT JOIN folio_inventory.electronic_access_relationship AS ear ON 
-        jsonb_extract_path_text(eaccess.jsonb, 'relationshipId')::uuid = ear.id
- ;
- 
+    CROSS JOIN LATERAL jsonb_array_elements(jsonb_extract_path(h.jsonb, 'electronicAccess')) WITH ORDINALITY AS eaccess (jsonb)
+    LEFT JOIN folio_inventory.electronic_access_relationship AS ear ON jsonb_extract_path_text(eaccess.jsonb, 'relationshipId')::uuid = ear.id;
+
 CREATE INDEX ON holdings_electronic_access (instance_id);
 
 CREATE INDEX ON holdings_electronic_access (instance_hrid);
@@ -45,3 +44,4 @@ CREATE INDEX ON holdings_electronic_access (public_note);
 CREATE INDEX ON holdings_electronic_access (eaccess_ordinality);
 
 VACUUM ANALYZE holdings_electronic_access;
+
