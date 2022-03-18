183382f2e21486f804bb318c727bfab69b701a7b
eliana-flo <66032622+eliana-flo@users.noreply.github.com>
2022-03-18 14:32:06 -0400

    Update public.sql
    Added column comments for the following tables:
    -public.circulation_loan_history
    -public.circulation_request_preference
    -public.circulation_request_policies
    -public.circulation_patron_notice_policies
    -public.circulation_check_ins
    -public.circulation_scheduled_notices

--- sql/comments/public.sql
+++ sql/comments/public.sql
@@ -13,7 +13,7 @@ COMMENT ON COLUMN public.circulation_loans.loan_date IS 'Date time when the loan
 COMMENT ON COLUMN public.circulation_loans.loan_policy_id IS 'ID of last policy used in relation to this loan';
 COMMENT ON COLUMN public.circulation_loans.lost_item_policy_id IS 'ID of lost item policy which determines when the item ages to lost and the associated fees or the associated fees if the patron declares the item lost.';
 COMMENT ON COLUMN public.circulation_loans.overdue_fine_policy_id IS 'ID of overdue fines policy at the time the item is check-in or renewed';
-COMMENT ON COLUMN public.circulation_loans.patron_group_id_at_checkout IS 'Patron Group Id at checkout';
+COMMENT ON COLUMN public.circulation_loans.patron_group_id_at_checkout IS 'Patron Group at checkout';
 COMMENT ON COLUMN public.circulation_loans.proxy_user_id IS 'ID of the user representing a proxy for the patron';
 COMMENT ON COLUMN public.circulation_loans.renewal_count IS 'Count of how many times a loan has been renewed (incremented by the client)';
 COMMENT ON COLUMN public.circulation_loans.return_date IS 'Date time when the item is returned and the loan ends (typically represented according to rfc3339 section-5.6. Has not had the date-time format validation applied as was not supported at point of introduction and would now be a breaking change)';
@@ -43,5 +43,49 @@ COMMENT ON COLUMN public.circulation_requests.request_type IS 'Whether the item 
 COMMENT ON COLUMN public.circulation_requests.status IS 'Status of the request';
 COMMENT ON COLUMN public.circulation_requests."data" IS 'JSON representation of the record';
 
+--public.circulation_loan_history
+COMMENT ON COLUMN public.circulation_loan_history.id IS 'Unique ID (generated UUID) of the snapshot of the loan record';
+COMMENT ON COLUMN public.circulation_loan_history.created_date IS 'When this snapshot was created';
+COMMENT ON COLUMN public.circulation_loan_history.operation IS 'Type of change that triggered this loan record snapshot: I for insert, U for update, D for delete';
+COMMENT ON COLUMN public.circulation_loan_history."data" IS 'JSON representation of the record';
 
+--public.circulation_request_preference
+COMMENT ON COLUMN public.circulation_request_preference.id IS 'Unique request preference ID';
+COMMENT ON COLUMN public.circulation_request_preference.delivery IS 'Whether Delivery option is available to the user.';
+COMMENT ON COLUMN public.circulation_request_preference.fulfillment IS 'Preferred fulfillment type. Possible values are Delivery or Hold Shelf';
+COMMENT ON COLUMN public.circulation_request_preference.hold_shelf IS 'Whether Hold Shelf option is available to the user.';
+COMMENT ON COLUMN public.circulation_request_preference.user_id IS 'UUID of user associated with this request preference';
+COMMENT ON COLUMN public.circulation_request_preference."data" IS 'JSON representation of the record';
 
+--public.circulation_request_policies
+COMMENT ON COLUMN public.circulation_request_policies.id IS 'Unique request policy ID';
+COMMENT ON COLUMN public.circulation_request_policies.description IS 'Description of request policy';
+COMMENT ON COLUMN public.circulation_request_policies.name IS 'Unique request policy name, required';
+COMMENT ON COLUMN public.circulation_request_policies."data" IS 'JSON representation of the record';
+
+--public.circulation_patron_notice_policies 
+COMMENT ON COLUMN public.circulation_patron_notice_policies.id IS 'Patron notice policy ID, UUID';
+COMMENT ON COLUMN public.circulation_patron_notice_policies.active IS 'A flag to determine if a patron notice policy is active';
+COMMENT ON COLUMN public.circulation_patron_notice_policies.description IS 'Patron notice policy description';
+COMMENT ON COLUMN public.circulation_patron_notice_policies.name IS 'Patron notice policy name';
+COMMENT ON COLUMN public.circulation_patron_notice_policies."data" IS 'JSON representation of the record';
+
+--public.circulation_check_ins
+COMMENT ON COLUMN public.circulation_check_ins.id IS 'Unique ID (generated UUID) of the check-in operation';
+COMMENT ON COLUMN public.circulation_check_ins.item_id IS 'ID of item that has been checked in';
+COMMENT ON COLUMN public.circulation_check_ins.item_location_id IS 'Location of the item at check-in time';
+COMMENT ON COLUMN public.circulation_check_ins.item_status_prior_to_check_in IS 'Item status prior to check-in';
+COMMENT ON COLUMN public.circulation_check_ins.occurred_date_time IS 'Date and time when the check-in occurred';
+COMMENT ON COLUMN public.circulation_check_ins.performed_by_user_id IS 'ID of user that performed check-in operation';
+COMMENT ON COLUMN public.circulation_check_ins.request_queue_size IS 'Request queue size for the item';
+COMMENT ON COLUMN public.circulation_check_ins.service_point_id IS 'Service point where the item was checked in';
+COMMENT ON COLUMN public.circulation_check_ins."data" IS 'JSON representation of the record';
+
+--public.circulation_scheduled_notices
+COMMENT ON COLUMN public.circulation_scheduled_notices.id IS 'UUID of scheduled task';
+COMMENT ON COLUMN public.circulation_scheduled_notices.loan_id IS 'UUID of related loan for loan notices';
+COMMENT ON COLUMN public.circulation_scheduled_notices.next_run_time IS 'Next run time';
+COMMENT ON COLUMN public.circulation_scheduled_notices.recipient_user_id IS 'ID of the user to whom this notice should be sent to';
+COMMENT ON COLUMN public.circulation_scheduled_notices.request_id IS 'UUID of related request for request notices';
+COMMENT ON COLUMN public.circulation_scheduled_notices.triggering_event IS 'Scheduled notice triggering event';
+COMMENT ON COLUMN public.circulation_scheduled_notices."data" IS 'JSON representation of the record';
