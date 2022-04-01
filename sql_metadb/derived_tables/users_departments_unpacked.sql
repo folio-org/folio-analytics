b63a6ce571511b8c59edb2f686189ec603d7cfe9
eliana-flo <66032622+eliana-flo@users.noreply.github.com>
2022-04-01 11:35:50 -0400

    Create users_departments_unpacked.sql
    Ported the users_departments_unpacked table to Metadb. Commented out 'usageNumber' column because the reporting dev team agreed it wasn't being used. Added COMMENT ON statements for all columns so descriptions for each will appear in DBeaver.

---
+++ sql_metadb/derived_tables/users_departments_unpacked.sql
@@ -0,0 +1,52 @@
+DROP TABLE IF EXISTS users_departments_unpacked;
+
+-- Create a derived table that takes the users table and joins
+-- in the department information
+CREATE TABLE users_departments_unpacked AS
+WITH departments_array AS (
+    SELECT
+        uu.id::uuid AS user_id,
+        departments.jsonb #>> '{}'AS department_id,
+        departments.ordinality AS department_ordinality
+    FROM
+        folio_users.users AS uu
+        CROSS JOIN LATERAL jsonb_array_elements(jsonb_extract_path(jsonb, 'departments'))
+        WITH ORDINALITY AS departments (jsonb)
+)
+SELECT
+    departments_array.user_id,
+    departments_array.department_id::uuid,
+    departments_array.department_ordinality,
+    jsonb_extract_path_text(jsonb, 'name') AS department_name,
+    jsonb_extract_path_text(jsonb, 'code') AS department_code
+    --jsonb_extract_path_text(jsonb, 'usageNumber') AS department_usage_number
+FROM
+    departments_array
+LEFT JOIN folio_users.departments AS ud
+    ON departments_array.department_id::uuid = ud.id
+;
+
+CREATE INDEX ON users_departments_unpacked (user_id);
+
+CREATE INDEX ON users_departments_unpacked (department_id);
+
+CREATE INDEX ON users_departments_unpacked (department_ordinality); --if a user has more than one department, ordinality should show a sequence of numbers indicating the order of the departments in the original list
+
+CREATE INDEX ON users_departments_unpacked (department_name);
+
+CREATE INDEX ON users_departments_unpacked (department_code);
+
+--CREATE INDEX ON users_departments_unpacked (department_usage_number);
+
+
+VACUUM ANALYZE  users_departments_unpacked;
+
+COMMENT ON COLUMN users_departments_unpacked.user_id IS 'User ID (Generated UUID) of the user associated with the department(s)';
+
+COMMENT ON COLUMN users_departments_unpacked.department_id IS 'Department ID (Generated UUID) of the department';
+
+COMMENT ON COLUMN users_departments_unpacked.department_ordinality IS 'The ordinality of the department(s) associated with a user';
+
+COMMENT ON COLUMN users_departments_unpacked.department_name IS 'The display name of the department';
+
+COMMENT ON COLUMN users_departments_unpacked.department_code IS 'The (user-supplied) code for the department';
