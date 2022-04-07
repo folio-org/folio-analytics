46e7fce91de4ed4c8bb70492ddf510bd6bffc074
eliana-flo <66032622+eliana-flo@users.noreply.github.com>
2022-04-07 09:20:16 -0400

    Update users_departments_unpacked.sql
    Removed unnecessary type casting, moved COMMENT ON lines above VACUUM ANALYZE and removed commented out lines.

--- sql_metadb/derived_tables/users_departments_unpacked.sql
+++ sql_metadb/derived_tables/users_departments_unpacked.sql
@@ -5,7 +5,7 @@ DROP TABLE IF EXISTS users_departments_unpacked;
 CREATE TABLE users_departments_unpacked AS
 WITH departments_array AS (
     SELECT
-        uu.id::uuid AS user_id,
+        uu.id AS user_id,
         departments.jsonb #>> '{}'AS department_id,
         departments.ordinality AS department_ordinality
     FROM
@@ -19,7 +19,6 @@ SELECT
     departments_array.department_ordinality,
     jsonb_extract_path_text(jsonb, 'name') AS department_name,
     jsonb_extract_path_text(jsonb, 'code') AS department_code
-    --jsonb_extract_path_text(jsonb, 'usageNumber') AS department_usage_number
 FROM
     departments_array
 LEFT JOIN folio_users.departments AS ud
@@ -36,11 +35,6 @@ CREATE INDEX ON users_departments_unpacked (department_name);
 
 CREATE INDEX ON users_departments_unpacked (department_code);
 
---CREATE INDEX ON users_departments_unpacked (department_usage_number);
-
-
-VACUUM ANALYZE  users_departments_unpacked;
-
 COMMENT ON COLUMN users_departments_unpacked.user_id IS 'User ID (Generated UUID) of the user associated with the department(s)';
 
 COMMENT ON COLUMN users_departments_unpacked.department_id IS 'Department ID (Generated UUID) of the department';
@@ -50,3 +44,7 @@ COMMENT ON COLUMN users_departments_unpacked.department_ordinality IS 'The ordin
 COMMENT ON COLUMN users_departments_unpacked.department_name IS 'The display name of the department';
 
 COMMENT ON COLUMN users_departments_unpacked.department_code IS 'The (user-supplied) code for the department';
+
+VACUUM ANALYZE  users_departments_unpacked;
+
+
