2558dbd70252f0de78e36f2e17611b5a8c872042
lib-eric <49041735+lib-eric@users.noreply.github.com>
2022-05-19 14:38:10 -0500

    add item_hrid

--- sql_metadb/derived_tables/item_tags.sql
+++ sql_metadb/derived_tables/item_tags.sql
@@ -6,13 +6,17 @@ DROP TABLE IF EXISTS item_tags;
 CREATE TABLE item_tags AS
 SELECT
     i.id AS item_id,
+    it.hrid AS item_hrid,
     tags.data #>> '{}' AS item_tag,
     tags.ordinality AS item_tag_ordinality
 FROM
     folio_inventory.item AS i
-    CROSS JOIN LATERAL jsonb_array_elements(jsonb_extract_path(jsonb, 'tags', 'tagList')) WITH ORDINALITY AS tags(data);
+    CROSS JOIN LATERAL jsonb_array_elements(jsonb_extract_path(jsonb, 'tags', 'tagList')) WITH ORDINALITY AS tags(data)
+    LEFT JOIN folio_inventory.item__t AS it ON i.id = it.id;
 
 CREATE INDEX ON item_tags (item_id);
+
+CREATE INDEX ON item_tags (item_hrid);
 
 CREATE INDEX ON item_tags (item_tag);
 
