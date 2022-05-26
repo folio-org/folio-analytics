643f51345557d115c9c30f0f1fed959193b9f493
jenmawe <jenmawe@gmail.com>
2022-05-26 08:18:32 -0400

    Removed type cast of uuid

--- sql_metadb/derived_tables/instance_subjects.sql
+++ sql_metadb/derived_tables/instance_subjects.sql
@@ -9,7 +9,7 @@ SELECT
     instsubjects.ordinality AS subjects_ordinality
 FROM 
 	folio_inventory.instance__t AS it
-	LEFT JOIN folio_inventory.instance AS inst ON inst.id::uuid = it.id::uuid
+	LEFT JOIN folio_inventory.instance AS inst ON inst.id = it.id
 	CROSS JOIN LATERAL jsonb_array_elements(jsonb_extract_path(inst.jsonb, 'subjects')) WITH ORDINALITY AS instsubjects (jsonb);
 	
 CREATE INDEX ON instance_subjects (instance_id);
