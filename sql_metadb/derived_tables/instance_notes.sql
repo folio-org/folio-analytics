63e62aef2fd65d20d9f3b3a659c19d5896f9d9c0
Nassib Nassar <nrn@etymon.com>
2022-06-08 22:14:46 -0400

    Adjust formatting

--- sql_metadb/derived_tables/instance_notes.sql
+++ sql_metadb/derived_tables/instance_notes.sql
@@ -1,21 +1,23 @@
--- This derived table extracts note information from the FOLIO instance record
+-- This derived table extracts note information from the FOLIO
+-- instance record
+
 DROP TABLE IF EXISTS instance_notes;
 
 CREATE TABLE instance_notes AS 
 SELECT 
-	it.id AS instance_id,
-	it.hrid AS instance_hrid,
-	jsonb_extract_path_text(instnotes.jsonb, 'note') AS instance_note,
-	jsonb_extract_path_text(instnotes.jsonb, 'staffOnly')::boolean AS staff_only_note,
-	jsonb_extract_path_text(instnotes.jsonb, 'instanceNoteTypeId')::uuid AS instance_note_type_id,
-	intt.name AS instance_note_type_name,
-	instnotes.ordinality AS instance_notes_ordinality
-FROM 
-	folio_inventory.instance__t AS it
-	LEFT JOIN folio_inventory.instance AS inst ON inst.id::uuid = it.id::uuid
-	CROSS JOIN LATERAL jsonb_array_elements(jsonb_extract_path(inst.jsonb, 'notes')) WITH ORDINALITY AS instnotes (jsonb)
-	LEFT JOIN folio_inventory.instance_note_type__t AS intt ON jsonb_extract_path_text(instnotes.jsonb, 'instanceNoteTypeId')::uuid = intt.id::uuid ;
-	
+    inst.id AS instance_id,
+    inst.hrid AS instance_hrid,
+    jsonb_extract_path_text(instnotes.jsonb, 'note') AS instance_note,
+    jsonb_extract_path_text(instnotes.jsonb, 'staffOnly')::boolean AS staff_only_note,
+    jsonb_extract_path_text(instnotes.jsonb, 'instanceNoteTypeId')::uuid AS instance_note_type_id,
+    notetype.name AS instance_note_type_name,
+    instnotes.ordinality AS instance_notes_ordinality
+FROM
+    folio_inventory.instance__t AS inst
+    LEFT JOIN folio_inventory.instance ON instance.id::uuid = inst.id::uuid
+    CROSS JOIN LATERAL jsonb_array_elements(jsonb_extract_path(instance.jsonb, 'notes')) WITH ORDINALITY AS instnotes (jsonb)
+    LEFT JOIN folio_inventory.instance_note_type__t AS notetype ON jsonb_extract_path_text(instnotes.jsonb, 'instanceNoteTypeId')::uuid = notetype.id::uuid;
+
 CREATE INDEX ON instance_notes (instance_id);
 
 CREATE INDEX ON instance_notes (instance_hrid);
