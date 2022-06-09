c22b8cdf5aae8d73c343de8cd5c5cb27eb8a0a68
Nassib Nassar <nrn@etymon.com>
2022-06-08 22:23:48 -0400

    Adjust formatting

--- sql_metadb/derived_tables/instance_series.sql
+++ sql_metadb/derived_tables/instance_series.sql
@@ -1,17 +1,19 @@
--- This derived table extracts series statements from the FOLIO Instance record
+-- This derived table extracts series statements from the FOLIO
+-- Instance record
+
 DROP TABLE IF EXISTS instance_series;
 
 CREATE TABLE instance_series AS 
 SELECT 
-	it.id AS instance_id,
-	it.hrid AS instance_hrid,
-	instseries.jsonb #>> '{}' AS series,
+    inst.id AS instance_id,
+    inst.hrid AS instance_hrid,
+    instseries.jsonb #>> '{}' AS series,
     instseries.ordinality AS series_ordinality
 FROM 
-	folio_inventory.instance__t AS it
-	LEFT JOIN folio_inventory.instance AS inst ON inst.id::uuid = it.id::uuid
-	CROSS JOIN LATERAL jsonb_array_elements(jsonb_extract_path(inst.jsonb, 'series')) WITH ORDINALITY AS instseries (jsonb);
-	
+    folio_inventory.instance__t AS inst
+    LEFT JOIN folio_inventory.instance ON instance.id::uuid = inst.id::uuid
+    CROSS JOIN LATERAL jsonb_array_elements(jsonb_extract_path(instance.jsonb, 'series')) WITH ORDINALITY AS instseries (jsonb);
+
 CREATE INDEX ON instance_series (instance_id);
 
 CREATE INDEX ON instance_series (instance_hrid);
