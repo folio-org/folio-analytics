1b1fabd4a8fa1610ecbfd9ebe61f3a010acc0639
Nassib Nassar <nrn@etymon.com>
2022-06-08 20:01:35 -0400

    Adjust table aliases

--- sql_metadb/derived_tables/item_notes.sql
+++ sql_metadb/derived_tables/item_notes.sql
@@ -1,23 +1,22 @@
--- MetaDB version of item_notes
 -- This derived table extracts the nested array item notes
+
 DROP TABLE IF EXISTS item_notes;
 
 CREATE TABLE item_notes AS
 SELECT
-    i__t.id AS item_id,
-    i__t.hrid AS item_hrid,
-    i__t.holdings_record_id AS holdings_record_id,
+    i.id AS item_id,
+    i.hrid AS item_hrid,
+    i.holdings_record_id AS holdings_record_id,
     jsonb_extract_path_text(notes.data, 'itemNoteTypeId')::uuid AS note_type_id,
-    int__t.name AS note_type_name,
+    nt.name AS note_type_name,
     jsonb_extract_path_text(notes.data, 'note') AS note,
     jsonb_extract_path_text(notes.data, 'staffOnly')::boolean AS staff_only,
     notes.ordinality AS note_ordinality
 FROM
-    folio_inventory.item AS i
+    folio_inventory.item
     CROSS JOIN LATERAL jsonb_array_elements(jsonb_extract_path(jsonb, 'notes')) WITH ORDINALITY AS notes (data)
-    LEFT JOIN folio_inventory.item__t AS i__t ON i.id = i__t.id
-    LEFT JOIN folio_inventory.item_note_type__t AS int__t ON jsonb_extract_path_text(notes.data, 'itemNoteTypeId')::uuid = int__t.id    
-;
+    LEFT JOIN folio_inventory.item__t AS i ON item.id = i.id
+    LEFT JOIN folio_inventory.item_note_type__t AS nt ON jsonb_extract_path_text(notes.data, 'itemNoteTypeId')::uuid = nt.id;
 
 CREATE INDEX ON item_notes (item_id);
 
