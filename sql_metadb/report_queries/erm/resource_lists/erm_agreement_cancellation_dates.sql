7b2fb8393556f6d7b668b5b86359bda62101f09f
Stefan Dombek <dombek@ub.uni-leipzig.de>
2022-06-09 09:09:29 +0200

    Fix erm_agreement_cancellation_dates
    The query has been adapted to the new data schema.

--- sql_metadb/report_queries/erm/resource_lists/erm_agreement_cancellation_dates.sql
+++ sql_metadb/report_queries/erm/resource_lists/erm_agreement_cancellation_dates.sql
@@ -5,7 +5,7 @@ WITH parameters AS (
     SELECT
         'vendor' AS sao_role, -- required to restrict the table column "agreement_vendor" to vendor.
         '2022-01-01'::date AS agreement_cancellation_interval_start, -- required, start date for cancellation interval, e.g. 2022-01-01
-        '2022-12-31'::date AS agreement_cancellation_interval_end, -- required, end date for cancellation interval, e.g. 2022-12-31
+        '2023-12-31'::date AS agreement_cancellation_interval_end, -- required, end date for cancellation interval, e.g. 2022-12-31
         'active' AS agreement_status, -- Enter your agreement_status eg. 'active', 'closed', 'draft', 'requested' etc.
         'yes' AS agreement_is_perpetual -- Enter your selection for agreement perpetual
 ),
@@ -13,40 +13,40 @@ organizations AS (
     SELECT
         *
     FROM
-        folio_agreements.subscription_agreement_org AS subscription_agreement_org
-        LEFT JOIN folio_agreements.org AS agreement_org ON agreement_org.org_id = subscription_agreement_org.sao_org_fk
-        LEFT JOIN folio_agreements.refdata_value AS sao_role ON sao_role.rdv_id = subscription_agreement_org.sao_role
+        folio_agreements.subscription_agreement_org
+        LEFT JOIN folio_agreements.org ON org.org_id = subscription_agreement_org.sao_org_fk
+        LEFT JOIN folio_agreements.subscription_agreement_org_role ON subscription_agreement_org_role.saor_owner_fk = subscription_agreement_org.sao_id
+        LEFT JOIN folio_agreements.refdata_value AS sao_role ON sao_role.rdv_id = subscription_agreement_org_role.saor_role_fk
     WHERE
         sao_role.rdv_value = (SELECT sao_role FROM parameters)
 ),
 internal_contacts AS (
     SELECT
         internal_contact.ic_owner_fk,
-        jsonb_extract_path_text(users_users."jsonb", 'personal', 'lastName') || ', ' || jsonb_extract_path_text(users_users."jsonb", 'personal', 'firstName') AS names
+        jsonb_extract_path_text(users.jsonb, 'personal', 'lastName') || ', ' || jsonb_extract_path_text(users.jsonb, 'personal', 'firstName') AS names
     FROM
-        folio_agreements.internal_contact AS internal_contact
-        LEFT JOIN folio_agreements.refdata_value AS internal_contact_refdata_value ON internal_contact_refdata_value.rdv_id = internal_contact.ic_role
-        LEFT JOIN folio_users.users AS users_users ON jsonb_extract_path_text(users_users."jsonb", 'id') = internal_contact.ic_user_fk
+        folio_agreements.internal_contact
+        LEFT JOIN folio_users.users ON jsonb_extract_path_text(users.jsonb, 'id')::uuid = internal_contact.ic_user_fk
 )
 SELECT
     subscription_agreement.sa_id AS agreement_id,
     subscription_agreement.sa_name AS agreement_name,
-    agreements_erm_resource.res_name AS agreement_res_name,
+    erm_resource.res_name AS agreement_res_name,
     subscription_agreement.sa_cancellation_deadline AS agreement_cancellation_deadline,
     agreement_is_perpetual.rdv_value AS agreement_is_perpetual,
     agreement_status.rdv_value AS agreement_status,
     organizations.org_name AS agreement_vendor,
     internal_contacts.names AS agreement_internal_contact,
-    agreements_order_line.pol_orders_fk AS pol_uuid
+    order_line.pol_orders_fk AS pol_uuid
 FROM
-    folio_agreements.subscription_agreement AS subscription_agreement
+    folio_agreements.subscription_agreement
     LEFT JOIN folio_agreements.refdata_value AS agreement_is_perpetual ON agreement_is_perpetual.rdv_id = subscription_agreement.sa_is_perpetual
     LEFT JOIN folio_agreements.refdata_value AS agreement_status ON agreement_status.rdv_id = subscription_agreement.sa_agreement_status
     LEFT JOIN organizations ON organizations.sao_owner_fk = subscription_agreement.sa_id
     LEFT JOIN internal_contacts ON internal_contacts.ic_owner_fk = subscription_agreement.sa_id
-    LEFT JOIN folio_agreements.entitlement AS agreements_entitlement ON agreements_entitlement.ent_owner_fk = subscription_agreement.sa_id
-    LEFT JOIN folio_agreements.erm_resource AS agreements_erm_resource ON agreements_erm_resource.id = agreements_entitlement.ent_resource_fk
-    LEFT JOIN folio_agreements.order_line AS agreements_order_line ON agreements_order_line.pol_owner_fk = agreements_entitlement.ent_id
+    LEFT JOIN folio_agreements.entitlement ON entitlement.ent_owner_fk = subscription_agreement.sa_id
+    LEFT JOIN folio_agreements.erm_resource ON erm_resource.id = entitlement.ent_resource_fk
+    LEFT JOIN folio_agreements.order_line ON order_line.pol_owner_fk = entitlement.ent_id
 WHERE
     subscription_agreement.sa_cancellation_deadline BETWEEN (SELECT agreement_cancellation_interval_start FROM parameters) AND (SELECT agreement_cancellation_interval_end FROM parameters)
     AND
