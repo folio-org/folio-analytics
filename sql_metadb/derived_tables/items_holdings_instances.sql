a92590e0e996788d2021000b02b3a5d402abafdf
Nassib Nassar <nrn@etymon.com>
2022-06-08 21:20:19 -0400

    Adjust table aliases

--- sql_metadb/derived_tables/items_holdings_instances.sql
+++ sql_metadb/derived_tables/items_holdings_instances.sql
@@ -1,45 +1,45 @@
-DROP TABLE IF EXISTS items_holdings_instances;
-
 -- Create an extended items table that includes holdings and instances
 -- information such as call number, material type, title, etc.
 
+DROP TABLE IF EXISTS items_holdings_instances;
+
 CREATE TABLE items_holdings_instances AS
 SELECT
-    ii.id AS item_id,
-    ii.barcode,
-    ii.copy_number AS item_copy_number,
-    ii.chronology,
-    ii.enumeration,
-    ii.volume,
-    ii.hrid AS item_hrid,
-    ii.item_identifier,
-    ii.item_level_call_number,
-    ih.call_number_type_id,
-    icnt.name AS call_number_type_name,
-    ii.material_type_id,
-    imt.name AS material_type_name,
-    ii.number_of_pieces,
-    ih.id AS holdings_id,
-    ih.call_number,
-    ih.acquisition_method,
-    ih.copy_number AS holdings_copy_number,
-    ih.holdings_type_id,
-    iht.name AS holdings_type_name,
-    ih.instance_id,
-    ih.shelving_title,
-    ii2.cataloged_date,
-    ii2.index_title,
-    ii2.title,
-    ilt.id AS loan_type_id,
-    ilt.name AS loan_type_name
+    item.id AS item_id,
+    item.barcode,
+    item.copy_number AS item_copy_number,
+    item.chronology,
+    item.enumeration,
+    item.volume,
+    item.hrid AS item_hrid,
+    item.item_identifier,
+    item.item_level_call_number,
+    holdings.call_number_type_id,
+    calltype.name AS call_number_type_name,
+    item.material_type_id,
+    mattype.name AS material_type_name,
+    item.number_of_pieces,
+    holdings.id AS holdings_id,
+    holdings.call_number,
+    holdings.acquisition_method,
+    holdings.copy_number AS holdings_copy_number,
+    holdings.holdings_type_id,
+    holdtype.name AS holdings_type_name,
+    holdings.instance_id,
+    holdings.shelving_title,
+    instance.cataloged_date,
+    instance.index_title,
+    instance.title,
+    loantype.id AS loan_type_id,
+    loantype.name AS loan_type_name
 FROM
-    folio_inventory.item__t AS ii
-    LEFT JOIN folio_inventory.holdings_record__t AS ih ON ii.holdings_record_id = ih.id
-    LEFT JOIN folio_inventory.instance__t AS ii2 ON ih.instance_id = ii2.id
-    LEFT JOIN folio_inventory.loan_type__t AS ilt ON ii.permanent_loan_type_id = ilt.id
-    LEFT JOIN folio_inventory.material_type__t AS imt ON ii.material_type_id = imt.id
-    LEFT JOIN folio_inventory.holdings_type__t AS iht ON ih.holdings_type_id = iht.id
-    LEFT JOIN folio_inventory.call_number_type__t AS icnt ON ih.call_number_type_id = icnt.id;
+    folio_inventory.item__t AS item
+    LEFT JOIN folio_inventory.holdings_record__t AS holdings ON item.holdings_record_id = holdings.id
+    LEFT JOIN folio_inventory.instance__t AS instance ON holdings.instance_id = instance.id
+    LEFT JOIN folio_inventory.loan_type__t AS loantype ON item.permanent_loan_type_id = loantype.id
+    LEFT JOIN folio_inventory.material_type__t AS mattype ON item.material_type_id = mattype.id
+    LEFT JOIN folio_inventory.holdings_type__t AS holdtype ON holdings.holdings_type_id = holdtype.id
+    LEFT JOIN folio_inventory.call_number_type__t AS calltype ON holdings.call_number_type_id = calltype.id;
 
 CREATE INDEX ON items_holdings_instances (item_id);
 
