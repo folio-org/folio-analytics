8e7279d3d66edf44bbdc285913d849976aab8a29
Nassib Nassar <nrn@etymon.com>
2022-06-08 21:57:43 -0400

    Adjust formatting

--- sql_metadb/derived_tables/instance_publication.sql
+++ sql_metadb/derived_tables/instance_publication.sql
@@ -1,19 +1,21 @@
--- This derived table extracts publication information from the FOLIO instance record
+-- This derived table extracts publication information from the FOLIO
+-- instance record
+
 DROP TABLE IF EXISTS instance_publication;
 
 CREATE TABLE instance_publication AS 
 SELECT 
-	it.id AS instance_id,
-	it.hrid AS instance_hrid,
-	jsonb_extract_path_text(pub.jsonb, 'place') AS publication_place,
-	jsonb_extract_path_text(pub.jsonb, 'publisher') AS publisher,
-	jsonb_extract_path_text(pub.jsonb, 'role') AS publication_role,
-	jsonb_extract_path_text(pub.jsonb, 'dateOfPublication') AS date_of_publication,
-	pub.ordinality AS publication_ordinality
+    instance__t.id AS instance_id,
+    instance__t.hrid AS instance_hrid,
+    jsonb_extract_path_text(pub.jsonb, 'place') AS publication_place,
+    jsonb_extract_path_text(pub.jsonb, 'publisher') AS publisher,
+    jsonb_extract_path_text(pub.jsonb, 'role') AS publication_role,
+    jsonb_extract_path_text(pub.jsonb, 'dateOfPublication') AS date_of_publication,
+    pub.ordinality AS publication_ordinality
 FROM 
-	folio_inventory.instance__t AS it
-	LEFT JOIN folio_inventory.instance AS inst ON inst.id::uuid  = it.id::uuid 
-	CROSS JOIN LATERAL jsonb_array_elements(jsonb_extract_path(inst.jsonb, 'publication')) WITH ORDINALITY AS pub (jsonb);
+    folio_inventory.instance__t
+    LEFT JOIN folio_inventory.instance ON instance.id::uuid  = instance__t.id::uuid 
+    CROSS JOIN LATERAL jsonb_array_elements(jsonb_extract_path(instance.jsonb, 'publication')) WITH ORDINALITY AS pub (jsonb);
 
 CREATE INDEX ON instance_publication (instance_id);
 
