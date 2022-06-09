dda77264da7efbcd0591a409bfca743e4a33b67f
Nassib Nassar <nrn@etymon.com>
2022-06-08 21:32:37 -0400

    Adjust table aliases

--- sql_metadb/derived_tables/item_tags.sql
+++ sql_metadb/derived_tables/item_tags.sql
@@ -1,18 +1,17 @@
--- metadb -- item_tags
 -- This table creates a listing of items and their tag(s).
 
 DROP TABLE IF EXISTS item_tags;
 
 CREATE TABLE item_tags AS
 SELECT
-    i.id AS item_id,
-    it.hrid AS item_hrid,
+    item.id AS item_id,
+    i.hrid AS item_hrid,
     tags.data #>> '{}' AS item_tag,
     tags.ordinality AS item_tag_ordinality
 FROM
-    folio_inventory.item AS i
+    folio_inventory.item
     CROSS JOIN LATERAL jsonb_array_elements(jsonb_extract_path(jsonb, 'tags', 'tagList')) WITH ORDINALITY AS tags(data)
-    LEFT JOIN folio_inventory.item__t AS it ON i.id = it.id;
+    LEFT JOIN folio_inventory.item__t AS i ON item.id = i.id;
 
 CREATE INDEX ON item_tags (item_id);
 
