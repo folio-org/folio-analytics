f077bd936489f095f8cbc38a2dd33e117310b0e6
Nassib Nassar <nrn@etymon.com>
2022-06-08 22:34:37 -0400

    Adjust formatting

--- sql_metadb/derived_tables/instance_subjects.sql
+++ sql_metadb/derived_tables/instance_subjects.sql
@@ -1,17 +1,18 @@
 -- This derived table extracts subjects from the FOLIO Instance record
+
 DROP TABLE IF EXISTS instance_subjects;
 
 CREATE TABLE instance_subjects AS 
 SELECT 
-	it.id AS instance_id,
-	it.hrid AS instance_hrid,
-	instsubjects.jsonb #>> '{}' AS subjects,
+    inst.id AS instance_id,
+    inst.hrid AS instance_hrid,
+    instsubjects.jsonb #>> '{}' AS subjects,
     instsubjects.ordinality AS subjects_ordinality
 FROM 
-	folio_inventory.instance__t AS it
-	LEFT JOIN folio_inventory.instance AS inst ON inst.id = it.id
-	CROSS JOIN LATERAL jsonb_array_elements(jsonb_extract_path(inst.jsonb, 'subjects')) WITH ORDINALITY AS instsubjects (jsonb);
-	
+    folio_inventory.instance__t AS inst
+    LEFT JOIN folio_inventory.instance ON instance.id = inst.id
+    CROSS JOIN LATERAL jsonb_array_elements(jsonb_extract_path(instance.jsonb, 'subjects')) WITH ORDINALITY AS instsubjects (jsonb);
+
 CREATE INDEX ON instance_subjects (instance_id);
 
 CREATE INDEX ON instance_subjects (instance_hrid);
