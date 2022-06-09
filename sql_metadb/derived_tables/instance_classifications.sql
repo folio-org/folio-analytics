b554a274dd2f4dc639724b44012f8c81c1805d10
Nassib Nassar <nrn@etymon.com>
2022-06-08 23:28:37 -0400

    Adjust formatting

--- sql_metadb/derived_tables/instance_classifications.sql
+++ sql_metadb/derived_tables/instance_classifications.sql
@@ -1,19 +1,21 @@
--- This derived table extracts classification values, ids, and names from the instance.
+-- This derived table extracts classification values, ids, and names
+-- from the instance.
+
 DROP TABLE IF EXISTS instance_classifications;
 
 CREATE TABLE instance_classifications AS 
 SELECT
-	it.id AS instance_id,
-	it.hrid AS instance_hrid,
-	jsonb_extract_path_text(instclass.jsonb, 'classificationNumber') AS classification_number,
-	jsonb_extract_path_text(instclass.jsonb, 'classificationTypeId')::uuid AS classification_type_id,
-	instclass.ordinality AS classification_ordinality,
-	ctt.name AS classification_name
-FROM 
-	folio_inventory.instance__t AS it 
-	LEFT JOIN folio_inventory.instance AS inst ON inst.id::uuid = it.id::uuid
-	CROSS JOIN LATERAL jsonb_array_elements(jsonb_extract_path(inst.jsonb, 'classifications')) WITH ORDINALITY AS instclass (jsonb)
-	LEFT JOIN folio_inventory.classification_type__t AS ctt ON jsonb_extract_path_text(instclass.jsonb, 'classificationTypeId')::uuid = ctt.id::uuid ;
+    inst.id AS instance_id,
+    inst.hrid AS instance_hrid,
+    jsonb_extract_path_text(instclass.jsonb, 'classificationNumber') AS classification_number,
+    jsonb_extract_path_text(instclass.jsonb, 'classificationTypeId')::uuid AS classification_type_id,
+    instclass.ordinality AS classification_ordinality,
+    classtype.name AS classification_name
+FROM
+    folio_inventory.instance__t AS inst
+    LEFT JOIN folio_inventory.instance ON instance.id::uuid = inst.id::uuid
+    CROSS JOIN LATERAL jsonb_array_elements(jsonb_extract_path(instance.jsonb, 'classifications')) WITH ORDINALITY AS instclass (jsonb)
+    LEFT JOIN folio_inventory.classification_type__t AS classtype ON jsonb_extract_path_text(instclass.jsonb, 'classificationTypeId')::uuid = classtype.id::uuid ;
 
 CREATE INDEX ON instance_classifications (instance_id);
 
