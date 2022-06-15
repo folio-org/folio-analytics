8ef77797eba794f94c5eddebaafe14a3f878aaf6
Eric Pennington <pennington66502@library.tamu.edu>
2022-06-15 10:13:19 -0500

    create item_administrative_notes
    New metadb derived table extracting administrative notes out of item records

---
+++ sql_metadb/derived_tables/item_administrative_notes.sql
@@ -0,0 +1,26 @@
+-- This derived table extracts administrative notes from the item record.
+DROP TABLE IF EXISTS item_administrative_notes;
+
+CREATE TABLE item_administrative_notes AS
+SELECT
+    it.id AS item_id,
+    it.hrid AS item_hrid,
+    it.holdings_record_id AS holdings_record_id,
+    adminNotes.jsonb #>> '{}' AS administrative_note,
+    adminNotes.ordinality AS administrative_note_ordinality
+FROM
+    folio_inventory.item AS i
+    CROSS JOIN LATERAL jsonb_array_elements(jsonb_extract_path(jsonb, 'administrativeNotes')) WITH ORDINALITY AS adminNotes (jsonb)
+    LEFT JOIN folio_inventory.item__t AS it ON i.id = it.id;
+
+CREATE INDEX ON item_administrative_notes (item_id);
+
+CREATE INDEX ON item_administrative_notes (item_hrid);
+
+CREATE INDEX ON item_administrative_notes (holdings_record_id);
+
+CREATE INDEX ON item_administrative_notes (administrative_note);
+
+CREATE INDEX ON item_administrative_notes (administrative_note_ordinality);
+
+VACUUM ANALYZE item_administrative_notes;
