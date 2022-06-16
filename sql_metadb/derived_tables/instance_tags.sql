3e01c49ad279974241d36de26dd0d7415564b721
jenmawe <jenmawe@gmail.com>
2022-06-16 10:38:32 -0400

    Made column names singular

--- sql_metadb/derived_tables/instance_tags.sql
+++ sql_metadb/derived_tables/instance_tags.sql
@@ -5,8 +5,8 @@ CREATE TABLE instance_tags AS
 SELECT 
 	it.id AS instance_id,
 	it.hrid AS instance_hrid,
-	insttags.jsonb #>> '{}' AS instance_tags,
-	insttags.ordinality AS instance_tags_ordinality
+	insttags.jsonb #>> '{}' AS instance_tag,
+	insttags.ordinality AS instance_tag_ordinality
 FROM 
 	folio_inventory.instance__t AS it
 	LEFT JOIN folio_inventory.instance AS inst ON inst.id = it.id
@@ -16,8 +16,8 @@ CREATE INDEX ON instance_tags (instance_id);
 
 CREATE INDEX ON instance_tags (instance_hrid);
 
-CREATE INDEX ON instance_tags (instance_tags);
+CREATE INDEX ON instance_tags (instance_tag);
 
-CREATE INDEX ON instance_tags (instance_tags_ordinality);
+CREATE INDEX ON instance_tags (instance_tag_ordinality);
 
 VACUUM ANALYZE instance_tags;
