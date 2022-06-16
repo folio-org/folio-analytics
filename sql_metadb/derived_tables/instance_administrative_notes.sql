2f4fb4bbcc946d936dd4fa7bcce051b379017955
jenmawe <jenmawe@gmail.com>
2022-06-16 10:09:45 -0400

    Changed column name to singular

--- sql_metadb/derived_tables/instance_administrative_notes.sql
+++ sql_metadb/derived_tables/instance_administrative_notes.sql
@@ -5,19 +5,19 @@ CREATE TABLE instance_administrative_notes AS
 SELECT 
 	it.id AS instance_id,
 	it.hrid AS instance_hrid,
-	adminNotes.jsonb #>> '{}' AS administrative_notes,
-	adminNotes.ordinality AS administrative_notes_ordinality
+	admin_note.jsonb #>> '{}' AS administrative_note,
+	admin_note.ordinality AS administrative_note_ordinality
 FROM 
 	folio_inventory.instance__t AS it
 	LEFT JOIN folio_inventory.instance AS inst ON inst.id = it.id 
-	CROSS JOIN LATERAL jsonb_array_elements(jsonb_extract_path(inst.jsonb, 'administrativeNotes')) WITH ORDINALITY AS adminNotes (jsonb);
+	CROSS JOIN LATERAL jsonb_array_elements(jsonb_extract_path(inst.jsonb, 'administrativeNotes')) WITH ORDINALITY AS admin_note (jsonb);
 	
 CREATE INDEX ON instance_administrative_notes (instance_id);
 
 CREATE INDEX ON instance_administrative_notes (instance_hrid);
 
-CREATE INDEX ON instance_administrative_notes (administrative_notes);
+CREATE INDEX ON instance_administrative_notes (administrative_note);
 
-CREATE INDEX ON instance_administrative_notes (administrative_notes_ordinality);
+CREATE INDEX ON instance_administrative_notes (administrative_note_ordinality);
 
 VACUUM ANALYZE instance_administrative_notes;
