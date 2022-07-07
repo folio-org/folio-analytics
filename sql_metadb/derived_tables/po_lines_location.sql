04a91ff47fecb5d5e3de8a4940b0799e4d2865c2
Nassib Nassar <nrn@etymon.com>
2022-07-07 19:41:48 -0400

    Clean up po_lines_locations

--- sql_metadb/derived_tables/po_lines_location.sql
+++ sql_metadb/derived_tables/po_lines_location.sql
@@ -1,30 +1,31 @@
+-- Create a local table for Purchase Order Line Locations including
+-- location quantity and type.
+
 DROP TABLE IF EXISTS po_lines_locations;
 
--- Create a local table for Purchase Order Line Locations including location quantity and type.
-
 CREATE TABLE po_lines_locations AS
-WITH pol_location AS (
+WITH ploc AS (
     SELECT
-        pol.id::uuid AS pol_id,
+        p.id::uuid AS pol_id,
         jsonb_extract_path_text(locations.data, 'locationId')::uuid AS pol_loc_id,
         jsonb_extract_path_text(locations.data, 'quantity')::int AS pol_loc_qty,
         jsonb_extract_path_text(locations.data, 'quantityElectronic')::int AS pol_loc_qty_elec,
         jsonb_extract_path_text(locations.data, 'quantityPhysical')::int AS pol_loc_qty_phys
     FROM
-        folio_orders.po_line AS pol
+        folio_orders.po_line AS p
         CROSS JOIN jsonb_array_elements(jsonb_extract_path(jsonb, 'locations')) AS locations (data)
 )
 SELECT
-    pol_location.pol_id,
-    pol_location.pol_loc_id,
-    pol_location.pol_loc_qty,
-    pol_location.pol_loc_qty_elec,
-    pol_location.pol_loc_qty_phys,
-    folio_inventory.location__t.name AS location_name
+    ploc.pol_id,
+    ploc.pol_loc_id,
+    ploc.pol_loc_qty,
+    ploc.pol_loc_qty_elec,
+    ploc.pol_loc_qty_phys,
+    loc.name AS location_name
 FROM
-    pol_location
-    LEFT JOIN folio_inventory.location__t ON folio_inventory.location__t.id = pol_location.pol_loc_id;
- 
+    ploc
+    LEFT JOIN folio_inventory.location__t AS loc ON loc.id = ploc.pol_loc_id;
+
 CREATE INDEX ON po_lines_locations (pol_id);
 
 CREATE INDEX ON po_lines_locations (pol_loc_id);
@@ -37,4 +38,4 @@ CREATE INDEX ON po_lines_locations (pol_loc_qty_phys);
 
 CREATE INDEX ON po_lines_locations (location_name);
 
-VACUUM ANALYZE  po_lines_locations;
+VACUUM ANALYZE po_lines_locations;
