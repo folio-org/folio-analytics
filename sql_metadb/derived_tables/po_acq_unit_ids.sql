6700c00ebdb2c70374e3bc6d59ec3cc31c2f4613
Nassib Nassar <nrn@etymon.com>
2022-07-07 21:23:16 -0400

    Clean up po_acq_unit_ids

--- sql_metadb/derived_tables/po_acq_unit_ids.sql
+++ sql_metadb/derived_tables/po_acq_unit_ids.sql
@@ -1,4 +1,6 @@
--- These derived table extracts aquisition unit ids from purchase order table.
+-- These derived table extracts aquisition unit ids from purchase
+-- order table.
+
 DROP TABLE IF EXISTS po_acq_unit_ids;
 
 CREATE TABLE po_acq_unit_ids AS
@@ -9,9 +11,9 @@ CREATE TABLE po_acq_unit_ids AS
         au.name AS po_acquisition_unit_name
     FROM
         folio_orders.purchase_order__t AS po
-        LEFT JOIN folio_orders.purchase_order AS pur ON po.id = pur.id
-        CROSS JOIN jsonb_array_elements(jsonb_extract_path(pur.jsonb, 'acqUnitIds')) WITH ORDINALITY AS acq_unit_ids (jsonb)
-        LEFT JOIN folio_orders.acquisitions_unit__t AS au ON (acq_unit_ids.jsonb #>> '{}')::uuid = au.id ;
+        LEFT JOIN folio_orders.purchase_order ON po.id = purchase_order.id
+        CROSS JOIN jsonb_array_elements(jsonb_extract_path(purchase_order.jsonb, 'acqUnitIds')) WITH ORDINALITY AS acq_unit_ids (jsonb)
+        LEFT JOIN folio_orders.acquisitions_unit__t AS au ON (acq_unit_ids.jsonb #>> '{}')::uuid = au.id;
     
 CREATE INDEX ON po_acq_unit_ids (po_id);
 
@@ -21,5 +23,4 @@ CREATE INDEX ON po_acq_unit_ids (po_acquisition_unit_id);
 
 CREATE INDEX ON po_acq_unit_ids (po_acquisition_unit_name);
 
-
-VACUUM ANALYZE  po_acq_unit_ids;
+VACUUM ANALYZE po_acq_unit_ids;
