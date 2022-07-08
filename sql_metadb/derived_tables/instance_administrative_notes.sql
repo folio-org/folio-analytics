023b7344b86b53b30226ec49bc345269aa506854
Nassib Nassar <nrn@etymon.com>
2022-07-07 21:05:31 -0400

    Clean up instance_administrative_notes

--- sql_metadb/derived_tables/instance_administrative_notes.sql
+++ sql_metadb/derived_tables/instance_administrative_notes.sql
@@ -1,17 +1,19 @@
--- This derived table extracts administrative notes from the instance record.
+-- This derived table extracts administrative notes from the instance
+-- record.
+
 DROP TABLE IF EXISTS instance_administrative_notes;
 
-CREATE TABLE instance_administrative_notes AS 
-SELECT 
-	it.id AS instance_id,
-	it.hrid AS instance_hrid,
-	admin_note.jsonb #>> '{}' AS administrative_note,
-	admin_note.ordinality AS administrative_note_ordinality
-FROM 
-	folio_inventory.instance__t AS it
-	LEFT JOIN folio_inventory.instance AS inst ON inst.id = it.id 
-	CROSS JOIN LATERAL jsonb_array_elements(jsonb_extract_path(inst.jsonb, 'administrativeNotes')) WITH ORDINALITY AS admin_note (jsonb);
-	
+CREATE TABLE instance_administrative_notes AS
+SELECT
+    i.id AS instance_id,
+    i.hrid AS instance_hrid,
+    admin_note.jsonb #>> '{}' AS administrative_note,
+    admin_note.ordinality AS administrative_note_ordinality
+FROM
+    folio_inventory.instance__t AS i
+    LEFT JOIN folio_inventory.instance AS inst ON inst.id = i.id
+    CROSS JOIN LATERAL jsonb_array_elements(jsonb_extract_path(inst.jsonb, 'administrativeNotes')) WITH ORDINALITY AS admin_note (jsonb);
+
 CREATE INDEX ON instance_administrative_notes (instance_id);
 
 CREATE INDEX ON instance_administrative_notes (instance_hrid);
