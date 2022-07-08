0aab7a042b988175536f629c954c1b21ba07ee9b
Nassib Nassar <nrn@etymon.com>
2022-07-07 21:10:40 -0400

    Clean up instance_tags

--- sql_metadb/derived_tables/instance_tags.sql
+++ sql_metadb/derived_tables/instance_tags.sql
@@ -1,17 +1,18 @@
 -- This derived table extracts tags from the instance record.
+
 DROP TABLE IF EXISTS instance_tags;
 
-CREATE TABLE instance_tags AS 
-SELECT 
-	it.id AS instance_id,
-	it.hrid AS instance_hrid,
-	insttags.jsonb #>> '{}' AS instance_tag,
-	insttags.ordinality AS instance_tag_ordinality
-FROM 
-	folio_inventory.instance__t AS it
-	LEFT JOIN folio_inventory.instance AS inst ON inst.id = it.id
-	CROSS JOIN LATERAL jsonb_array_elements(jsonb_extract_path(inst.jsonb, 'tags', 'tagList')) WITH ORDINALITY AS insttags (jsonb);
-	
+CREATE TABLE instance_tags AS
+SELECT
+    i.id AS instance_id,
+    i.hrid AS instance_hrid,
+    taglist.jsonb #>> '{}' AS instance_tag,
+    taglist.ordinality AS instance_tag_ordinality
+FROM
+    folio_inventory.instance__t AS i
+    LEFT JOIN folio_inventory.instance AS inst ON inst.id = i.id
+    CROSS JOIN LATERAL jsonb_array_elements(jsonb_extract_path(inst.jsonb, 'tags', 'tagList')) WITH ORDINALITY AS taglist (jsonb);
+
 CREATE INDEX ON instance_tags (instance_id);
 
 CREATE INDEX ON instance_tags (instance_hrid);
