fee695bcd7df2949caf70ef930f87ad29d70a5fd
Natalya Pikulik <62571646+natalyapik@users.noreply.github.com>
2022-07-14 10:32:11 -0400

    Update po_lines_vendor_reference_numbers.sql
    fixed the typo, removed schema name and lowered case 'data'

--- sql/derived_tables/po_lines_vendor_reference_numbers.sql
+++ sql/derived_tables/po_lines_vendor_reference_numbers.sql
@@ -1,14 +1,14 @@
--- This derived table extracts refeence numbers from po_lines vendor details.
+-- This derived table extracts reference numbers from po_lines vendor details.
 
-DROP TABLE IF EXISTS folio_reporting.po_lines_vendor_reference_numbers;
+DROP TABLE IF EXISTS po_lines_vendor_reference_numbers;
 
-CREATE TABLE folio_reporting.po_lines_vendor_reference_numbers AS 
+CREATE TABLE po_lines_vendor_reference_numbers AS 
 SELECT
         po_lines.id AS po_line_id,
         po_lines.po_line_number AS po_line_number,
         json_extract_path_text(numbers.data, 'refNumber') AS vendor_reference_number,
         json_extract_path_text(numbers.data, 'refNumberType') AS vendor_reference_number_type,
-        json_extract_path_text(po_lines.DATA, 'vendorDetail', 'instructions') AS vendor_instructions
+        json_extract_path_text(po_lines.data, 'vendorDetail', 'instructions') AS vendor_instructions
 FROM po_lines
      CROSS JOIN json_array_elements(json_extract_path(po_lines.data, 'vendorDetail', 'referenceNumbers'))
                 AS numbers (data);
