e72f9c18743b46804fde73820c50a66177fb2ae4
Natalya Pikulik <62571646+natalyapik@users.noreply.github.com>
2022-08-15 11:54:27 -0400

    Create instance_administrative_notes.sql
    creates new LDP derived table instance_administrative_notes, closes issue #661

---
+++ sql/derived_tables/instance_administrative_notes.sql
@@ -0,0 +1,25 @@
+-- This derived table extracts administrative notes from the instance
+-- record.
+
+DROP TABLE IF EXISTS instance_administrative_notes;
+
+CREATE TABLE instance_administrative_notes AS
+SELECT
+    i.id AS instance_id,
+    i.hrid AS instance_hrid,
+    admin_note.data #>> '{}' AS administrative_note,
+    admin_note.ordinality AS administrative_note_ordinality
+FROM
+    inventory_instances AS i
+    CROSS JOIN LATERAL json_array_elements(json_extract_path(data, 'administrativeNotes'))
+    WITH ORDINALITY AS admin_note (data);
+
+CREATE INDEX ON instance_administrative_notes (instance_id);
+
+CREATE INDEX ON instance_administrative_notes (instance_hrid);
+
+CREATE INDEX ON instance_administrative_notes (administrative_note);
+
+CREATE INDEX ON instance_administrative_notes (administrative_note_ordinality);
+
+VACUUM ANALYZE instance_administrative_notes;
