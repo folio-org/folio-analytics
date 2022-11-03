39f88f10718df95bb6c3e944f1f6a8dd79276781
Arthur Aguilera <49205333+arthur-aguilera@users.noreply.github.com>
2022-11-03 11:46:17 -0600

    Update finance_transaction_invoices.sql
    Related to issue #624. Adding comments with field definitions in the comments from the FOLIO API documentation.

--- sql_metadb/derived_tables/finance_transaction_invoices.sql
+++ sql_metadb/derived_tables/finance_transaction_invoices.sql
@@ -1,5 +1,4 @@
 DROP TABLE IF EXISTS finance_transaction_invoices;
-
 -- Create a derived table that joins invoice and invoice_lines fields to transactions for expenditure reports in system currency
 --
 -- NOTE: effective_fund_id, effective_fund_name, effective_fund_code were derived from the set from_fund or to_fund as a convenient 
@@ -11,6 +10,7 @@ DROP TABLE IF EXISTS finance_transaction_invoices;
 --   folio_invoice.invoice_lines
 --   folio_finance.fund
 --   folio_finance.budget
+
 CREATE TABLE finance_transaction_invoices AS
 SELECT
     ft.id AS transaction_id,
@@ -42,7 +42,7 @@ SELECT
     jsonb_extract_path_text(ii.jsonb, 'currency') AS invoice_currency,
     jsonb_extract_path_text(il.jsonb, 'poLineId') AS po_line_id,
     jsonb_extract_path_text(ii.jsonb, 'vendorId')::uuid AS invoice_vendor_id,
-    jsonb_extract_path_text(oo.jsonb, 'name') AS invoice_vendor_name
+    jsonb_extract_path_text(oo.jsonb, 'name') AS invoice_vendor_name   
 FROM
     folio_finance.transaction AS ft
     LEFT JOIN folio_invoice.invoices AS ii ON jsonb_extract_path_text(ft.jsonb, 'sourceInvoiceId')::uuid = ii.id
@@ -51,6 +51,7 @@ FROM
     LEFT JOIN folio_finance.fund AS tf ON ft.tofundid = tf.id
     LEFT JOIN folio_finance.budget AS fb ON ft.fromfundid = fb.fundid AND ft.fiscalyearid = fb.fiscalyearid
     LEFT JOIN folio_organizations. organizations AS oo ON jsonb_extract_path_text(ii.jsonb, 'vendorId')::uuid = oo.id
+    
 WHERE (jsonb_extract_path_text(ft.jsonb, 'transactionType') = 'Pending payment'
     OR jsonb_extract_path_text(ft.jsonb, 'transactionType') = 'Payment'
     OR jsonb_extract_path_text(ft.jsonb, 'transactionType') = 'Credit');
@@ -114,5 +115,65 @@ CREATE INDEX ON finance_transaction_invoices (po_line_id);
 CREATE INDEX ON finance_transaction_invoices (invoice_vendor_id);
 
 CREATE INDEX ON finance_transaction_invoices (invoice_vendor_name);
+
+COMMENT ON COLUMN finance_transaction_invoices.transaction_id IS 'UUID of this transaction';
+
+COMMENT ON COLUMN finance_transaction_invoices.transaction_amount IS 'The amount of this transaction. For encumbrances: This is initialAmountEncumbered - (amountAwaitingPayment + amountExpended)';
+
+COMMENT ON COLUMN finance_transaction_invoices.transaction_currency IS 'Currency code for this transaction - from the system currency';
+
+COMMENT ON COLUMN finance_transaction_invoices.transaction_created_date IS 'Date and time when the record was created';
+
+COMMENT ON COLUMN finance_transaction_invoices.transaction_updated_date IS 'Date and time when the record was last updated';
+
+COMMENT ON COLUMN finance_transaction_invoices.transaction_description IS 'Description of this transaction';
+
+COMMENT ON COLUMN finance_transaction_invoices.transaction_expense_class_id IS 'UUID of the associated expense class';
+
+COMMENT ON COLUMN finance_transaction_invoices.transaction_fiscal_year_id IS 'UUID of the fiscal year that the transaction is taking place in';
+
+COMMENT ON COLUMN finance_transaction_invoices.transaction_from_fund_id IS 'UUID of the fund money is moving from';
+
+COMMENT ON COLUMN finance_transaction_invoices.transaction_from_fund_name IS 'The name of this fund money is moving from';
+
+COMMENT ON COLUMN finance_transaction_invoices.transaction_from_fund_code IS 'A unique code associated with the fund money is moving from';
+
+COMMENT ON COLUMN finance_transaction_invoices.transaction_to_fund_id IS 'UUID of the fund money is moving to';
+
+COMMENT ON COLUMN finance_transaction_invoices.transaction_to_fund_name IS 'The name of the fund money is moving to';
+
+COMMENT ON COLUMN finance_transaction_invoices.transaction_to_fund_code IS 'A unique code associated with the fund money is moving to';
+
+COMMENT ON COLUMN finance_transaction_invoices.effective_fund_id IS 'UUID of the current effective fund';
+
+COMMENT ON COLUMN finance_transaction_invoices.effective_fund_name IS 'The name of the current effective fund';
+
+COMMENT ON COLUMN finance_transaction_invoices.effective_fund_code IS 'A unique code for the current effective fund';
+
+COMMENT ON COLUMN finance_transaction_invoices.transaction_from_budget_id IS 'UUID of the budget the transaction is coming from';
+
+COMMENT ON COLUMN finance_transaction_invoices.transaction_from_budget_name IS 'The name of the budget the transation is comign from';
+
+COMMENT ON COLUMN finance_transaction_invoices.invoice_id IS 'UUID of this invoice';
+
+COMMENT ON COLUMN finance_transaction_invoices.invoice_line_id IS 'UUID of the invoice line associated with this fund distribution';
+
+COMMENT ON COLUMN finance_transaction_invoices.transaction_type IS 'This describes the type of transaction';
+
+COMMENT ON COLUMN finance_transaction_invoices.invoice_date IS 'Invoice date';
+
+COMMENT ON COLUMN finance_transaction_invoices.invoice_payment_date IS 'When the invoice was actually paid';
+
+COMMENT ON COLUMN finance_transaction_invoices.invoice_exchange_rate IS 'Exchange rate';
+
+COMMENT ON COLUMN finance_transaction_invoices.invoice_line_total IS 'Total of each separate invoice line';
+
+COMMENT ON COLUMN finance_transaction_invoices.invoice_currency IS 'Ideally this is the ISO code and not something the user defines';
+
+COMMENT ON COLUMN finance_transaction_invoices.po_line_id IS 'UUID of encumbrance record associated with this fund distribution';
+
+COMMENT ON COLUMN finance_transaction_invoices.invoice_vendor_id IS 'UUID for vendor';
+
+COMMENT ON COLUMN finance_transaction_invoices.invoice_vendor_name IS 'Name of vendor';
 
 VACUUM ANALYZE finance_transaction_invoices;
