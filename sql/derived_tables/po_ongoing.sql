8ae193862aa60fb00e1ec8abb75848e4c0de4aa7
Nassib Nassar <nrn@etymon.com>
2022-11-08 23:57:08 -0500

    Use timestamptz for extracting metadata createdDate fields

--- sql/derived_tables/po_ongoing.sql
+++ sql/derived_tables/po_ongoing.sql
@@ -4,11 +4,11 @@ DROP TABLE IF EXISTS po_ongoing;
 CREATE TABLE po_ongoing AS
 SELECT
     po.id AS po_id,
-    json_extract_path_text(data, 'ongoing', 'interval')::INT AS po_ongoing_interval,
-    json_extract_path_text(data, 'ongoing', 'isSubscription')::BOOLEAN AS po_ongoing_is_subscription,
-    json_extract_path_text(data, 'ongoing', 'manualRenewal')::BOOLEAN AS po_ongoing_manual_renewal,
-    json_extract_path_text(data, 'ongoing', 'renewalDate')::TIMESTAMP WITH TIME ZONE AS po_ongoing_renewal_date,
-    json_extract_path_text(data, 'ongoing', 'reviewPeriod')::INT AS po_ongoing_review_period
+    json_extract_path_text(data, 'ongoing', 'interval')::integer AS po_ongoing_interval,
+    json_extract_path_text(data, 'ongoing', 'isSubscription')::boolean AS po_ongoing_is_subscription,
+    json_extract_path_text(data, 'ongoing', 'manualRenewal')::boolean AS po_ongoing_manual_renewal,
+    json_extract_path_text(data, 'ongoing', 'renewalDate')::timestamptz AS po_ongoing_renewal_date,
+    json_extract_path_text(data, 'ongoing', 'reviewPeriod')::integer AS po_ongoing_review_period
 FROM
     po_purchase_orders AS po;
 
