1bd82651f64ba6f3b6acaddc7ab6a6e77b16b081
Angela Zoss <angela.zoss@gmail.com>
2022-11-09 16:12:36 -0500

    adding documentation and removing tabs from LDP1 version

--- sql/derived_tables/locations_service_points.sql
+++ sql/derived_tables/locations_service_points.sql
@@ -1,19 +1,23 @@
+-- Creates a derived table that extracts the service points array from 
+-- locations and creates a direct connection between the locations and
+-- all of their service points.
+ 
 DROP TABLE IF EXISTS locations_service_points;
 
 CREATE TABLE locations_service_points AS
     SELECT 
-    	service_points.data #>> '{}' AS service_point_id,  
-    	isp.discovery_display_name AS service_point_discovery_display_name,
-    	isp."name" AS service_point_name,
-    	ll.location_id,
-    	ll.discovery_display_name AS location_discovery_display_name,
-    	ll.location_name,
-    	ll.library_id,
-    	ll.library_name,
-    	ll.campus_id, 
-    	ll.campus_name,
-    	ll.institution_id,
-    	ll.institution_name 
+        service_points.data #>> '{}' AS service_point_id,  
+        isp.discovery_display_name AS service_point_discovery_display_name,
+        isp."name" AS service_point_name,
+        ll.location_id,
+        ll.discovery_display_name AS location_discovery_display_name,
+        ll.location_name,
+        ll.library_id,
+        ll.library_name,
+        ll.campus_id, 
+        ll.campus_name,
+        ll.institution_id,
+        ll.institution_name 
     FROM public.inventory_locations AS il
         CROSS JOIN json_array_elements(json_extract_path(il.data, 'servicePointIds')) AS service_points (data)
         LEFT JOIN public.inventory_service_points AS isp ON service_points.data #>> '{}' = isp.id 
@@ -24,7 +28,7 @@ CREATE INDEX ON locations_service_points (service_point_id);
 
 CREATE INDEX ON locations_service_points (service_point_discovery_display_name);
 
-CREATE INDEX ON locations_service_points (service_point_name);	
+CREATE INDEX ON locations_service_points (service_point_name);
 
 CREATE INDEX ON locations_service_points (location_id);
 
