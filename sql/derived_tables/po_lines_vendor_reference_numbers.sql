43fec2cdb00d2881970aa5be0605376a95fdc52e
Natalya Pikulik <62571646+natalyapik@users.noreply.github.com>
2023-02-06 16:04:53 -0500

    Update po_lines_vendor_reference_numbers.sql
    added comments to LDP po_lines_vendor_reference_numbers.sql

--- sql/derived_tables/po_lines_vendor_reference_numbers.sql
+++ sql/derived_tables/po_lines_vendor_reference_numbers.sql
@@ -23,3 +23,15 @@ CREATE INDEX ON po_lines_vendor_reference_numbers (vendor_reference_number_type)
 CREATE INDEX ON po_lines_vendor_reference_numbers (vendor_instructions);
 
 VACUUM ANALYZE  po_lines_vendor_reference_numbers;
+
+COMMENT ON COLUMN po_lines_vendor_reference_number.po_line_id IS 'UUID identifying this purchase order line';
+
+COMMENT ON COLUMN po_lines_vendor_reference_number.po_line_number IS 'A human readable number assigned to this PO line';
+
+COMMENT ON COLUMN po_lines_vendor_reference_number.vendor_reference_number IS 'A reference number for this purchase order line';
+
+COMMENT ON COLUMN po_lines_vendor_reference_number.vendor_reference_number_type IS 'The reference number type';
+
+COMMENT ON COLUMN po_lines_vendor_reference_number.vendor_instructions IS 'Special instructions for the vendor';
+
+
