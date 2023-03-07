c5b8a19cd4d25aac7e099f4ca2f460f669e862e3
Natalya Pikulik <62571646+natalyapik@users.noreply.github.com>
2023-03-07 16:55:38 -0500

    Update instance_series.sql
    edit json data extract

--- sql/derived_tables/instance_series.sql
+++ sql/derived_tables/instance_series.sql
@@ -6,12 +6,13 @@ CREATE TABLE instance_series AS
 SELECT
     instances.id AS instance_id,
     instances.hrid AS instance_hrid,
-    series.data #>> '{}' AS series,
+    json_extract_path_text(series.data, 'value') AS series,
     series.ordinality AS series_ordinality
 FROM
     inventory_instances AS instances
-    CROSS JOIN LATERAL json_array_elements(json_extract_path(data, 'series')) WITH ORDINALITY AS series (data);
-
+   CROSS JOIN json_array_elements(json_extract_path(instances.data, 'series'))
+   WITH ORDINALITY AS series (data);
+   
 CREATE INDEX ON instance_series (instance_id);
 
 CREATE INDEX ON instance_series (instance_hrid);
@@ -21,4 +22,12 @@ CREATE INDEX ON instance_series (series);
 CREATE INDEX ON instance_series (series_ordinality);
 
 VACUUM ANALYZE instance_series;
+
+COMMENT ON COLUMN instance_series.instance_id IS 'UUID of the instance record';
+
+COMMENT ON COLUMN instance_series.instance_hrid IS 'A human readable system-assigned sequential ID which maps to the Instance ID';
+
+COMMENT ON COLUMN instance_series.series IS 'Series title value';
+
+COMMENT ON COLUMN instance_series.series_ordinality IS 'Series title value ordinality';
 
