61d54ff5d5773d1beb96f08170cf9a374b38ec70
Natalya Pikulik <62571646+natalyapik@users.noreply.github.com>
2023-03-29 14:01:53 -0400

    Update po_lines_vendor_reference_numbers.sql
    changed table names

--- sql/derived_tables/po_lines_vendor_reference_numbers.sql
+++ sql/derived_tables/po_lines_vendor_reference_numbers.sql
@@ -12,26 +12,26 @@ SELECT
 FROM po_lines
     CROSS JOIN json_array_elements(json_extract_path(po_lines.data, 'vendorDetail', 'referenceNumbers')) AS numbers (data);
 
-CREATE INDEX ON po_lines_vendor_reference_numbers (po_line_id);
+CREATE INDEX ON po_line_vendor_reference_numbers (po_line_id);
 
-CREATE INDEX ON po_lines_vendor_reference_numbers (po_line_number);
+CREATE INDEX ON po_line_vendor_reference_numbers (po_line_number);
 
-CREATE INDEX ON po_lines_vendor_reference_numbers (vendor_reference_number);
+CREATE INDEX ON po_line_vendor_reference_numbers (vendor_reference_number);
 
-CREATE INDEX ON po_lines_vendor_reference_numbers (vendor_reference_number_type);
+CREATE INDEX ON po_line_vendor_reference_numbers (vendor_reference_number_type);
 
-CREATE INDEX ON po_lines_vendor_reference_numbers (vendor_instructions);
+CREATE INDEX ON po_line_vendor_reference_numbers (vendor_instructions);
 
-VACUUM ANALYZE  po_lines_vendor_reference_numbers;
+VACUUM ANALYZE  po_line_vendor_reference_numbers;
 
-COMMENT ON COLUMN po_lines_vendor_reference_numbers.po_line_id IS 'UUID identifying this purchase order line';
+COMMENT ON COLUMN po_line_vendor_reference_numbers.po_line_id IS 'UUID identifying this purchase order line';
 
-COMMENT ON COLUMN po_lines_vendor_reference_numbers.po_line_number IS 'A human readable number assigned to this PO line';
+COMMENT ON COLUMN po_line_vendor_reference_numbers.po_line_number IS 'A human readable number assigned to this PO line';
 
-COMMENT ON COLUMN po_lines_vendor_reference_numbers.vendor_reference_number IS 'A reference number for this purchase order line';
+COMMENT ON COLUMN po_line_vendor_reference_numbers.vendor_reference_number IS 'A reference number for this purchase order line';
 
-COMMENT ON COLUMN po_lines_vendor_reference_numbers.vendor_reference_number_type IS 'The reference number type';
+COMMENT ON COLUMN po_line_vendor_reference_numbers.vendor_reference_number_type IS 'The reference number type';
 
-COMMENT ON COLUMN po_lines_vendor_reference_numbers.vendor_instructions IS 'Special instructions for the vendor';
+COMMENT ON COLUMN po_line_vendor_reference_numbers.vendor_instructions IS 'Special instructions for the vendor';
 
 
