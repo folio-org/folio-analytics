06581b2e531df6cb22ba2ad881a1ce1c59bc6638
Nassib Nassar <nrn@etymon.com>
2023-04-04 23:24:48 -0400

    Add GitHub CI tests for Metadb derived tables

---
+++ .github/workflows/runtests.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+cd sql_metadb/derived_tables
+# check for duplicates in runlist
+if [[ $(sort runlist.txt | uniq -d) ]]; then
+       echo "runtests.sh: runlist.txt contains duplicates: `sort runlist.txt | uniq -d`"
+       exit 1
+fi
+for f in $( cat runlist.txt ); do
+       if ! PGOPTIONS='--client-min-messages=warning' psql -bq -d metadb -c '\set ON_ERROR_STOP on' -c 'set search_path = folio_derived' -f $f -U runner ; then
+               exit 1
+       fi
+done || exit 1
+
