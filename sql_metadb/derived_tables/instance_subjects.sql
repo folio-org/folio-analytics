b00c9d2bd6992fb9aeb4e1b9a0d30149023a93ea
Nassib Nassar <nrn@etymon.com>
2023-05-26 16:31:44 -0400

    Remove extra join from instance_subjects

--- sql_metadb/derived_tables/instance_subjects.sql
+++ sql_metadb/derived_tables/instance_subjects.sql
@@ -6,12 +6,10 @@ DROP TABLE IF EXISTS instance_subjects;
 
 CREATE TABLE instance_subjects AS 
 SELECT 
-    inst.id AS instance_id,
-    inst.hrid AS instance_hrid,
+    (instance.jsonb->>'id')::uuid AS instance_id,
+    instance.jsonb->>'hrid' AS instance_hrid,
     instsubjects.jsonb #>> '{}' AS subjects,
     instsubjects.ordinality AS subjects_ordinality
 FROM 
-    folio_inventory.instance__t AS inst
-    LEFT JOIN folio_inventory.instance ON instance.id = inst.id
+    folio_inventory.instance
     CROSS JOIN LATERAL jsonb_array_elements(jsonb_extract_path(instance.jsonb, 'subjects')) WITH ORDINALITY AS instsubjects (jsonb);
-
