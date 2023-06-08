ae3a7d36c6ecf12ba2ead7c4a7b905e63727d0d4
Wayne Schneider <wayne@indexdata.com>
2023-06-08 11:44:58 -0500

    Fix typo in COMMENT statements for derived table po_lines_vendor_reference_numbers (#770)
    Fixes #769

--- sql/derived_tables/po_lines_vendor_reference_numbers.sql
+++ sql/derived_tables/po_lines_vendor_reference_numbers.sql
@@ -12,14 +12,12 @@ SELECT
 FROM po_lines
     CROSS JOIN json_array_elements(json_extract_path(po_lines.data, 'vendorDetail', 'referenceNumbers')) AS numbers (data);
 
-COMMENT ON COLUMN po_line_vendor_reference_numbers.po_line_id IS 'UUID identifying this purchase order line';
+COMMENT ON COLUMN po_lines_vendor_reference_numbers.po_line_id IS 'UUID identifying this purchase order line';
 
-COMMENT ON COLUMN po_line_vendor_reference_numbers.po_line_number IS 'A human readable number assigned to this PO line';
+COMMENT ON COLUMN po_lines_vendor_reference_numbers.po_line_number IS 'A human readable number assigned to this PO line';
 
-COMMENT ON COLUMN po_line_vendor_reference_numbers.vendor_reference_number IS 'A reference number for this purchase order line';
+COMMENT ON COLUMN po_lines_vendor_reference_numbers.vendor_reference_number IS 'A reference number for this purchase order line';
 
-COMMENT ON COLUMN po_line_vendor_reference_numbers.vendor_reference_number_type IS 'The reference number type';
+COMMENT ON COLUMN po_lines_vendor_reference_numbers.vendor_reference_number_type IS 'The reference number type';
 
-COMMENT ON COLUMN po_line_vendor_reference_numbers.vendor_instructions IS 'Special instructions for the vendor';
-
-
+COMMENT ON COLUMN po_lines_vendor_reference_numbers.vendor_instructions IS 'Special instructions for the vendor';
