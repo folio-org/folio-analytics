6be81702742798707d6e3097d4f4a3321078405b
eliana-flo <66032622+eliana-flo@users.noreply.github.com>
2023-06-09 14:11:09 -0400

    Update loans_renewal_count.sql
    Moved the documentation for the query to the top. Removed INDEX and VACUUM ANALYZE lines.

--- sql/derived_tables/loans_renewal_count.sql
+++ sql/derived_tables/loans_renewal_count.sql
@@ -1,9 +1,10 @@
-DROP TABLE IF EXISTS loans_renewal_count;
-
 -- Create a derived table that contains all items from inventory_items
 -- and adds circulation info (total loans and renewals) from
 -- circulation_loans; add in a date to show when the report was last
 -- run and fill circulation nulls with zero.
+
+DROP TABLE IF EXISTS loans_renewal_count;
+
 CREATE TABLE loans_renewal_count AS
 WITH loan_count AS (
     SELECT
@@ -25,14 +26,6 @@ FROM
     inventory_items AS it
     LEFT JOIN loan_count AS lc ON it.id = lc.item_id;
 
-CREATE INDEX ON loans_renewal_count (current_as_of_date);
-
-CREATE INDEX ON loans_renewal_count (item_id);
-
-CREATE INDEX ON loans_renewal_count (num_loans);
-
-CREATE INDEX ON loans_renewal_count (num_renewals);
-
 COMMENT ON COLUMN loans_renewal_count.current_as_of_date IS 'Current date when report was run';
 
 COMMENT ON COLUMN loans_renewal_count.item_id IS 'Item UUID';
@@ -43,5 +36,5 @@ COMMENT ON COLUMN loans_renewal_count.num_loans IS 'Count of loans associated wi
 
 COMMENT ON COLUMN loans_renewal_count.num_renewals IS 'Count of renewals associated with the item';
 
-VACUUM ANALYZE loans_renewal_count;
+
 
