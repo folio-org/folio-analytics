9e775833ce4d397b0842f75329e31ad9dadb91bb
Nassib Nassar <nrn@etymon.com>
2023-06-18 19:58:58 -0400

    Cast UUID in po_prod_ids

--- sql/derived_tables/po_prod_ids.sql
+++ sql/derived_tables/po_prod_ids.sql
@@ -6,7 +6,7 @@ WITH po_prod_id AS (
         pl.id AS pol_id,
         pl.po_line_number AS pol_number,
         (product_ids.data #>> '{productId}')::uuid AS prod_id,
-        product_ids.data #>> '{productIdType}' AS prod_id_type
+        (product_ids.data #>> '{productIdType}')::uuid AS prod_id_type
     FROM
         po_lines AS pl
         CROSS JOIN jsonb_extract_path(pl.data::jsonb, 'details') AS details (data)
