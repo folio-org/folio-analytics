5a585f137c455d2a6578cc016765e6629718dc72
Nassib Nassar <nrn@etymon.com>
2023-06-24 22:45:03 -0400

    Remove varchar cast in instance_formats

--- sql_metadb/derived_tables/instance_formats.sql
+++ sql_metadb/derived_tables/instance_formats.sql
@@ -1,7 +1,6 @@
 --metadb:table instance_formats
 
 -- Create derived table for instance formats bringing together the identifier and name
--- Note: Because of inaccurate data in FOLIO, instance_format_id is a varchar and the ift.id has to be cast as a varchar.
 
 DROP TABLE IF EXISTS instance_formats;
 
@@ -10,11 +9,12 @@ WITH instances AS (
     SELECT
         i.id AS instance_id,
         jsonb_extract_path_text(i.jsonb, 'hrid') AS instance_hrid,
-        instance_format_ids.jsonb #>> '{}' AS instance_format_id,
+        (instance_format_ids.jsonb #>> '{}')::uuid AS instance_format_id,
         instance_format_ids.ordinality AS instance_format_ordinality
     FROM
         folio_inventory.instance AS i
-        CROSS JOIN LATERAL jsonb_array_elements(jsonb_extract_path(i.jsonb, 'instanceFormatIds')) WITH ORDINALITY AS instance_format_ids (jsonb)
+        CROSS JOIN LATERAL jsonb_array_elements(jsonb_extract_path(i.jsonb, 'instanceFormatIds'))
+            WITH ORDINALITY AS instance_format_ids (jsonb)
 )
 SELECT
     it.instance_id,
@@ -26,6 +26,4 @@ SELECT
     ift.source AS instance_format_source
 FROM
     instances AS it
-    LEFT JOIN folio_inventory.instance_format__t AS ift ON it.instance_format_id = ift.id::varchar;
-
-
+    LEFT JOIN folio_inventory.instance_format__t AS ift ON it.instance_format_id = ift.id;
