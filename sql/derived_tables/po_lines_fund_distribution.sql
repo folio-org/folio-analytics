4e6e4fcef88e5ad7ff0ed59dec9320dd94d06819
Nassib Nassar <nrn@etymon.com>
2023-06-28 16:58:46 -0400

    Add derived table to extract fund distribution from po_lines

---
+++ sql/derived_tables/po_lines_fund_distribution.sql
@@ -0,0 +1,14 @@
+DROP TABLE IF EXISTS po_lines_fund_distribution;
+
+CREATE TABLE po_lines_fund_distribution AS
+SELECT p.id AS po_lines_id,
+       f.data #>> '{code}' AS code,
+       f.data #>> '{distributionType}' AS distribution_type,
+       (f.data #>> '{encumbrance}')::uuid AS encumbrance,
+       (f.data #>> '{expenseClassId}')::uuid AS expense_class_id,
+       (f.data #>> '{fundId}')::uuid AS fund_id,
+       (f.data #>> '{value}')::numeric AS value,
+       f.ordinality
+    FROM public.po_lines AS p
+        CROSS JOIN LATERAL jsonb_array_elements((data #> '{fundDistribution}')::jsonb) WITH ORDINALITY
+            AS f (data);
