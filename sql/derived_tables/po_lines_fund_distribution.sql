92b62956e3149ef4c1f2e5f2857ffce6eec7ff76
Nassib Nassar <nrn@etymon.com>
2023-08-04 12:00:01 -0400

    Adjustments to po_lines_fund_distribution (#788)
    * Use new form of JSON extraction for LDP
    * Change column names to match source data
    * Remove indexes for non-ID columns
    * Remove VACUUM ANALYZE
    * Use lateral join for array extraction

--- sql/derived_tables/po_lines_fund_distribution.sql
+++ sql/derived_tables/po_lines_fund_distribution.sql
@@ -1,45 +1,39 @@
--- This derived table shows data of the fund distribution of the purchase order lines
+-- This derived table shows data of the fund distribution of the
+-- purchase order lines.
 
 DROP TABLE IF EXISTS po_lines_fund_distribution;
 
 CREATE TABLE po_lines_fund_distribution AS
-SELECT
-    po_lines.id AS pol_id,
-    json_extract_path_text(json_array_elements(po_lines.data->'fundDistribution'), 'fundId') AS pol_distr_fund_id,
-    json_extract_path_text(json_array_elements(po_lines.data->'fundDistribution'), 'value')::numeric(19, 4) AS pol_distr_value,
-    json_extract_path_text(json_array_elements(po_lines.data->'fundDistribution'), 'distributionType') AS pol_distr_type,
-    json_extract_path_text(json_array_elements(po_lines.data->'fundDistribution'), 'code') AS pol_distr_code,
-    json_extract_path_text(json_array_elements(po_lines.data->'fundDistribution'), 'encumbrance') AS pol_distr_encumbrance_id,
-    json_extract_path_text(json_array_elements(po_lines.data->'fundDistribution'), 'expenseClassId') AS pol_distr_expense_class_id
-FROM
-    public.po_lines;
+SELECT p.id AS po_lines_id,
+       f.data #>> '{code}' AS code,
+       f.data #>> '{distributionType}' AS distribution_type,
+       (f.data #>> '{encumbrance}')::uuid AS encumbrance,
+       (f.data #>> '{expenseClassId}')::uuid AS expense_class_id,
+       (f.data #>> '{fundId}')::uuid AS fund_id,
+       (f.data #>> '{value}')::numeric(19, 4) AS value,
+       f.ordinality
+    FROM public.po_lines AS p
+        CROSS JOIN LATERAL jsonb_array_elements((data #> '{fundDistribution}')::jsonb) WITH ORDINALITY
+            AS f (data);
 
-CREATE INDEX ON po_lines_fund_distribution (pol_id);
+CREATE INDEX ON po_lines_fund_distribution (po_lines_id);
 
-CREATE INDEX ON po_lines_fund_distribution (pol_distr_fund_id);
+CREATE INDEX ON po_lines_fund_distribution (encumbrance);
 
-CREATE INDEX ON po_lines_fund_distribution (pol_distr_value);
+CREATE INDEX ON po_lines_fund_distribution (expense_class_id);
 
-CREATE INDEX ON po_lines_fund_distribution (pol_distr_type);
+CREATE INDEX ON po_lines_fund_distribution (fund_id);
 
-CREATE INDEX ON po_lines_fund_distribution (pol_distr_code);
+COMMENT ON COLUMN po_lines_fund_distribution.po_lines_id IS 'UUID identifying this purchase order line';
 
-CREATE INDEX ON po_lines_fund_distribution (pol_distr_encumbrance_id);
+COMMENT ON COLUMN po_lines_fund_distribution.code IS 'The fund code';
 
-CREATE INDEX ON po_lines_fund_distribution (pol_distr_expense_class_id);
+COMMENT ON COLUMN po_lines_fund_distribution.distribution_type IS 'Percentage or amount type of the value property';
 
-COMMENT ON COLUMN po_lines_fund_distribution.pol_id IS 'UUID identifying this purchase order line';
+COMMENT ON COLUMN po_lines_fund_distribution.encumbrance IS 'UUID of encumbrance record associated with this fund distribution';
 
-COMMENT ON COLUMN po_lines_fund_distribution.pol_distr_fund_id IS 'UUID of the fund associated with this fund distribution';
+COMMENT ON COLUMN po_lines_fund_distribution.expense_class_id IS 'UUID of the expense class associated with this fund distribution';
 
-COMMENT ON COLUMN po_lines_fund_distribution.pol_distr_value IS 'The value of the cost to be applied to this fund';
+COMMENT ON COLUMN po_lines_fund_distribution.fund_id IS 'UUID of the fund associated with this fund distribution';
 
-COMMENT ON COLUMN po_lines_fund_distribution.pol_distr_type IS 'Percentage or amount type of the value property';
-
-COMMENT ON COLUMN po_lines_fund_distribution.pol_distr_code IS 'The fund code';
-
-COMMENT ON COLUMN po_lines_fund_distribution.pol_distr_encumbrance_id IS 'UUID of encumbrance record associated with this fund distribution';
-
-COMMENT ON COLUMN po_lines_fund_distribution.pol_distr_expense_class_id IS 'UUID of the expense class associated with this fund distribution';
-
-VACUUM ANALYZE po_lines_fund_distribution;
+COMMENT ON COLUMN po_lines_fund_distribution.value IS 'The value of the cost to be applied to this fund';
