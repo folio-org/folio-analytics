a1249584cddcbf5da8e79e78c7282022b01525a4
Nassib Nassar <nrn@etymon.com>
2023-09-06 19:33:48 -0400

    Update sample report format

--- reports/count_loans_and_renewals/count_loans_and_renewals.sql
+++ reports/count_loans_and_renewals/count_loans_and_renewals.sql
@@ -1,3 +1,5 @@
+--metadb:report count_loans_and_renewals
+
 -- Report produces a list of individual loans which can then be
 -- grouped and summed to create loans and renewals counts.
 
@@ -33,8 +35,8 @@ RETURNS TABLE(
     effective_location_name text,
     permanent_location_library_name text,
     permanent_location_campus_name text,
-    permanent_location_institution_name text) AS
-$$
+    permanent_location_institution_name text)
+AS $$
 SELECT start_date || ' to ' || end_date AS date_range,
        loan_date::date,
        loan_due_date AS loan_due_date,
