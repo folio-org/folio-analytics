29a7999f82d372f047cfd9dc9de51a9276afe494
Arthur Aguilera <49205333+arthur-aguilera@users.noreply.github.com>
2023-11-09 09:38:17 -0700

    Update feesfines_accounts_actions.sql
    Added ';' at the end of columns; added back in documentation for the table; changed payment_status_name to payment_status, and removed comment or operator_id

--- sql_metadb/derived_tables/feesfines_accounts_actions.sql
+++ sql_metadb/derived_tables/feesfines_accounts_actions.sql
@@ -1,4 +1,6 @@
 --metadb:table feesfines_accounts_actions	
+--Creates a derived table that takes folio_feesfines.accounts as the main table
+--Payments and credits are shown in as a negative balance for accounting purposes
 
 DROP TABLE IF EXISTS feesfines_accounts_actions;
 
@@ -15,7 +17,7 @@ SELECT
     jsonb_extract_path_text(fa.jsonb, 'feeFineType') AS fee_fine_type,
     jsonb_extract_path_text(fa.jsonb, 'materialTypeId')::uuid AS material_type_id,
     jsonb_extract_path_text(fa.jsonb, 'materialType') AS material_type,
-    jsonb_extract_path_text(fa.jsonb, 'paymentStatus', 'name') AS payment_status_name,
+    jsonb_extract_path_text(fa.jsonb, 'paymentStatus', 'name') AS payment_status,
     jsonb_extract_path_text(fa.jsonb, 'status', 'name') AS fine_status, -- open or closed
     jsonb_extract_path_text(fa.jsonb, 'userId')::uuid AS account_user_id,
     ff.id AS transaction_id,
@@ -43,54 +45,52 @@ FROM
     LEFT JOIN folio_users.groups__t AS ug ON uu.patron_group = ug.id
 ORDER BY fine_account_id, transaction_date;
 
-COMMENT ON COLUMN feesfines_accounts_actions.fine_account_id IS 'User fine/fee account id, UUID'
+COMMENT ON COLUMN feesfines_accounts_actions.fine_account_id IS 'User fine/fee account id, UUID';
 
-COMMENT ON COLUMN feesfines_accounts_actions.fine_account_amount IS 'Amount of the fine/fee'
+COMMENT ON COLUMN feesfines_accounts_actions.fine_account_amount IS 'Amount of the fine/fee';
 
-COMMENT ON COLUMN feesfines_accounts_actions.fine_date IS 'Date and time the account of the fine/fee was created'
+COMMENT ON COLUMN feesfines_accounts_actions.fine_date IS 'Date and time the account of the fine/fee was created';
 
-COMMENT ON COLUMN feesfines_accounts_actions.fine_updated_date IS 'Date and time the account of the fine/fee was updated'
+COMMENT ON COLUMN feesfines_accounts_actions.fine_updated_date IS 'Date and time the account of the fine/fee was updated';
 
-COMMENT ON COLUMN feesfines_accounts_actions.fee_fine_id IS 'ID of the fee/fine'
+COMMENT ON COLUMN feesfines_accounts_actions.fee_fine_id IS 'ID of the fee/fine';
 
-COMMENT ON COLUMN feesfines_accounts_actions.owner_id IS 'ID of the account owner'
+COMMENT ON COLUMN feesfines_accounts_actions.owner_id IS 'ID of the account owner';
 
-COMMENT ON COLUMN feesfines_accounts_actions.fee_fine_owner IS 'Owner of the account'
+COMMENT ON COLUMN feesfines_accounts_actions.fee_fine_owner IS 'Owner of the account';
 
-COMMENT ON COLUMN feesfines_accounts_actions.fee_fine_type IS 'Fee/fine that is up to the desecration of the user'
+COMMENT ON COLUMN feesfines_accounts_actions.fee_fine_type IS 'Fee/fine that is up to the desecration of the user';
 
-COMMENT ON COLUMN feesfines_accounts_actions.material_type_id IS 'ID of the material type of the item'
+COMMENT ON COLUMN feesfines_accounts_actions.material_type_id IS 'ID of the material type of the item';
 
-COMMENT ON COLUMN feesfines_accounts_actions.material_type IS 'Material type of the item'
+COMMENT ON COLUMN feesfines_accounts_actions.material_type IS 'Material type of the item';
 
-COMMENT ON COLUMN feesfines_accounts_actions.payment_status IS 'Overall status of the payment/waive/transfer/refund/cancel'
+COMMENT ON COLUMN feesfines_accounts_actions.payment_status IS 'Overall status of the payment/waive/transfer/refund/cancel';
 
-COMMENT ON COLUMN feesfines_accounts_actions.fine_status IS 'Overall status of the fee/fine'
+COMMENT ON COLUMN feesfines_accounts_actions.fine_status IS 'Overall status of the fee/fine';
 
-COMMENT ON COLUMN feesfines_accounts_actions.account_user_id IS 'ID of the user'
+COMMENT ON COLUMN feesfines_accounts_actions.account_user_id IS 'ID of the user';
 
-COMMENT ON COLUMN feesfines_accounts_actions.transaction_id IS 'Fine/fee action id, UUID'
+COMMENT ON COLUMN feesfines_accounts_actions.transaction_id IS 'Fine/fee action id, UUID';
 
-COMMENT ON COLUMN feesfines_accounts_actions.transaction_amount IS 'Amount of activity'
+COMMENT ON COLUMN feesfines_accounts_actions.transaction_amount IS 'Amount of activity';
 
-COMMENT ON COLUMN feesfines_accounts_actions.account_balance IS 'Calculated amount of remaining balance based on original fee/fine and what has been paid/waived/transferred/refunded'
+COMMENT ON COLUMN feesfines_accounts_actions.account_balance IS 'Calculated amount of remaining balance based on original fee/fine and what has been paid/waived/transferred/refunded';
 
-COMMENT ON COLUMN feesfines_accounts_actions.type_action IS 'Type of activity including the type of transaction'
+COMMENT ON COLUMN feesfines_accounts_actions.type_action IS 'Type of activity including the type of transaction';
 
-COMMENT ON COLUMN feesfines_accounts_actions.transaction_date IS 'Date and time the transaction of the fine/fee was created'
+COMMENT ON COLUMN feesfines_accounts_actions.transaction_date IS 'Date and time the transaction of the fine/fee was created';
 
-COMMENT ON COLUMN feesfines_accounts_actions.transaction_location IS 'The service point where the action was created'
+COMMENT ON COLUMN feesfines_accounts_actions.transaction_location IS 'The service point where the action was created';
 
-COMMENT ON COLUMN feesfines_accounts_actions.transaction_information IS 'Number or other transaction id related to payment'
+COMMENT ON COLUMN feesfines_accounts_actions.transaction_information IS 'Number or other transaction id related to payment';
 
-COMMENT ON COLUMN feesfines_accounts_actions.operator_id IS 'Person who processed activity (from login information)'
+COMMENT ON COLUMN feesfines_accounts_actions.payment_method IS 'Overall status of the action-setting';
 
-COMMENT ON COLUMN feesfines_accounts_actions.payment_method IS 'Overall status of the action-setting'
+COMMENT ON COLUMN feesfines_accounts_actions.user_id IS 'User UUID';
 
-COMMENT ON COLUMN feesfines_accounts_actions.user_id IS 'User UUID'
+COMMENT ON COLUMN feesfines_accounts_actions.user_patron_group_id IS 'UUID for user patron group';
 
-COMMENT ON COLUMN feesfines_accounts_actions.user_patron_group_id IS 'UUID for user patron group'
+COMMENT ON COLUMN feesfines_accounts_actions.patron_group_name IS 'User patron group';
 
-COMMENT ON COLUMN feesfines_accounts_actions.patron_group_name IS 'User patron group'
-
-COMMENT ON COLUMN feesfines_accounts_actions.signed_transaction_amount IS 'Identifies the type_action value that decreases the balance of the account and adds a - to those values'
+COMMENT ON COLUMN feesfines_accounts_actions.signed_transaction_amount IS 'Identifies the type_action value that decreases the balance of the account and adds a - to those values';
