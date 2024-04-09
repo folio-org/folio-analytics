2926369d947a66bd807b6fa4db26eca49ca28354
Nassib Nassar <nrn@etymon.com>
2024-04-09 17:35:58 -0400

    Check for empty string UUID in instance_nature_content

--- sql_metadb/derived_tables/instance_nature_content.sql
+++ sql_metadb/derived_tables/instance_nature_content.sql
@@ -9,21 +9,20 @@ WITH nature_content AS (
     SELECT
         i.id AS instance_id,
         jsonb_extract_path_text(i.jsonb, 'hrid') AS instance_hrid,
-        nature_of_content_term_ids.jsonb #>> '{}' AS nature_of_content_term_id,
+        NULLIF(nature_of_content_term_ids.jsonb #>> '{}', '')::uuid AS nature_of_content_term_id,
         nature_of_content_term_ids.ordinality AS nature_of_content_ordinality
     FROM
         folio_inventory.instance AS i
-        CROSS JOIN jsonb_array_elements(jsonb_extract_path(i.jsonb, 'natureOfContentTermIds')) WITH ORDINALITY AS nature_of_content_term_ids (jsonb)
+        CROSS JOIN jsonb_array_elements(jsonb_extract_path(i.jsonb, 'natureOfContentTermIds'))
+            WITH ORDINALITY AS nature_of_content_term_ids (jsonb)
 )
 SELECT
     nc.instance_id,
     nc.instance_hrid,
-    nc.nature_of_content_term_id::uuid,
+    nc.nature_of_content_term_id,
     noctt.name AS nature_of_content_term_name,
     noctt.source AS nature_of_content_term_source,
     nc.nature_of_content_ordinality
 FROM
     nature_content AS nc
-    LEFT JOIN folio_inventory.nature_of_content_term__t AS noctt ON nc.nature_of_content_term_id::uuid = noctt.id::uuid;
-
-
+    LEFT JOIN folio_inventory.nature_of_content_term__t AS noctt ON nc.nature_of_content_term_id = noctt.id::uuid;
