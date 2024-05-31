e8d3dd5c7522091281a14e067a9e25428ae4649f
Sharon Markus <36549480+sbeltaine@users.noreply.github.com>
2024-05-31 17:12:33 -0400

    Update finance_transaction_purchase_order.sql
    This update is being submitted on behalf of Joanne Leary. It corrects the PO line acquisition method and PO order type data field extractions from the JSON data array in the finance_transaction_purchase_order derived table for Metadb. It also adds the acquisition method name to the table.

--- sql_metadb/derived_tables/finance_transaction_purchase_order.sql
+++ sql_metadb/derived_tables/finance_transaction_purchase_order.sql
@@ -1,11 +1,13 @@
 --metadb:table finance_transaction_purchase_order
 --metadb:require folio_finance.expense_class__t.id uuid
 
--- Create a derived table that joins purchase orders and po_lines fields to transactions for encumbranced cost reports in system currency
+--This query creates a derived table that joins purchase orders and po_lines fields to 
+--transactions for encumbranced cost reports in system currency.
 
 DROP TABLE IF EXISTS finance_transaction_purchase_order;
 
 CREATE TABLE finance_transaction_purchase_order AS
+
 SELECT
     ft.id AS transaction_id,
     jsonb_extract_path_text(ft.jsonb, 'amount')::numeric(19,4) AS transaction_amount,
@@ -30,21 +32,24 @@ SELECT
     jsonb_extract_path_text(ft.jsonb, 'encumbrance', 'sourcePurchaseOrderId')::uuid AS po_id,
     jsonb_extract_path_text(pol.jsonb, 'poLineNumber') AS pol_number,
     jsonb_extract_path_text(pol.jsonb, 'description') AS pol_description,
-    jsonb_extract_path_text(pol.jsonb, 'acquisition_method') AS pol_acquisition_method,
-    jsonb_extract_path_text(po.jsonb, 'order_type') AS po_order_type,
+    jsonb_extract_path_text(pol.jsonb, 'acquisitionMethod') AS pol_acquisition_method,
+    acqt.value as acquisition_method_name,
+    jsonb_extract_path_text(po.jsonb, 'orderType') AS po_order_type,
     jsonb_extract_path_text(po.jsonb, 'vendor')::uuid AS po_vendor_id,
     jsonb_extract_path_text(oo.jsonb, 'name') AS po_vendor_name
 FROM
     folio_finance.transaction AS ft
     LEFT JOIN folio_orders.po_line AS pol ON jsonb_extract_path_text(ft.jsonb, 'encumbrance', 'sourcePoLineId')::uuid = pol.id
     LEFT JOIN folio_orders.purchase_order AS po ON jsonb_extract_path_text(ft.jsonb, 'encumbrance', 'sourcePurchaseOrderId')::uuid = po.id
+    left join folio_orders.acquisition_method__t as acqt on jsonb_extract_path_text(pol.jsonb, 'acquisitionMethod')::UUID = acqt.id
     LEFT JOIN folio_finance.fund AS ff ON ft.fromfundid = ff.id
     LEFT JOIN folio_finance.budget AS fb ON ft.fromfundid = fb.fundid AND ft.fiscalyearid = fb.fiscalyearid
     LEFT JOIN folio_organizations.organizations AS oo ON jsonb_extract_path_text(po.jsonb, 'vendor')::uuid = oo.id
     LEFT JOIN folio_finance.expense_class__t AS ec ON ec.id = ft.expenseclassid
 WHERE
     jsonb_extract_path_text(ft.jsonb, 'transactionType') = 'Encumbrance';
-
+   
+   
 COMMENT ON COLUMN finance_transaction_purchase_order.transaction_id IS 'UUID of this transaction';
 
 COMMENT ON COLUMN finance_transaction_purchase_order.transaction_amount IS 'The amount of this transaction. For encumbrances: This is initialAmountEncumbered - (amountAwaitingPayment + amountExpended)';
@@ -93,9 +98,7 @@ COMMENT ON COLUMN finance_transaction_purchase_order.pol_description IS 'purchas
 
 COMMENT ON COLUMN finance_transaction_purchase_order.pol_acquisition_method IS 'UUID of the acquisition method for this purchase order line';
 
+COMMENT ON COLUMN finance_transaction_purchase_order.acquisition_method_name IS 'Name of the acquisition method for this purchase order line';
+
 COMMENT ON COLUMN finance_transaction_purchase_order.po_order_type IS 'the purchase order type';
-
-COMMENT ON COLUMN finance_transaction_purchase_order.po_vendor_id IS 'UUID of the vendor record';
-
-COMMENT ON COLUMN finance_transaction_purchase_order.po_vendor_name IS 'The name of vendor';
 
