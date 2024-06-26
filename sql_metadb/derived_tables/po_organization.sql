e424c9e13115045abce81ac4725689f471461887
Nassib Nassar <nrn@etymon.com>
2024-06-25 20:39:41 -0400

    Adjust formatting of po_organization

--- sql_metadb/derived_tables/po_organization.sql
+++ sql_metadb/derived_tables/po_organization.sql
@@ -3,100 +3,81 @@
 --metadb:require folio_organizations.contacts__t.first_name text
 --metadb:require folio_organizations.contacts__t.last_name text
 
---Create a local table for contact information for organization or vendor used in PO.
-
--- 1. Get the contact id from the data blob
+-- Create a local table for contact information for organization or vendor used in PO.
 
 DROP TABLE IF EXISTS po_organization;
 
 CREATE TABLE po_organization AS
-
-WITH recs AS 
-(SELECT DISTINCT
-   oo.id AS org_id,
-   JSONB_EXTRACT_PATH_TEXT (oo.jsonb, 'code') as org_code,
-   JSONB_EXTRACT_PATH_TEXT (oo.jsonb, 'name') as org_name,
-   JSONB_EXTRACT_PATH_TEXT (oo.jsonb, 'isVendor') as is_vendor,
-   JSONB_EXTRACT_PATH_TEXT (JSONB_ARRAY_ELEMENTS (JSONB_EXTRACT_PATH (oo.jsonb, 'aliases')),'value') AS alias,
-   JSONB_EXTRACT_PATH_TEXT (JSONB_ARRAY_ELEMENTS (JSONB_EXTRACT_PATH (oo.jsonb, 'aliases')),'description') AS alias_description,
-   TRIM ('"' from JSONB_ARRAY_ELEMENTS ((oo.jsonb #>'{contacts}')::JSONB)::VARCHAR) AS contact_id
-
-FROM folio_organizations.organizations AS oo
+-- 1. Get the contact id from the data blob
+WITH recs AS (
+SELECT DISTINCT oo.id AS org_id,
+                jsonb_extract_path_text(oo.jsonb, 'code') AS org_code,
+                jsonb_extract_path_text(oo.jsonb, 'name') AS org_name,
+                jsonb_extract_path_text(oo.jsonb, 'isVendor') AS is_vendor,
+                jsonb_extract_path_text(jsonb_array_elements(jsonb_extract_path(oo.jsonb, 'aliases')),'value') AS alias,
+                jsonb_extract_path_text(jsonb_array_elements(jsonb_extract_path(oo.jsonb, 'aliases')),'description') AS alias_description,
+                trim('"' FROM jsonb_array_elements((oo.jsonb #> '{contacts}')::jsonb)::text) AS contact_id
+    FROM folio_organizations.organizations AS oo
 ),
-
 -- 2. Get the contact information from the folio_organizations.contacts table and join to the contact id extracted from the folio_organizations.organizations table
-
-recs2 AS 
-(SELECT DISTINCT
-	recs.org_id,
-	recs.org_name,
-	recs.org_code,
-	recs.is_vendor,
-	recs.alias,
-	recs.alias_description,
-	recs.contact_id AS oo_contact_id,
-	oc.id AS oc_concact_id,
-	oc.creation_date::date AS created_date, 
-	jsonb_extract_path_text (oc.jsonb,'metadata','updatedDate') as updated_date,
-	jsonb_extract_path_text (oc.jsonb,'metadata','updatedByUserId') as updated_by_user_id,
-	concat (jsonb_extract_path_text (uu.jsonb,'personal','lastName'),' ',jsonb_extract_path_text (uu.jsonb,'personal','firstName')) as updated_by,
-	jsonb_extract_path_text (oc.jsonb,'lastname') as contact_last_name,
-	jsonb_extract_path_text (oc.jsonb,'firstName') as contact_first_name,
-	jsonb_extract_path_text (oc.jsonb,'notes') as notes,
-	JSONB_EXTRACT_PATH_TEXT (JSONB_ARRAY_ELEMENTS (JSONB_EXTRACT_PATH (oc.jsonb, 'addresses')),'addressLine1') AS address_line_1,
-	JSONB_EXTRACT_PATH_TEXT (JSONB_ARRAY_ELEMENTS (JSONB_EXTRACT_PATH (oc.jsonb, 'addresses')),'addressLine2') AS address_line_2,
-	JSONB_EXTRACT_PATH_TEXT (JSONB_ARRAY_ELEMENTS (JSONB_EXTRACT_PATH (oc.jsonb, 'addresses')),'city') AS city,
-	JSONB_EXTRACT_PATH_TEXT (JSONB_ARRAY_ELEMENTS (JSONB_EXTRACT_PATH (oc.jsonb, 'addresses')),'stateRegion') AS state_region,
-	JSONB_EXTRACT_PATH_TEXT (JSONB_ARRAY_ELEMENTS (JSONB_EXTRACT_PATH (oc.jsonb, 'addresses')),'country') AS country,
-	JSONB_EXTRACT_PATH_TEXT (JSONB_ARRAY_ELEMENTS (JSONB_EXTRACT_PATH (oc.jsonb, 'addresses')),'zipCode') AS zip_code,
-	JSONB_EXTRACT_PATH_TEXT (JSONB_ARRAY_ELEMENTS (JSONB_EXTRACT_PATH (oc.jsonb, 'addresses')),'isPrimary') AS is_primary,
-	JSONB_EXTRACT_PATH_TEXT (JSONB_ARRAY_ELEMENTS (JSONB_EXTRACT_PATH (oc.jsonb, 'emails')),'value') AS email_address,
-	JSONB_EXTRACT_PATH_TEXT (JSONB_ARRAY_ELEMENTS (JSONB_EXTRACT_PATH (oc.jsonb, 'phoneNumbers')),'phoneNumber') AS phone_number,
-	JSONB_EXTRACT_PATH_TEXT (JSONB_ARRAY_ELEMENTS (JSONB_EXTRACT_PATH (oc.jsonb, 'urls')),'value') AS url,
-	JSONB_EXTRACT_PATH_TEXT (JSONB_ARRAY_ELEMENTS (JSONB_EXTRACT_PATH (oc.jsonb, 'urls')),'description') AS url_description
-
-FROM recs
-	FULL OUTER JOIN folio_organizations.contacts AS oc 
-	ON recs.contact_id::UUID = oc.id::UUID
-	
-	LEFT JOIN folio_users.users AS uu 
-	ON jsonb_extract_path_text (oc.jsonb,'metadata','updatedByUserId')::UUID = uu.id::UUID
+recs2 AS (
+SELECT DISTINCT recs.org_id,
+	        recs.org_name,
+	        recs.org_code,
+	        recs.is_vendor,
+	        recs.alias,
+	        recs.alias_description,
+	        recs.contact_id AS oo_contact_id,
+	        oc.id AS oc_concact_id,
+	        oc.creation_date::date AS created_date,
+	        jsonb_extract_path_text(oc.jsonb, 'metadata', 'updatedDate') AS updated_date,
+	        jsonb_extract_path_text(oc.jsonb, 'metadata', 'updatedByUserId') AS updated_by_user_id,
+	        concat(jsonb_extract_path_text(uu.jsonb, 'personal', 'lastName'), ' ', jsonb_extract_path_text(uu.jsonb, 'personal', 'firstName')) AS updated_by,
+	        jsonb_extract_path_text(oc.jsonb, 'lastname') AS contact_last_name,
+	        jsonb_extract_path_text(oc.jsonb, 'firstName') AS contact_first_name,
+	        jsonb_extract_path_text(oc.jsonb, 'notes') AS notes,
+	        jsonb_extract_path_text(jsonb_array_elements(jsonb_extract_path(oc.jsonb, 'addresses')), 'addressLine1') AS address_line_1,
+	        jsonb_extract_path_text(jsonb_array_elements(jsonb_extract_path(oc.jsonb, 'addresses')), 'addressLine2') AS address_line_2,
+	        jsonb_extract_path_text(jsonb_array_elements(jsonb_extract_path(oc.jsonb, 'addresses')), 'city') AS city,
+	        jsonb_extract_path_text(jsonb_array_elements(jsonb_extract_path(oc.jsonb, 'addresses')), 'stateRegion') AS state_region,
+	        jsonb_extract_path_text(jsonb_array_elements(jsonb_extract_path(oc.jsonb, 'addresses')), 'country') AS country,
+	        jsonb_extract_path_text(jsonb_array_elements(jsonb_extract_path(oc.jsonb, 'addresses')), 'zipCode') AS zip_code,
+	        jsonb_extract_path_text(jsonb_array_elements(jsonb_extract_path(oc.jsonb, 'addresses')), 'isPrimary') AS is_primary,
+	        jsonb_extract_path_text(jsonb_array_elements(jsonb_extract_path(oc.jsonb, 'emails')), 'value') AS email_address,
+	        jsonb_extract_path_text(jsonb_array_elements(jsonb_extract_path(oc.jsonb, 'phoneNumbers')), 'phoneNumber') AS phone_number,
+	        jsonb_extract_path_text(jsonb_array_elements(jsonb_extract_path(oc.jsonb, 'urls')), 'value') AS url,
+	        jsonb_extract_path_text(jsonb_array_elements(jsonb_extract_path(oc.jsonb, 'urls')), 'description') AS url_description
+    FROM recs
+        FULL JOIN folio_organizations.contacts AS oc ON recs.contact_id::uuid = oc.id::uuid
+        LEFT JOIN folio_users.users AS uu ON jsonb_extract_path_text(oc.jsonb, 'metadata', 'updatedByUserId')::uuid = uu.id::uuid
 )
-
 --3. Join results of recs2 to the purchase_order__t table to get the vendor contact information
-
-SELECT distinct
-	ppo.po_number,
-    ppo.vendor AS vendor_id,
-	oo.id AS organization_id,
-	oo.code AS organization_code,
-	oo.name AS organization_name,
-	oo.description AS organization_description,
-	recs2.is_vendor,
-	recs2.contact_first_name,
-	recs2.contact_last_name,
-	recs2.notes,
-	recs2.address_line_1,
-	recs2.address_line_2,
-	recs2.city,
-	recs2.state_region,
-	recs2.country,
-	recs2.zip_code,
-	recs2.is_primary,
-	recs2.email_address,
-	recs2.phone_number,
-	recs2.url,
-	recs2.url_description
-
-FROM folio_organizations.organizations__t AS oo 
-	LEFT JOIN recs2 
-	ON oo.id::UUID = recs2.org_id::UUID
-	
-	INNER JOIN folio_orders.purchase_order__t AS ppo -- join the organization_id to the vendor id in the po_purchase_orders table
-	ON oo.id::UUID = ppo.vendor::UUID
-
-ORDER BY po_number, oo.name
-;
+SELECT DISTINCT ppo.po_number,
+                ppo.vendor AS vendor_id,
+	        oo.id AS organization_id,
+	        oo.code AS organization_code,
+	        oo.name AS organization_name,
+	        oo.description AS organization_description,
+	        recs2.is_vendor,
+	        recs2.contact_first_name,
+	        recs2.contact_last_name,
+	        recs2.notes,
+	        recs2.address_line_1,
+	        recs2.address_line_2,
+	        recs2.city,
+	        recs2.state_region,
+	        recs2.country,
+	        recs2.zip_code,
+	        recs2.is_primary,
+	        recs2.email_address,
+	        recs2.phone_number,
+	        recs2.url,
+	        recs2.url_description
+    FROM folio_organizations.organizations__t AS oo
+        LEFT JOIN recs2 ON oo.id::uuid = recs2.org_id::uuid
+        -- Join the organization_id to the vendor id in the po_purchase_orders table.
+        JOIN folio_orders.purchase_order__t AS ppo ON oo.id::uuid = ppo.vendor::uuid
+    ORDER BY po_number, oo.name;
 
 COMMENT ON COLUMN po_organization.po_number IS 'A human readable number assigned to PO';
 
