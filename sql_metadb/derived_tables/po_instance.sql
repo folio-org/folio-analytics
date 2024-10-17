56bbf24347678616d54079a3c224eadd537518b4
Nassib Nassar <nrn@etymon.com>
2024-10-16 21:51:22 -0400

    Adjust formatting of po_instance

--- sql_metadb/derived_tables/po_instance.sql
+++ sql_metadb/derived_tables/po_instance.sql
@@ -4,54 +4,54 @@ DROP TABLE IF EXISTS po_instance;
 
 CREATE TABLE po_instance AS
 SELECT DISTINCT 
-    purchase_order__t.manual_po::BOOLEAN AS manual_po,
+    purchase_order__t.manual_po::boolean AS manual_po,
     po_line__t.rush AS rush,
     purchase_order__t.order_type AS order_type,
     po_line__t.requester AS requester,
     po_line__t.selector AS selector,
     purchase_order__t.po_number AS po_number,
     po_line.purchaseorderid AS po_number_id,
-    jsonb_extract_path_text(po_line.JSONB,'poLineNumber') AS po_line_number,
+    jsonb_extract_path_text(po_line.jsonb, 'poLineNumber') AS po_line_number,
     po_line.id AS po_line_id,
     organizations__t.code AS vendor_code, 
     users__t.username AS created_by_username,
     purchase_order__t.workflow_status AS po_workflow_status,
-    purchase_order__t.approved::BOOLEAN AS status_approved,
-    jsonb_extract_path_text(purchase_order.jsonb,'metadata','createdDate')::timestamptz AS created_date,  
-    jsonb_extract_path_text(cdt.value::jsonb,'name') AS bill_to,
-    jsonb_extract_path_text(cdt2.value::jsonb,'name') AS ship_to,
+    purchase_order__t.approved::boolean AS status_approved,
+    jsonb_extract_path_text(purchase_order.jsonb, 'metadata', 'createdDate')::timestamptz AS created_date,  
+    jsonb_extract_path_text(cdt.value::jsonb, 'name') AS bill_to,
+    jsonb_extract_path_text(cdt2.value::jsonb, 'name') AS ship_to,
     po_line__t.instance_id AS pol_instance_id,
     instance__t.hrid AS pol_instance_hrid,
-    jsonb_extract_path_text(locations.jsonb,'holdingId')::uuid AS pol_holding_id,
-    jsonb_extract_path_text(locations.jsonb,'locationId')::uuid AS pol_location_id,     
-    COALESCE (location__t.name, location__t2.name,'deleted_holding') AS pol_location_name,
-    CASE 
-        WHEN (location__t.name IS NULL AND location__t2.name IS NULL) THEN 'no sourse'
-        WHEN (location__t.name IS NULL AND location__t2.name IS NOT NULL) THEN 'pol_holding'
-        WHEN (location__t.name IS NOT NULL AND location__t2.name IS NULL) THEN 'pol_location'
-        ELSE 'x' END AS pol_location_source,    
+    jsonb_extract_path_text(locations.jsonb, 'holdingId')::uuid AS pol_holding_id,
+    jsonb_extract_path_text(locations.jsonb, 'locationId')::uuid AS pol_location_id,     
+    COALESCE(location__t.name, location__t2.name, 'deleted_holding') AS pol_location_name,
+    CASE WHEN (location__t.name IS NULL AND location__t2.name IS NULL) THEN 'no sourse'
+         WHEN (location__t.name IS NULL AND location__t2.name IS NOT NULL) THEN 'pol_holding'
+         WHEN (location__t.name IS NOT NULL AND location__t2.name IS NULL) THEN 'pol_location'
+         ELSE 'x'
+    END AS pol_location_source,    
     po_line__t.title_or_package AS title,
     po_line__t.publication_date AS publication_date,
     po_line__t.publisher AS publisher,
-    jsonb_extract_path_text(po_line.jsonb,'paymentStatus') AS payment_status,
-    jsonb_extract_path_text(po_line.jsonb,'receiptStatus') AS receipt_status,
-    users__t2.username as po_updated_by, 
-    jsonb_extract_path_text(purchase_order.jsonb, 'metadata', 'updatedDate')::TIMESTAMPTZ AS po_updated_date, 
+    jsonb_extract_path_text(po_line.jsonb, 'paymentStatus') AS payment_status,
+    jsonb_extract_path_text(po_line.jsonb, 'receiptStatus') AS receipt_status,
+    users__t2.username AS po_updated_by, 
+    jsonb_extract_path_text(purchase_order.jsonb, 'metadata', 'updatedDate')::timestamptz AS po_updated_date, 
     location__t2.name AS holdings_location_name
     FROM folio_orders.po_line 
-         CROSS JOIN LATERAL jsonb_array_elements((po_line.jsonb #> '{locations}')::jsonb) AS locations (data)
-    LEFT JOIN folio_orders.po_line__t  ON po_line.id = po_line__t.id::uuid
-    LEFT JOIN folio_inventory.instance__t ON (jsonb_extract_path_text(po_line.jsonb,'instanceId'))::UUID = instance__t.id::uuid
-    LEFT JOIN folio_inventory.location__t ON (jsonb_extract_path_text(locations.jsonb,'locationId'))::uuid = location__t.id::uuid
-    LEFT JOIN folio_orders.purchase_order__t ON po_line.purchaseorderid = purchase_order__t.id::uuid
-    LEFT JOIN folio_inventory.holdings_record__t ON jsonb_extract_path_text(locations.jsonb, 'holdingId')::uuid = holdings_record__t.id::uuid
-    LEFT JOIN folio_inventory.location__t AS location__t2 ON holdings_record__t.permanent_location_id = location__t2.id 
-    LEFT JOIN folio_organizations.organizations__t ON purchase_order__t.vendor = organizations__t.id
-    LEFT JOIN folio_orders.purchase_order ON purchase_order__t.id::uuid = purchase_order.id
-    LEFT JOIN folio_configuration.config_data__t AS cdt ON jsonb_extract_path_text(purchase_order.jsonb,'billTo')::uuid = cdt.id::uuid
-    LEFT JOIN folio_configuration.config_data__t AS cdt2 ON jsonb_extract_path_text(purchase_order.jsonb,'shipTo')::uuid = cdt2.id::uuid
-    LEFT JOIN folio_users.users__t ON jsonb_extract_path_text(purchase_order.jsonb,'metadata','createdByUserId')::uuid = users__t.id::uuid
-    LEFT JOIN folio_users.users__t AS users__t2 ON jsonb_extract_path_text(purchase_order.jsonb,'metadata','updatedByUserId')::uuid = users__t2.id::uuid;
+        CROSS JOIN LATERAL jsonb_array_elements((po_line.jsonb #> '{locations}')::jsonb) AS locations (data)
+        LEFT JOIN folio_orders.po_line__t ON po_line.id = po_line__t.id::uuid
+        LEFT JOIN folio_inventory.instance__t ON (jsonb_extract_path_text(po_line.jsonb, 'instanceId'))::uuid = instance__t.id::uuid
+        LEFT JOIN folio_inventory.location__t ON (jsonb_extract_path_text(locations.jsonb, 'locationId'))::uuid = location__t.id::uuid
+        LEFT JOIN folio_orders.purchase_order__t ON po_line.purchaseorderid = purchase_order__t.id::uuid
+        LEFT JOIN folio_inventory.holdings_record__t ON jsonb_extract_path_text(locations.jsonb, 'holdingId')::uuid = holdings_record__t.id::uuid
+        LEFT JOIN folio_inventory.location__t AS location__t2 ON holdings_record__t.permanent_location_id = location__t2.id 
+        LEFT JOIN folio_organizations.organizations__t ON purchase_order__t.vendor = organizations__t.id
+        LEFT JOIN folio_orders.purchase_order ON purchase_order__t.id::uuid = purchase_order.id
+        LEFT JOIN folio_configuration.config_data__t AS cdt ON jsonb_extract_path_text(purchase_order.jsonb, 'billTo')::uuid = cdt.id::uuid
+        LEFT JOIN folio_configuration.config_data__t AS cdt2 ON jsonb_extract_path_text(purchase_order.jsonb, 'shipTo')::uuid = cdt2.id::uuid
+        LEFT JOIN folio_users.users__t ON jsonb_extract_path_text(purchase_order.jsonb, 'metadata', 'createdByUserId')::uuid = users__t.id::uuid
+        LEFT JOIN folio_users.users__t AS users__t2 ON jsonb_extract_path_text(purchase_order.jsonb, 'metadata', 'updatedByUserId')::uuid = users__t2.id::uuid;
     
 COMMENT ON COLUMN po_instance.manual_po IS 'If true, order cannot be sent automatically, e.g. via EDI';
 
