24fb11380f4fcd718af579ad529c0930b0f6f316
Nassib Nassar <nrn@etymon.com>
2025-02-12 19:12:19 -0500

    Remove uuid casts in some derived tables

--- sql/derived_tables/invoice_adjustments_ext.sql
+++ sql/derived_tables/invoice_adjustments_ext.sql
@@ -57,7 +57,7 @@ FROM
     LEFT JOIN invoice_adjustments_in_addition_to AS invadj ON invadj.invoice_id = inv.id
     LEFT JOIN invl_total AS invltotal ON inv.id = invltotal.inv_id
     LEFT JOIN finance_transaction_invoices AS fintrainv ON fintrainv.invoice_id = inv.id AND fintrainv.invoice_line_id IS NULL
-    LEFT JOIN finance_transaction_invoices AS fintrainvl ON fintrainvl.invoice_line_id = invl.id
+    LEFT JOIN finance_transaction_invoices AS fintrainvl ON fintrainvl.invoice_line_id::text = invl.id::text
 GROUP BY
     inv.id,
     invl_id,
